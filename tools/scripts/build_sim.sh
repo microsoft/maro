4327d6f13b2e52953213f77fbc36b999a39ce7f8
Chaos Yu <v-chaoyu@microsoft.com>
2020-01-02 16:19:02 +0800

    remove unused scripts

--- tools/scripts/build_sim.sh
+++
@@ -1,8 +0,0 @@
-# temp file to quich launch mock runner
-export PYTHONPATH="$PROJECT_ROOT;$PYTHONPATH"
-
-# move to maro_sim folder to build the extensions
-cd $PROJECT_ROOT
-
-# build the cython extension first
-python setup.py build_ext -i
