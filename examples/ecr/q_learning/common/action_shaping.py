01fbfdca8d172cc4888b260b933b37513908f790
Jinyu <FAREAST.jinywan@GCRAZGDL429.redmond.corp.microsoft.com>
2020-01-16 08:59:48 +0000

    switch of early_discharge added for action_shaping

--- examples/ecr/q_learning/common/action_shaping.py
+++ examples/ecr/q_learning/common/action_shaping.py
@@ -10,7 +10,7 @@ from maro.simulator.scenarios.ecr.common import ActionScope
 from maro.utils import Logger, LogFormat
 
 class DiscreteActionShaping():
-    def __init__(self, action_space: [float]):
+    def __init__(self, action_space: [float], consider_early_discharge: bool = False):
         '''
         Init action shaping.
         Args:
@@ -18,8 +18,10 @@ class DiscreteActionShaping():
                                     action, can be symmetry or asymmetric.
                                     i.e. [-1.0, -0.9, ... , 0 , ... , 0.9, 1.0]
                                          [-1.0, -0.5, 0, ... , 0.9, 1.0]
+            consider_early_discharge (bool): minus early discharge or not
         '''
         self._action_space = action_space
+        self._consider_early_discharge = consider_early_discharge
         zero_action_indexes = []
         for i, v in enumerate(self._action_space):
             if v == 0:
@@ -27,12 +29,15 @@ class DiscreteActionShaping():
         assert(len(zero_action_indexes) == 1)
         self._zero_action_index = zero_action_indexes[0]
 
-    def __call__(self, scope: ActionScope, action_index: int, port_empty: float, vessel_remaining_space: float, early_discharge: int) -> int:
+    def __call__(self, scope: ActionScope, action_index: int, port_empty: int, vessel_remaining_space: int, early_discharge: int) -> int:
         '''
         Args:
             scope (ActionScope): Action actual available scope.
                         e.g. {'discharge': 0, 'load': 2000}
             action_index (int): Module output.
+            port_empty (int): emport inventory in current port.
+            vessel_remaining_space (int): remaining space of current vessel.
+            early_discharge (int): early discharged happended when loading full from current port to current vessel.
         '''
         assert(0 <= action_index < len(self._action_space))
 
@@ -40,11 +45,10 @@ class DiscreteActionShaping():
             return max(round(self._action_space[action_index] * port_empty), -vessel_remaining_space)
 
         if action_index > self._zero_action_index:
-            plan_action = self._action_space[action_index] * (scope.discharge + early_discharge) - early_discharge
-            if plan_action > 0:
-                return round(plan_action)
-            else:
-                return round(self._action_space[action_index] * scope.discharge) 
+            if self._consider_early_discharge:
+                plan_action = self._action_space[action_index] * (scope.discharge + early_discharge) - early_discharge
+                return max(round(plan_action), 0)
+            return round(self._action_space[action_index] * scope.discharge) 
 
         return 0
 
