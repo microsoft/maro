9adffade82145e43e281208c0085504c37dff020
Chaos Yu <v-chaoyu@microsoft.com>
2020-02-16 18:21:12 +0800

    corret api

--- maro/simulator/tests/test_snapshot.py
+++ maro/simulator/tests/test_snapshot.py
@@ -4,78 +4,53 @@
 
 import unittest
 
-from maro.simulator.graph import Graph, SnapshotList, GraphAttributeType, ResourceNodeType, GraphAttributeNotFoundError, \
-    SnapshotSliceError
+from maro.simulator.graph import Graph, SnapshotList, GraphDataType, AttributeType, GraphAttribute
 
 STATIC_NODE_NUM = 10
 DYNAMIC_NODE_NUM = 10
 MAX_TICK = 10
+
+STATIC = AttributeType.STATIC_NODE
+DYNAMIC = AttributeType.DYNAMIC_NODE
+GENERAL = AttributeType.GENERAL
+INT = GraphDataType.INT32
+FLOAT = GraphDataType.FLOAT32
 
 
 class TestGraph(unittest.TestCase):
     def setUp(self):
         self.graph = Graph(STATIC_NODE_NUM, DYNAMIC_NODE_NUM)
 
-        self.graph.register_attribute("a1", GraphAttributeType.FLOAT, 1)
-        self.graph.register_attribute("a2", GraphAttributeType.INT, 1)
-        self.graph.register_attribute("m1", GraphAttributeType.INT_MAT, 10, 2, 5)
+        self.graph.register_attribute(STATIC, "a1", FLOAT, 1)
+        self.graph.register_attribute(DYNAMIC, "a2", INT, 1)
+        self.graph.register_attribute(GENERAL, "m1", INT, 10)
 
         self.graph.setup()
 
         # init value
-        self.graph.set_attribute(ResourceNodeType.STATIC, 0, "a1", 0, 123)
-        self.graph.set_attribute(ResourceNodeType.DYNAMIC, 0, "a1", 0, 1234)
+        self.graph.set_attribute(STATIC, 0, "a1", 0, 123)
+        self.graph.set_attribute(DYNAMIC, 0, "a2", 0, 1234.567)
 
         self.snapshots = SnapshotList(self.graph, MAX_TICK)
 
-    def test_get_attribute_correct(self):
-        # take a snapshot first
-        self.snapshots.insert_snapshot(self.graph, 0)
-
-        v = self.snapshots.get_attribute(0, ResourceNodeType.STATIC, 0, "a1", 0)
-
-        self.assertEqual(v, 123)
-
-        v = self.snapshots.get_attribute(0, ResourceNodeType.DYNAMIC, 0, "a1", 0)
-
-        self.assertEqual(v, 1234)
-
-    def test_take_snapshot_correct(self):
-        self.snapshots.insert_snapshot(self.graph, 0)  # take snapshot for tick 0
-
-        v = self.snapshots.get_attribute(0, ResourceNodeType.STATIC, 0, "a1", 0)
-
-        self.assertEqual(v, 123)
-
-        # test if the value in snapshot will be changed after change graph
-        self.graph.set_attribute(ResourceNodeType.STATIC, 0, "a1", 0, 1234)
-
-        v = self.graph.get_attribute(ResourceNodeType.STATIC, 0, "a1", 0)
-
-        self.assertEqual(v, 1234)
-
-        v = self.snapshots.get_attribute(1, ResourceNodeType.STATIC, 0, "a1", 0)
-
-        self.assertEqual(v, 123)
-
     def test_get_attributes_correct(self):
-        self.snapshots.insert_snapshot(self.graph, 0)
+        self.snapshots.insert_snapshot()
 
         # change the value and insert another snapshot
-        self.graph.set_attribute(ResourceNodeType.STATIC, 0, "a1", 0, 1234)
+        self.graph.set_attribute(STATIC, 0, "a1", 0, 1234)
 
-        self.snapshots.insert_snapshot(self.graph, 1)
+        self.snapshots.insert_snapshot()
 
         # query 1st slot of a1 at tick (0, 1)
-        states = self.snapshots.get_attributes(ResourceNodeType.STATIC, [0, 1], [0, ], ["a1", ], [0, ])
+        states = self.snapshots.get_node_attributes(STATIC, [0, 1], [0, ], ["a1", ], [0, ], 0)
 
         self.assertEqual(states[0], 123)  # value at tick 0
         self.assertEqual(states[1], 1234)  # value at tick 1
 
     def test_get_attributes_with_invalid_ticks(self):
-        self.snapshots.insert_snapshot(self.graph, 0)
+        self.snapshots.insert_snapshot()
 
-        states = self.snapshots.get_attributes(ResourceNodeType.STATIC, [-1, 0, 1], [0, ], ["a1", ], [0, ])
+        states = self.snapshots.get_node_attributes(STATIC, [-1, 0, 1], [0, ], ["a1", ], [0, ], 0)
 
         # since we will padding the value for invalid ticks, so it should return numpy array that shape is (3,)
         self.assertEqual(states.shape, (3,))
@@ -84,13 +59,6 @@ class TestGraph(unittest.TestCase):
         self.assertEqual(states[0], 0)
         self.assertEqual(states[1], 123)
         self.assertEqual(states[2], 0)
-
-    def test_get_attribute_with_undefined_attribute(self):
-        self.snapshots.insert_snapshot(self.graph, 0)
-
-        # get_attribute method require that the attribute must be registered
-        with self.assertRaises(GraphAttributeNotFoundError) as ctx:
-            self.snapshots.get_attribute(1, ResourceNodeType.STATIC, 0, "a3", 0)
 
     def test_attribute_info(self):
         attrs = self.snapshots.attributes
@@ -101,23 +69,21 @@ class TestGraph(unittest.TestCase):
         self.assertEqual("a2", attrs[1]["name"])
         self.assertEqual(1, attrs[1]["slot length"])
 
-        self.assertEqual(1, self.snapshots.get_slot_length("a1"))
-
     def test_get_matrix(self):
-        self.snapshots.insert_snapshot(self.graph, 0)
+        self.snapshots.insert_snapshot()
 
         for i in range(2):
             for j in range(5):
-                self.graph.set_int_matrix_value("m1",  i, j, i*10 + j)
+                self.graph.set_attribute(GENERAL, 0, "m1", i*5 + j, i*5 + j)
 
-        self.snapshots.insert_snapshot(self.graph, 1)
+        self.snapshots.insert_snapshot()
 
         # check matrix value at tick 1
         v = self.snapshots.matrix[1: "m1"]
 
         for i in range(2):
             for j in range(5):
-                self.assertEqual(i*10+j, v[0][i*5+j])
+                self.assertEqual(i*5+j, v[0][i*5+j])
 
         # check matrix value at tick 0
         v = self.snapshots.matrix[0: "m1"]
@@ -127,12 +93,12 @@ class TestGraph(unittest.TestCase):
                 self.assertEqual(0, v[0][i*5+j])
 
     def test_snapshot_slice(self):
-        self.snapshots.insert_snapshot(self.graph, 0)
+        self.snapshots.insert_snapshot()
 
         # change the value and insert another snapshot
-        self.graph.set_attribute(ResourceNodeType.STATIC, 0, "a1", 0, 1234)
+        self.graph.set_attribute(STATIC, 0, "a1", 0, 1234)
 
-        self.snapshots.insert_snapshot(self.graph, 1)
+        self.snapshots.insert_snapshot()
 
         ticks = [0, 1]
         ids = [0]
@@ -141,7 +107,7 @@ class TestGraph(unittest.TestCase):
 
         # query 1st slot of a1 at tick (0, 1)
 
-        states = self.snapshots.get_attributes(ResourceNodeType.STATIC, ticks, ids, attrs, slots)
+        states = self.snapshots.get_node_attributes(STATIC, ticks, ids, attrs, slots, 0)
 
         v = self.snapshots.static_nodes[ticks: ids: (attrs, slots)]
         v = self.snapshots.static_nodes[ticks: 0: ("a1", 0)]
@@ -171,8 +137,8 @@ class TestGraph(unittest.TestCase):
 
         # more on short version
         # we should provide attributes names and indices, cannot get all
-        with self.assertRaises(SnapshotSliceError) as ctx:
-            v = self.snapshots.static_nodes[::]
+        # with self.assertRaises(SnapshotSliceError) as ctx:
+        #     v = self.snapshots.static_nodes[::]
 
         # get all the ticks
         v = self.snapshots.static_nodes[: 0: ("a1", 0)]
