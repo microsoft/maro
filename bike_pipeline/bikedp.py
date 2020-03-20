49b11c5b349010a5504984f7b8b1710a8c43bd7b
pocket5084 <pocket_2001@163.com>
2020-03-20 16:54:39 +0800

    test gen bin data with h3 data

--- bike_pipeline/bikedp.py
+++ bike_pipeline/bikedp.py
@@ -1,11 +1,15 @@
+# usage:
+# python bikedp.py ../../ny ../../ny/bin ../../ny/full/h3_201306_202001.station.csv
+
 import os
 import sys
 import csv
 import math
 import datetime
 import numpy as np
+import pandas as pd
 
-from geopy import distance
+# from geopy import distance
 from collections import defaultdict
 
 ######### options ##########
@@ -19,9 +23,92 @@ from collections import defaultdict
 "2014-01 - Citi Bike trip data.csv",
 "2014-02 - Citi Bike trip data.csv",
 """
+######### full data ##########
+"""
+"201306-citibike-tripdata.csv",
+"201307-citibike-tripdata.csv",
+"201308-citibike-tripdata.csv",
+"201309-citibike-tripdata.csv",
+"201310-citibike-tripdata.csv",
+"201311-citibike-tripdata.csv",
+"201312-citibike-tripdata.csv",
+"201401-citibike-tripdata.csv",
+"201402-citibike-tripdata.csv",
+"201403-citibike-tripdata.csv",
+"201404-citibike-tripdata.csv",
+"201405-citibike-tripdata.csv",
+"201406-citibike-tripdata.csv",
+"201407-citibike-tripdata.csv",
+"201408-citibike-tripdata.csv",
+"201409-citibike-tripdata.csv",
+"201410-citibike-tripdata.csv",
+"201411-citibike-tripdata.csv",
+"201412-citibike-tripdata.csv",
+"201501-citibike-tripdata.csv",
+"201502-citibike-tripdata.csv",
+"201503-citibike-tripdata.csv",
+"201504-citibike-tripdata.csv",
+"201505-citibike-tripdata.csv",
+"201506-citibike-tripdata.csv",
+"201507-citibike-tripdata.csv",
+"201508-citibike-tripdata.csv",
+"201509-citibike-tripdata.csv",
+"201510-citibike-tripdata.csv",
+"201511-citibike-tripdata.csv",
+"201512-citibike-tripdata.csv",
+"201601-citibike-tripdata.csv",
+"201602-citibike-tripdata.csv",
+"201603-citibike-tripdata.csv",
+"201604-citibike-tripdata.csv",
+"201605-citibike-tripdata.csv",
+"201606-citibike-tripdata.csv",
+"201607-citibike-tripdata.csv",
+"201608-citibike-tripdata.csv",
+"201609-citibike-tripdata.csv",
+"201610-citibike-tripdata.csv",
+"201611-citibike-tripdata.csv",
+"201612-citibike-tripdata.csv",
+"201701-citibike-tripdata.csv",
+"201702-citibike-tripdata.csv",
+"201703-citibike-tripdata.csv",
+"201704-citibike-tripdata.csv",
+"201705-citibike-tripdata.csv",
+"201706-citibike-tripdata.csv",
+"201707-citibike-tripdata.csv",
+"201708-citibike-tripdata.csv",
+"201709-citibike-tripdata.csv",
+"201710-citibike-tripdata.csv",
+"201711-citibike-tripdata.csv",
+"201712-citibike-tripdata.csv",
+"201801-citibike-tripdata.csv",
+"201802-citibike-tripdata.csv",
+"201803-citibike-tripdata.csv",
+"201804-citibike-tripdata.csv",
+"201805-citibike-tripdata.csv",
+"201806-citibike-tripdata.csv",
+"201807-citibike-tripdata.csv",
+"201808-citibike-tripdata.csv",
+"201809-citibike-tripdata.csv",
+"201810-citibike-tripdata.csv",
+"201811-citibike-tripdata.csv",
+"201812-citibike-tripdata.csv",
+"201901-citibike-tripdata.csv",
+"201902-citibike-tripdata.csv",
+"201903-citibike-tripdata.csv",
+"201904-citibike-tripdata.csv",
+"201905-citibike-tripdata.csv",
+"201906-citibike-tripdata.csv",
+"201907-citibike-tripdata.csv",
+"201908-citibike-tripdata.csv",
+"201909-citibike-tripdata.csv",
+"201910-citibike-tripdata.csv",
+"201911-citibike-tripdata.csv",
+"201912-citibike-tripdata.csv",
+"202001-citibike-tripdata.csv"
+"""
 # NOTE: the order will affect the result data
 input_file_list = [
-    "JC-201912-citibike-tripdata.csv"
+    "201912-citibike-tripdata.csv"
 ]
 
 usertype_map={
@@ -101,8 +188,8 @@ def read_src_file(file: str):
 
                 ret.append(item)
 
-                stations[item[2]] = (item[3], item[6], item[7]) # start station, log and lat
-                stations[item[4]] = (item[5], item[8], item[9]) # end station, log and lat
+                # stations[item[2]] = (item[3], item[6], item[7]) # start station, log and lat
+                # stations[item[4]] = (item[5], item[8], item[9]) # end station, log and lat
 
     return ret, stations
 
@@ -123,19 +210,33 @@ def init(output_folder: str):
     np.memmap()
 
 
-def concat(data: list, file: str):
+def concat(data: list, file: str, station_data: pd.DataFrame):
     ret = []
 
     item_num = len(data)
 
     for d in data:
+        from_cell_id = station_data.loc[int(station_data['station_id']) == d[2], 'hex_id']
+        to_cell_id = station_data.loc[int(station_data['station_id']) == d[4], 'hex_id']
+        print((
+            d[1],
+            d[2],
+            d[4],
+            d[0],
+            d[10],
+            d[11],
+            from_cell_id,
+            to_cell_id
+        ))
         ret.append((
             d[1],
             d[2],
             d[4],
             d[0],
             d[10],
-            d[11]
+            d[11],
+            from_cell_id,
+            to_cell_id
         ))
 
     # get the file size
@@ -192,32 +293,43 @@ def save_distance_table(data: dict, file: str):
                 row_data
             )
 
+def read_station_file(station_file_path):
+    h3_data = None
+    if os.path.exists(station_file_path):
+        with open(station_file_path, mode="r", encoding="utf-8") as station_file:
+            h3_data = pd.read_csv(station_file)
+    return h3_data
+
+
 if __name__ == "__main__":
     input_folder = sys.argv[1]
     output_folder = sys.argv[2]
+    station_file_path = sys.argv[3]
 
     output_data_path = os.path.join(output_folder, data_file_name)
 
-    if len(sys.argv) >= 4:
-        # tick type
-        time_lvl = sys.argv[3]
-    
     if len(sys.argv) >= 5:
-        duration_type = sys.argv[4]
+        # tick type
+        time_lvl = sys.argv[4]
+    
+    if len(sys.argv) >= 6:
+        duration_type = sys.argv[5]
 
     station_map = {}
+
+    station_data = read_station_file(station_file_path)
 
     for src_file in input_file_list:
         src_full_path = os.path.join(input_folder, src_file)
 
         r,s = read_src_file(src_full_path)
 
-        s = distinct_stations(station_map, s)
+        # s = distinct_stations(station_map, s)
 
-        concat(r, output_data_path)
+        concat(r, output_data_path, station_data)
 
-    map_full_path = os.path.join(output_folder, mapping_file_name)
-    save_mapping(station_map, map_full_path)
+    # map_full_path = os.path.join(output_folder, mapping_file_name)
+    # save_mapping(station_map, map_full_path)
 
-    distance_full_path = os.path.join(output_folder, distance_table_name)
-    save_distance_table(station_map, distance_full_path)
+    # distance_full_path = os.path.join(output_folder, distance_table_name)
+    # save_distance_table(station_map, distance_full_path)
