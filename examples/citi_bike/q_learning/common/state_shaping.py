3ef11efceb72dc56a44d3f738ee48d4acc0e3608
Xinran Wei <v-xinwe@microsoft.com>
2020-03-24 13:43:58 +0800

    init dqn

--- examples/citi_bike/q_learning/common/state_shaping.py
+++ examples/citi_bike/q_learning/common/state_shaping.py
@@ -18,32 +18,22 @@ class StateShaping():
     def __init__(self,
                  env: Env,
                  relative_tick_list: [int],
-                 port_downstream_max_number: int,
-                 port_attribute_list: [str],
-                 vessel_attribute_list: [str]
+                 station_attribute_list: [str],
+                 neighbor_number: int,
                  ):
         self._env = env
         self._relative_tick_list = relative_tick_list
-        self._port_downstream_max_number = port_downstream_max_number
-        self._port_attribute_list = port_attribute_list
-        self._vessel_attribute_list = vessel_attribute_list
+        self._station_attribute_list = station_attribute_list
         self._dim = (len(self._relative_tick_list) + 1) * \
-            (self._port_downstream_max_number + 1) * \
-            len(self._port_attribute_list) + len(self._vessel_attribute_list)
-
-    def __call__(self, cur_tick: int, cur_port_idx: int, cur_vessel_idx: int):
+            (neighbor_number + 1) * \
+            len(self._station_attribute_list)
+            
+    def __call__(self, cur_tick: int, cur_station_idx: int, cur_neighbor_idx_list = [int]):
         ticks = [cur_tick] + [cur_tick + rt for rt in self._relative_tick_list]
-        future_port_slot_idx_list = [i for i in range(
-            self._port_downstream_max_number)]
-        future_port_idx_list = self._env.snapshot_list.dynamic_nodes[cur_tick: cur_vessel_idx: ('future_stop_list', future_port_slot_idx_list)]
+        # cur_neighbor_idx_list = self._env.snapshot_list.static_nodes[ticks: [cur_station_idx]: ('neightbors', 0)]
+        station_features = self._env.snapshot_list.static_nodes[ticks: [cur_station_idx] + list(cur_neighbor_idx_list): (self._station_attribute_list, 0)]
 
-        port_features = self._env.snapshot_list.static_nodes[ticks: [cur_port_idx] + list(future_port_idx_list): (self._port_attribute_list, 0)]
-
-        vessel_features = self._env.snapshot_list.dynamic_nodes[cur_tick: cur_vessel_idx: (self._vessel_attribute_list, 0)]
-
-        res = np.concatenate((port_features, vessel_features))
-
-        return res
+        return station_features
 
     @property
     def dim(self):
