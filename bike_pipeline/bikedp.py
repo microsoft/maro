9e7917169b7a968ecbe52ce1495b58947775e31d
pocket5084 <pocket_2001@163.com>
2020-04-07 17:47:00 +0800

    integrate h3 neighbor in 100 distance to process

--- bike_pipeline/bikedp.py
+++ bike_pipeline/bikedp.py
@@ -13,6 +13,8 @@ import pandas as pd
 # from geopy import distance
 from collections import defaultdict
 
+from h3 import h3
+
 # NOTE: the order will affect the result data
 input_file_list = [
     # "201306-citibike-tripdata.csv",
@@ -105,7 +107,7 @@ usertype_map = {
 }
 
 data_file_name = "data.bin"
-mapping_file_name = "map.csv"
+neighbor_file_name = "map.csv"
 cell_file_name = "cell.csv"
 cell_name_file_name = "cell_name.csv"
 
@@ -161,52 +163,74 @@ def read_station_file(station_file_path):
     return h3_data
 
 
-def station_to_cell(station_file_path: str):
+def _station_to_cell(station_file_path: str):
     # cell init data
-    cell_data = None
+    cell_init = None
     # station to cell mapping
-    station_data = None
+    station_to_cell = None
     # cell neighbors data
-    mapping_map = None
+    # mapping_map = None
 
     if os.path.exists(station_file_path):
         with open(station_file_path, mode="r", encoding="utf-8") as station_file:
             # read station to cell file
             raw_station_data = pd.read_csv(station_file)
             # group by cell to generate cell init info
-            cell_data = raw_station_data[['hex_id', 'capacity', 'init']].groupby(['hex_id']).sum().sort_values(by='hex_id', ascending=True).reset_index()
+            cell_init = raw_station_data[['hex_id', 'capacity', 'init']].groupby(['hex_id']).sum().sort_values(by='hex_id', ascending=True).reset_index()
             # generate cell id by index
-            cell_data['cell_id'] = pd.to_numeric(cell_data.index)
-            cell_data['capacity'] = pd.to_numeric(cell_data['capacity'], downcast='integer')
-            cell_data['init'] = pd.to_numeric(cell_data['init'], downcast='integer')
+            cell_init['cell_id'] = pd.to_numeric(cell_init.index)
+            cell_init['capacity'] = pd.to_numeric(cell_init['capacity'], downcast='integer')
+            cell_init['init'] = pd.to_numeric(cell_init['init'], downcast='integer')
             # cell_data columns = ['cell_id','capacity','init','hex_id']
 
             # fill cell id back to station-cell mapping
-            station_data = raw_station_data.join(cell_data[['cell_id', 'hex_id']].set_index('hex_id'), on='hex_id')
+            station_to_cell = raw_station_data.join(cell_init[['cell_id', 'hex_id']].set_index('hex_id'), on='hex_id')
 
-            print(station_data, cell_data)
+            print(station_to_cell, cell_init)
 
             # generate cell neighbors data from column neighbors
-            mapping_data = station_data[['cell_id', 'hex_id', 'neighbors']].drop_duplicates(subset=['cell_id']).reset_index()
-            mapping_data['mapping'] = mapping_data['neighbors'].apply(lambda x: _gen_neighbor_mapping(x, mapping_data[['cell_id', 'hex_id']]))
-            mapping_map = pd.DataFrame(-1, index=np.arange(len(mapping_data)), columns=np.arange(6), dtype=np.int64)
-            mapping_data[['cell_id', 'mapping']].apply(lambda x:  _fill_mapping(x, mapping_map), axis=1)
-            mapping_map['cell_id'] = mapping_data['cell_id']
+            # mapping_data = station_data.drop_duplicates(subset=['cell_id']).reset_index()
+            # mapping_data['neighbors_v2'] = mapping_data.apply(lambda x: _find_neighbors_by_row(x, mapping_data[['cell_id', 'hex_id']], 6), axis = 1)
+            # mapping_data['mapping'] = mapping_data['neighbors_v2'].apply(lambda x: _gen_neighbor_mapping_v2(x, mapping_data[['cell_id', 'hex_id']]))
+            # mapping_map = pd.DataFrame(-1, index=np.arange(len(mapping_data)), columns=np.arange(6), dtype=np.int64)
+            # mapping_data[['cell_id', 'mapping']].apply(lambda x:  _fill_mapping(x, mapping_map), axis=1)
+            # mapping_map['cell_id'] = mapping_data['cell_id']
+            # print(mapping_map)
 
-            print(mapping_map)
-    return cell_data, station_data, mapping_map
+    return cell_init, station_to_cell
 
+def _find_neighbors_by_row(row, cell_to_hex, tar_num):
+    neighbors = []
+    cur_distance = 1
+    # check if there are 6 neighbors in 100 cell distance
+    while len(neighbors) < tar_num and cur_distance < 100:
+        new_neighbors = pd.Series(list(h3.k_ring_distances(row['hex_id'],cur_distance)[cur_distance]))
+        selected_neighbors = new_neighbors[new_neighbors.isin(cell_to_hex['hex_id'])].to_list()
+        neighbors = (neighbors + selected_neighbors)[:tar_num]
+        cur_distance += 1
+        if cur_distance == 100:
+            print(row)
+    return neighbors
 
-def _gen_neighbor_mapping(neighbors: str, neighbors_mapping: pd.DataFrame):
+def _gen_neighbor_mapping(neighbors: str, cell_to_hex: pd.DataFrame):
     # get neighbors list from neighbors string
     hex_list = re.findall(r'[0-9a-fA-F]+', neighbors)
     # remove neighbors not in cell list
-    hex_df = pd.DataFrame(pd.Series(hex_list), columns=['hex_id']).join(neighbors_mapping.set_index('hex_id'), on='hex_id').reset_index()
+    hex_df = pd.DataFrame(pd.Series(hex_list), columns=['hex_id']).join(cell_to_hex.set_index('hex_id'), on='hex_id').reset_index()
     hex_df.dropna(subset=['cell_id'], inplace=True)
     # pick cell id of neighbors
     ret = hex_df['cell_id'].tolist()
     return ret
 
+def _gen_neighbor_mapping_v2(neighbors: list, cell_to_hex: pd.DataFrame):
+    # remove neighbors not in cell list
+    hex_df = pd.DataFrame()
+    hex_df['hex_id']=pd.Series(neighbors)
+    hex_df = hex_df.join(cell_to_hex[['cell_id', 'hex_id']].set_index('hex_id'), on='hex_id')
+    hex_df.dropna(subset=['cell_id'], inplace=True)
+    # pick cell id of neighbors
+    ret = hex_df['cell_id'].tolist()
+    return ret
 
 def _fill_mapping(row, mapping_map: pd.DataFrame):
     column = 0
@@ -216,49 +240,45 @@ def _fill_mapping(row, mapping_map: pd.DataFrame):
         # filter self in neighbors
         if row['mapping'][i] != x:
             # fill mapping_map
-            mapping_map.loc[x, column] = int(row['mapping'][i])
+            mapping_map.loc[mapping_map.index==x, column] = int(row['mapping'][i])
             # skip self, use new column c in mapping_map 
             column += 1
 
-
-
-def _filter_mapping(row, cell_list):
-    ret = row.copy()
-    for idx in row.index:
-        if row[idx] != -1 and row[idx] not in cell_list:
-            ret[idx] = -1
-    return ret
-
 ######### output ############
 
-def concat(data: pd.DataFrame, file: str, station_data: pd.DataFrame, mapping_map: pd.DataFrame):
+def concat(data: pd.DataFrame, file: str, station_to_cell: pd.DataFrame):
     ret = data[['starttime', 'start station id', 'end station id', 'tripduration', 'usertype', 'gender']]
 
     # get the file size
     file_size = 0
-    ret = ret.join(station_data[['station_id', 'cell_id']].set_index('station_id'), on='start station id').rename(columns={'cell_id': 'start_cell'})
-    ret = ret.join(station_data[['station_id', 'cell_id']].set_index('station_id'), on='end station id').rename(columns={'cell_id': 'end_cell'})
+    ret = ret.join(station_to_cell[['station_id', 'cell_id']].set_index('station_id'), on='start station id').rename(columns={'cell_id': 'start_cell'})
+    ret = ret.join(station_to_cell[['station_id', 'cell_id']].set_index('station_id'), on='end station id').rename(columns={'cell_id': 'end_cell'})
     ret = ret.rename(columns={'starttime': 'start_time', 'start station id': 'start_station', 'end station id': 'end_station', 'tripduration': 'duration'})
     ret = ret[['start_time', 'start_station', 'end_station', 'duration', 'gender', 'usertype', 'start_cell', 'end_cell']]
 
     # generate cell need to be dropped, because it has no neighbors
+
+    #   get in data cells
     used_cells = []
     used_cells.append(ret[['start_cell']].drop_duplicates(subset=['start_cell']).rename(columns={'start_cell': 'cell_id'}))
     used_cells.append(ret[['end_cell']].drop_duplicates(subset=['end_cell']).rename(columns={'end_cell': 'cell_id'}))
-    data_cell = pd.concat(used_cells).drop_duplicates().sort_values(by=['cell_id']).reset_index()
-    data_mapping_data = mapping_data.set_index('cell_id')
-    data_mapping_data = data_cell.join(data_mapping_data, on='cell_id').set_index('cell_id').drop(['index'], axis = 1)
-    data_mapping_data_drop = data_mapping_data.copy().apply(lambda x: _filter_mapping(x,data_cell['cell_id'].to_list()), axis = 1)
-
-    # print(data_mapping_data)
-    drop_mapping_data = data_mapping_data_drop[data_mapping_data_drop.sum(axis=1) == -6]
-    # print(drop_mapping_data)
-    # print(data_mapping_data[pd.to_numeric(data_mapping_data.index).isin (pd.to_numeric(drop_mapping_data.index))])
-    drop_cell = pd.to_numeric(drop_mapping_data.index).to_list()
-    print(drop_cell)
+    in_data_cell = pd.concat(used_cells).drop_duplicates(subset=['cell_id']).sort_values(by=['cell_id']).reset_index()
+    #   get in data cell hex_id
+    data_mapping_data = in_data_cell.join(station_to_cell[['cell_id', 'hex_id']].drop_duplicates(subset=['cell_id']).set_index('cell_id'), on='cell_id').drop(['index'], axis = 1)
+    #   get in data cell neighbors in hex_id
+    data_mapping_data['neighbors_v2'] = data_mapping_data.apply(lambda x: _find_neighbors_by_row(x, data_mapping_data, 6), axis = 1)
+    #   get in data cell neighbors in cell_id
+    data_mapping_data['mapping'] = data_mapping_data['neighbors_v2'].apply(lambda x: _gen_neighbor_mapping_v2(x, data_mapping_data[['cell_id', 'hex_id']]))
+    #   get neighbors in cell bool matrix
+    data_neighbor = pd.DataFrame(-1, index=data_mapping_data['cell_id'], columns=np.arange(6), dtype=np.int64)
+    data_mapping_data[['cell_id', 'mapping']].apply(lambda x:  _fill_mapping(x, data_neighbor), axis=1)
+    #   get cells without neighbors
+    drop_mapping_data = data_neighbor[data_neighbor.sum(axis=1) == -6]
+    drop_mapping_data["cell_id"] = pd.to_numeric(drop_mapping_data.index)
+    print(drop_mapping_data)
     before = len(ret)
     # drop cell have no neighbors
-    ret.drop(ret[ret['start_cell'].apply(lambda x: x in drop_cell) | ret['end_cell'] .apply(lambda x: x in drop_cell)].index, axis=0, inplace=True)
+    ret.drop(ret[ret['start_cell'].isin(drop_mapping_data['cell_id']) | ret['end_cell'].isin(drop_mapping_data['cell_id'])].index, axis=0, inplace=True)
     after = len(ret)
     print(f"{before - after}/{before} rows droped {((before - after)/before * 100):.2f}%")
     mem_output = list(ret.itertuples(index=False, name=None))
@@ -278,7 +298,7 @@ def concat(data: pd.DataFrame, file: str, station_data: pd.DataFrame, mapping_ma
     arr[:] = np.array(mem_output, dtype=output_data_dtype)
 
     
-    return data_cell
+    return in_data_cell
 
 
 
@@ -292,11 +312,11 @@ if __name__ == "__main__":
 
     output_data_path = os.path.join(output_folder, data_file_name)
     cell_file_path = os.path.join(output_folder, cell_file_name)
-    mapping_file_path = os.path.join(output_folder, mapping_file_name)
+    neighbor_file_path = os.path.join(output_folder, neighbor_file_name)
     cell_name_file_path = os.path.join(output_folder, cell_name_file_name)
 
     # generate full cell data
-    cell_init, station_data, mapping_data = station_to_cell(station_file_path)
+    cell_init, station_to_cell = _station_to_cell(station_file_path)
 
     data_cell_dfs = []
     # generate bin file
@@ -308,8 +328,8 @@ if __name__ == "__main__":
 
         r, s = read_src_file(src_full_path)
 
-        if r is not None:
-            data_cell_dfs.append(concat(r, output_data_path, station_data, mapping_data))
+        if r is not None and len(r)>0:
+            data_cell_dfs.append(concat(r, output_data_path, station_to_cell))
 
     # filter cell by data
     data_cell = pd.concat(data_cell_dfs).drop_duplicates(subset=['cell_id']).sort_values(by=['cell_id']).reset_index()
@@ -317,10 +337,18 @@ if __name__ == "__main__":
     data_cell_init = data_cell.join(cell_init.set_index('cell_id'), on='cell_id')
 
     data_cell_name = data_cell_init[['cell_id','hex_id']]
+    
+    data_mapping_data = data_cell_init[['cell_id','hex_id']]
+
     data_cell_init = data_cell_init[['cell_id', 'capacity', 'init']]
 
-    data_mapping_data = data_cell_init[['cell_id']].join(mapping_data.set_index('cell_id'), on='cell_id')
-    data_mapping_data= data_mapping_data.apply(lambda x: _filter_mapping(x,data_cell_init['cell_id'].to_list()), axis = 1)
+    # generate cell neighbors
+    data_mapping_data['neighbors'] = data_mapping_data.apply(lambda x: _find_neighbors_by_row(x, data_cell_name, 6), axis = 1)
+    data_mapping_data['mapping'] = data_mapping_data['neighbors'].apply(lambda x: _gen_neighbor_mapping_v2(x, data_cell_name))
+    data_neighbor = pd.DataFrame(-1, index=data_mapping_data['cell_id'], columns=np.arange(6), dtype=np.int64)
+    data_mapping_data[['cell_id', 'mapping']].apply(lambda x:  _fill_mapping(x, data_neighbor), axis=1)
+    data_neighbor['cell_id'] = pd.to_numeric(data_neighbor.index, downcast='integer')
+
 
 
     # write cell init file
@@ -328,8 +356,8 @@ if __name__ == "__main__":
         data_cell_init.to_csv(cell_file, index=False)
 
     # write cell neighbors file
-    with open(mapping_file_path, mode="w", encoding="utf-8", newline='') as mapping_file:
-        data_mapping_data.to_csv(mapping_file, index=False)
+    with open(neighbor_file_path, mode="w", encoding="utf-8", newline='') as mapping_file:
+        data_neighbor.to_csv(mapping_file, index=False)
 
     # write cell name file
     with open(cell_name_file_path, mode="w", encoding="utf-8", newline='') as cell_name_file:
