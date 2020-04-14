37eeb13e7a3bfba9e7154f9561dcf3adf95e7964
Chaos Yu <v-chaoyu@microsoft.com>
2020-04-14 21:26:45 +0800

    update hello

--- examples/hello_world/logistics/hello.py
+++ examples/hello_world/logistics/hello.py
@@ -13,15 +13,19 @@ def start():
     for episode in range(MAX_EPISODE):
         env.reset()
 
-        reward, decision_event, is_done = env.step(None)
+        # NOTE: first action must be None
+        action = None
 
         while not is_done:
-            reward, decision_event, is_done = env.step(randint(1, 10))
+            reward, decision_event, is_done = env.step(action)
+
+            action = randint(1, 10)
+            # action = 0
 
         # following code show how to retrieve states from snapshotlist
         stock_at_all_tick = env.snapshot_list.static_nodes[::("stock", 0)]
 
-        print(stock_at_all_tick)
+        # print(stock_at_all_tick)
 
         # since our business engine will stop before reach the max tick, so we should use current tick as upper bound to query valid sequence
         ticks = [i for i in range(env.tick+1)] # include last tick
