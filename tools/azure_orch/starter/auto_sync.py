1413dc79318a4121859f11fe05106249378dc684
Ubuntu <tianyi@god.ntfvthicpanudaa4bcpauw22zf.ix.internal.cloudapp.net>
2020-04-14 18:31:59 +0000

    delete files

--- tools/azure_orch/starter/auto_sync.py
+++
@@ -1,9 +0,0 @@
-import sys 
-import os
-sys.path.append("..") 
-from utils import auto_sync
-
-if __name__ == "__main__":
-    src = os.environ['PYTHONPATH']
-    dest = "/codepoint/"
-    auto_sync(src, dest)
