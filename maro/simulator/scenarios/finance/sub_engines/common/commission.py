92a54cbb39d9db17095fa99103ffb93f103d55f7
pocket5084 <pocket_2001@163.com>
2020-05-15 10:22:30 +0800

    refine commission

--- maro/simulator/scenarios/finance/sub_engines/common/commission.py
+++ maro/simulator/scenarios/finance/sub_engines/common/commission.py
@@ -10,78 +10,78 @@ class CommissionType(Enum):
 
 
 class Commission():
-    __commission_type = None
-    __min_fee = 0
+    _commission_type = None
+    _min_fee = 0
 
     def __init__(self, min_fee):
-        self.__min_fee = min_fee
+        self._min_fee = min_fee
 
     def execute(self, actual_price: float, actual_volume: int):
         pass
 
     @property
     def commission_type(self):
-        return self.__commission_type
+        return self._commission_type
 
     @property
     def min_fee(self):
-        return self.__min_fee
+        return self._min_fee
 
 
 class ByMoneyCommission(Commission):
-    __fee_rate = 0
+    _fee_rate = 0
 
     def __init__(self, fee_rate: float = 0, min_fee: float = 0):
         Commission.__init__(self, min_fee)
-        self.__commission_type = CommissionType.by_money_commission
-        self.__fee_rate = fee_rate
-        self.__min_fee = min_fee
+        self._commission_type = CommissionType.by_money_commission
+        self._fee_rate = fee_rate
+        self._min_fee = min_fee
 
     def execute(self, actual_price: float, actual_volume: int)-> float:
-        return round(max(actual_price*abs(actual_volume)*self.__fee_rate, self.__min_fee), 2)
+        return round(max(actual_price*abs(actual_volume)*self.fee_rate, self.min_fee), 2)
 
     @property
     def fee_rate(self):
-        return self.__fee_rate
+        return self._fee_rate
 
 
 class ByVolumeCommission(Commission):
-    __pre_volume_fee = 0
+    _pre_volume_fee = 0
 
     def __init__(self, pre_volume_fee: float = 0, min_fee: float = 0):
         Commission.__init__(self, min_fee)
-        self.__commission_type = CommissionType.by_volume_commission
-        self.__pre_volume_fee = pre_volume_fee
+        self._commission_type = CommissionType.by_volume_commission
+        self._pre_volume_fee = pre_volume_fee
 
     def execute(self, actual_price: float, actual_volume: int)-> float:
-        return round(max(abs(actual_volume)*self.__pre_volume_fee, self.__min_fee), 2)
+        return round(max(abs(actual_volume)*self.pre_volume_fee, self.min_fee), 2)
 
     @property
     def pre_volume_fee(self):
-        return self.__pre_volume_fee
+        return self._pre_volume_fee
 
 
 class ByTradeCommission(Commission):
-    __pre_trade_fee = 0
+    _pre_trade_fee = 0
 
     def __init__(self, pre_trade_fee: float = 0, min_fee: float = 0):
         Commission.__init__(self, min_fee)
-        self.__commission_type = CommissionType.by_volume_commission
-        self.__pre_trade_fee = pre_trade_fee
+        self._commission_type = CommissionType.by_volume_commission
+        self._pre_trade_fee = pre_trade_fee
 
     def execute(self, actual_price: float, actual_volume: int)-> float:
-        return round(max(self.__pre_trade_fee, self.__min_fee), 2)
+        return round(max(self.pre_trade_fee, self.min_fee), 2)
 
     @property
     def pre_trade_fee(self):
-        return self.__pre_trade_fee
+        return self._pre_trade_fee
 
 
 class StampTaxCommission(ByMoneyCommission):
 
     def __init__(self, tax_rate: float = 0, min_fee: float = 0):
         ByMoneyCommission.__init__(self, tax_rate, min_fee)
-        self.__commission_type = CommissionType.stamp_tax_commission
+        self._commission_type = CommissionType.stamp_tax_commission
 
     def execute(self, actual_price: float, actual_volume: int)-> float:
         if actual_volume < 0:
