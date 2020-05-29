ba445fb5876d4e4a89edf4569e9fa6b83ae9e35b
kaiqli <v-kaiqli@microsoft.com>
2020-05-29 09:23:00 +0000

    partial update registry_table

--- maro/distributed/registry_table.py
+++ maro/distributed/registry_table.py
@@ -1,87 +1,93 @@
 
+# subconstraint = 'source:type:num'
+
+class Constraint():
+    def __init__(self, constraint: str (subconstriant or (subconstriant, subconstraint, operation))):
+         # decode constraint
+        self._subconstraint_dict = {} # {subconstraint: msg list}
+        self._logic_relation = [[subconstraint, subconstraint, ...], [subconstraint, subconstraint, ...]]
+        constraint = constraint.replace(' ', '')
+
+    def _constraint_type(self, constraint):
+        cparen_position = constraint.rfind(')')
+        if cparen_position == -1:
+            self._check_subconstraint_expression(constraint)
+            self._subconstraint_dict[constraint] = []
+            self._logic_relation.append([constraint])
+        else:
+            oparen_position = constraint[:cparen_position].find('(')
+            self._constraint_decomposer(constraint[oparen_position+1:cparen_position])
+
+    def _check_subconstraint_expression(self, sub_constraint):
+        decomposer = sub_constraint.split(':')
+        assert(len(decomposer)==3)
+        # number must be int or percentage(*%)
+        if decomposer[-1][-1] == '%':
+            decomposer[-1] = decomposer[-1][:-1]
+        
+        try:
+            number = int(decomposer[-1])
+        except:
+            print(f"number must be int or percentage with % in the end.")
+    
+    def _constraint_decomposer(self, constraint):
+        operation_list = ['&&', '||', 'AND', 'OR']
+        # clear constraint
+        constraint = constraint.replace('(', '')
+        constraint = constraint.replace(')', '')
+
+        constraint_decomposer = constraint.split(',')
+        op_index = []
+        # check expression
+        for idx, component in enumerate(constraint_decomposer):
+            if component in operation_list:
+                op_index.append(idx)
+            else:
+                self._check_subconstraint_expression(component)
+        
+        used_component_index = []
+        for op_i in op_index:
+            used_component_index.append(op_i)
+            combined_constraint = False
+            curr_index = op_i - 1
+            while curr_index in used_component_index:
+                curr_index -= 1
+                combined_constraint = True
+            if combined_constraint:
+                self._logic_update(constraint_decomposer[curr_index], constraint_decomposer[op_i])
+                used_component_index.append(curr_index)
+            else:
+                used_component_index.append(curr_index)
+                used_component_index.append(curr_index)
+
+
+    def _check_constraint_satisfied(self):
+        # check if self satisfied
+
+    def constraint_satisfied(self, message):
+        # check message satisfied any sub-constaint in this Constriant
+
+
 class RegisterTable():
     def __init__(self):
-        self._msg_request_handle_dict = {}
-                                        {idx: {HandlerKey.REMAIN: {(env, type): num},
-                                               HandlerKey.OPERATION: 'AND'/'OR'
-                                               HandlerKey.MSG_LST: [],
-                                               HandlerKey.HANDLER_FN: ..}}
-        self._message_cache = []
+        self._handler_constraint_dict = {handler_fn: constraint class}        
 
-    def add_constraint(self, constraint: tuple(env, msg_type, num) or list of tuple, 
-                        handler_fn: callable, operation: AND or OR, only available on list constraint):
-        constraint_index = self._generate_dict_index(constraint)
-        self._msg_request_handle_dict[constraint_index] = ...
-
-    def _generate_dict_index(self, constraint):
-        return string -> "env+type+num ..."
-
-    def _init_msg_request(self, constraint_index, handler_fn, operation):
-        ...
-
-    def _check_msg_satisfied(self, constraint_index):
-        target_constraint = self._msg_request_handle_dict[constraint_index]
-
-        if target_constraint[HandlerKey.OPERATION] == 'AND':
-            for key, num in target_constraint[HandlerKey.REMAIN].items():
-                if num != 0:
-                    return False
-            return True
-        elif target_constraint[HandlerKey.OPERATION] == 'OR':
-            for key, num in target_constraint[HandlerKey.REMAIN].items():
-                if num == 0:
-                    return True
-            return False
-
-    def _msg_in_constraint(self, msg_source, msg_type):
-        idx = str(msg_source) + str(msg_type)
-        satisfied_constraint = []
-
-        for constriant_idx in self._msg_request_handle_dict.keys():
-            if idx in constriant_idx:
-                satisfied_constraint.append((constraint_idx, (msg_source, msg_type)))
-            # other situation 
-            idx = str(source) + str(ConstraintType.ANYTYPE)
-
-        return satisfied_constraint
-
-    def _message_trigger(self, message):
-        satisfied_handler_fn = []
-        satisfied_constraint = self._msg_in_constraint(message.source, message.type)
-        message_spend = False
-
-        for constraint_index, single_constraint in satisfied_constraint:
-            remain_num = self._msg_request_handle_dict[constraint_index][HandlerKey.REMAIN][single_constraint]
-            if remain_num != 0:
-                self._msg_request_handle_dict[constraint_index][HandlerKey.REMAIN][single_constraint] -= 1
-                self._msg_request_handle_dict[constraint_index][HandlerKey.MSG_LST].append(message)
-                message_spend = True
-
-            if self._check_msg_satisfied(constraint_index):
-                handler_fn = self._msg_request_handle_dict[constraint_index][HandlerKey.HANDLER_FN]
-                msg_lst = self._msg_request_handle_dict[constraint_index][HandlerKey.MSG_LST][:]
-                satisfied_handler_fn.append((handler_fn, msg_lst))
-                self._init_msg_request(constraint_index, handler_fn)
-            
-        if not message_spend:
-            self._message_cache.append(message)
-
-        return satisfied_handler_fn
+    def add_constraint(self, constraint: class, handler_fn: callable):
+        self._msg_request_handle_dict[handler_fn] = constraint
 
     def message_trigger(self, message):
         satisfied_handler_fn = []
 
-        pending_message = self._message_cache[:]
-        pending_message.append(message)
-        self._message_cache = []
+        for handler_fn, constraint in self._handler_constraint_dict.items():
+            msg_lst = constraint.constraint_satisfied(message)
 
-        for msg in pending_message:
-            satisfied_handler_fn.append(self._message_trigger(msg))
+            if msg_lst:    
+                satisfied_handler_fn.append((handler_fn, msg_lst))
         
         return satisfied_handler_fn
 
     def handler_fn(self):
-        return handler_fn_list
+        return self._handler_constraint_dict.keys()
 
 
 if __name__ == '__main__':
