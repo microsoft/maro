e5b14a860e47287f4fe76530db81279f05891894
kaiqli <v-kaiqli@microsoft.com>
2020-06-07 07:58:36 +0000

    registry table version 1

--- maro/distributed/dist_decorator.py
+++ maro/distributed/dist_decorator.py
@@ -7,58 +7,7 @@ from typing import Callable
 from functools import partial
 from copy import deepcopy
 from maro.distributed.message import Message, HandlerKey, ConstraintType
-
-
-class HandlerTrigger():
-    def __init__(self, handler_dict):
-        self._msg_request_handle_list = []
-        for info in handler_dict:
-            # CONSTRAINT class
-            # remain -> count
-            self._msg_request_handle_list.append({HandlerKey.CONSTRAINT: info[HandlerKey.CONSTRAINT],
-                                                  HandlerKey.REMAIN: deepcopy(info[HandlerKey.CONSTRAINT]), 
-                                                  HandlerKey.MSG_LIST: [],
-                                                  HandlerKey.HANDLER_FN: info[HandlerKey.HANDLER_FN]})
-        self._message_cache = []
-
-    def _message_trigger(self, message):
-        message_spend = False
-
-        for req_dict in self._msg_request_handle_list:
-            for constraint, num in list(req_dict[HandlerKey.REMAIN].items()):
-                component_name = message.source[:message.source.rindex("_")]
-                if constraint == (message.source, message.type) or constraint == (ConstraintType.ANY_SOURCE, message.type) or \
-                   constraint == (message.source, ConstraintType.ANY_TYPE) or constraint == (component_name, message.type):
-                    if num > 1:
-                        req_dict[HandlerKey.REMAIN].update({constraint: num - 1})
-                    else:
-                        del req_dict[HandlerKey.REMAIN][constraint]
-                
-                    req_dict[HandlerKey.MSG_LIST].append(message)
-                    message_spend = True
-
-                if not req_dict[HandlerKey.REMAIN]:
-                    request_msg_list = req_dict[HandlerKey.MSG_LIST][:]
-                    req_dict[HandlerKey.MSG_LIST] = []
-                    req_dict[HandlerKey.REMAIN] = deepcopy(req_dict[HandlerKey.CONSTRAINT])
-                    self._satisfied_constraint.append((req_dict[HandlerKey.HANDLER_FN], request_msg_list))
-        
-        if not message_spend:
-            return message
-
-    def message_trigger(self, message):
-        self._message_cache.append(message)
-        self._satisfied_constraint=[]
-        unused_message_list = []
-        
-        for msg in self._message_cache:
-            ret_msg = self._message_trigger(msg)
-            if ret_msg:
-                unused_message_list.append(ret_msg)
-        
-        self._message_cache = unused_message_list[:]
-
-        return self._satisfied_constraint
+from maro.distributed.registry_table import RegisterTable
         
 
 def dist(proxy: Proxy, handler_dict: {object: Callable}):
@@ -76,13 +25,13 @@ def dist(proxy: Proxy, handler_dict: {object: Callable}):
             def __init__(self, *args, **kwargs):
                 self.local_instance = cls(*args, **kwargs)
                 self.proxy = proxy
-                self._msg_request_handle_list = []
                 self._handler_function = {}
+                self._registry_table = RegisterTable(self.proxy.peers)
                 # use functools.partial to freeze handling function's local_instance and proxy
                 # arguments to self.local_instance and self.proxy
-                for info in handler_dict:
-                    self._handler_function[info[HandlerKey.HANDLER_FN]] = partial(info[HandlerKey.HANDLER_FN], self.local_instance, self.proxy)
-                self._handler_trigger = HandlerTrigger(handler_dict)
+                for handler_fn, constraint in handler_dict.items():
+                    self._handler_function[handler_fn] = partial(handler_fn, self.local_instance, self.proxy)
+                    self._registry_table.register_constraint(constraint, handler_fn)
 
             def __getattr__(self, name):
                 if name in self.__dict__:
@@ -96,7 +45,7 @@ def dist(proxy: Proxy, handler_dict: {object: Callable}):
                 """
                 self.proxy.join()
                 for msg in self.proxy.receive():
-                    satisfied_constraint = self._handler_trigger.message_trigger(msg)  
+                    satisfied_constraint = self._registry_table.push(msg)
 
                     for handler_fn, msg_lst in satisfied_constraint:
                         self._handler_function[handler_fn](msg_lst)         
--- maro/distributed/registry_table.py
+++ maro/distributed/registry_table.py
@@ -1,22 +1,56 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
 
-# subconstraint = 'source:type:num'
+import itertools
+import numpy as np
+
+from enum import Enum
+from maro.distributed.message import Message
+
+
+class Operation(Enum):
+    AND = 0
+    OR = 1
+
+
+class LogicTree():
+    def __init__(self, value=None, parent=None, child=None):
+        self.value = value
+        self.child = child if child else []
+
+    def show(self):
+        print(self.value)
+        queue = self.child[:]
+        idx = 0
+        while idx < len(queue):
+            curr_q = queue[idx]
+            if isinstance(curr_q, LogicTree):
+                for item in curr_q.child:
+                    queue.append(item)
+                print(curr_q.value)
+            else:
+                print(curr_q)
+            idx += 1
+        
 
 class Constraint():
-    def __init__(self, constraint: str (subconstriant or (subconstriant, subconstraint, operation))):
-         # decode constraint
+    def __init__(self, constraint: str, peer_list=None):
         self._subconstraint_dict = {} # {subconstraint: msg list}
-        self._logic_relation = [[subconstraint, subconstraint, ...], [subconstraint, subconstraint, ...]]
-        constraint = constraint.replace(' ', '')
+        self._logic_tree = LogicTree()
+        self._peer_list = peer_list
 
-    def _constraint_type(self, constraint):
-        cparen_position = constraint.rfind(')')
+        # regularize constraint input
+        constraint = constraint.replace(' ', '')
+        self._init_constraint(constraint)
+
+    def _init_constraint(self, constraint):
+        cparen_position = constraint.rfind(',')
         if cparen_position == -1:
             self._check_subconstraint_expression(constraint)
             self._subconstraint_dict[constraint] = []
-            self._logic_relation.append([constraint])
+            self._logic_tree.value = constraint
         else:
-            oparen_position = constraint[:cparen_position].find('(')
-            self._constraint_decomposer(constraint[oparen_position+1:cparen_position])
+            self._constraint_decomposer(constraint)
 
     def _check_subconstraint_expression(self, sub_constraint):
         decomposer = sub_constraint.split(':')
@@ -28,69 +62,219 @@ class Constraint():
         try:
             number = int(decomposer[-1])
         except:
-            print(f"number must be int or percentage with % in the end.")
+            raise ValueError(f"number in subconstraint must be int or percentage with % in the end.")
+    
+    def _logic_tree_generator(self, constraint, childtree=None):
+        operation_and_list = ['&&', 'AND']
+        operation_or_list = ['||', 'OR']
+        temp_logictree = LogicTree()
+        
+        decomposer = constraint.split(',')
+        for subc in decomposer:
+            if subc in operation_and_list:
+                temp_logictree.value = Operation.AND
+            elif subc in operation_or_list:
+                temp_logictree.value = Operation.OR
+            elif subc != '#'*len(subc):
+                self._check_subconstraint_expression(subc)
+                self._subconstraint_dict[subc] = []
+                temp_logictree.child.append(subc)
+        
+        if childtree:
+            for tree in childtree:
+                temp_logictree.child.append(tree)
+
+        return temp_logictree
     
     def _constraint_decomposer(self, constraint):
-        operation_list = ['&&', '||', 'AND', 'OR']
-        # clear constraint
-        constraint = constraint.replace('(', '')
-        constraint = constraint.replace(')', '')
-
-        constraint_decomposer = constraint.split(',')
-        op_index = []
-        # check expression
-        for idx, component in enumerate(constraint_decomposer):
-            if component in operation_list:
-                op_index.append(idx)
+        tree_idx_dict = {}
+        while True:
+            lparen_idx = constraint.find(')')
+            rparen_idx = constraint[:lparen_idx].rfind('(')
+            temp_tree_list = []
+            for tree_idx in list(tree_idx_dict.keys()):
+                if tree_idx > rparen_idx:
+                    temp_tree_list.append(tree_idx_dict[tree_idx])
+                    del tree_idx_dict[tree_idx]
+            
+            if rparen_idx == 0:
+                self._logic_tree = self._logic_tree_generator(constraint[rparen_idx+1:lparen_idx], temp_tree_list)
+                break
             else:
-                self._check_subconstraint_expression(component)
+                newtree = self._logic_tree_generator(constraint[rparen_idx+1:lparen_idx], temp_tree_list)
+
+            tree_idx_dict[rparen_idx] = newtree
+            # replace used constraint by #
+            constraint = constraint.replace(constraint[rparen_idx:lparen_idx+1], '#'*(lparen_idx+1-rparen_idx))
+
+    def _get_target_component_number(self, source):
+        target_source_group_number = 0
+        for peer in self._peer_list:
+            if source in peer:
+                target_source_group_number += 1
         
-        used_component_index = []
-        for op_i in op_index:
-            used_component_index.append(op_i)
-            combined_constraint = False
-            curr_index = op_i - 1
-            while curr_index in used_component_index:
-                curr_index -= 1
-                combined_constraint = True
-            if combined_constraint:
-                self._logic_update(constraint_decomposer[curr_index], constraint_decomposer[op_i])
-                used_component_index.append(curr_index)
-            else:
-                used_component_index.append(curr_index)
-                used_component_index.append(curr_index)
+        if target_source_group_number == 0:
+            raise ValueError(f"There is no target source in peer list!")
 
+        return target_source_group_number
+
+    def _satisfied_subconstraint(self, subconstraint):
+        source, _, num = subconstraint.split(':')
+        if num[-1] == '%':
+            component_group_num = self._get_target_component_number(source)
+            num = np.floor(int(num[:-1])*component_group_num/100)
+
+        if int(num) <= len(self._subconstraint_dict[subconstraint]):
+            return [subconstraint]
+        
+        return []
+
+    def _satisfied_constraint(self, logictree):
+        operation = logictree.value
+        result = []
+        for subc in logictree.child:
+            if isinstance(subc, LogicTree):
+                result.append(self._satisfied_constraint(subc))
+            else:
+                result.append(self._satisfied_subconstraint(subc))
+
+        # remove [] in result
+        for r in result:
+            if operation == Operation.AND and not r:
+                return []
+            if operation == Operation.OR and r:
+                return r
+
+        flatten_result = list(itertools.chain.from_iterable(result))
+        return flatten_result
 
     def _check_constraint_satisfied(self):
-        # check if self satisfied
+        # check if constraint satisfied
+        if isinstance(self._logic_tree.value, Operation):
+            return self._satisfied_constraint(self._logic_tree)
+        else:
+            return self._satisfied_subconstraint(self._logic_tree.value)
 
-    def constraint_satisfied(self, message):
+    def push_msg(self, message):
+        # check if message satisfied any subconstraint and put msg in target msg_list
+        message_source, message_type = message.source, message.type
+        for subconstraint in self._subconstraint_dict.keys():
+            source, s_type, _ = subconstraint.split(':')
+            source_match, type_match = False, False
+            if source == message_source or source == '*' or source in message_source:
+                source_match = True
+            if s_type == message_type or s_type == '*':
+                type_match = True
+
+            if source_match and type_match:
+                self._subconstraint_dict[subconstraint].append(message)
+
+    def pull_msg(self):
+        total_msg_list = []
+        satisfied_subconstraint_list = self._check_constraint_satisfied()
+
+        if satisfied_subconstraint_list:
+            if isinstance(satisfied_subconstraint_list, list):
+                for subc in satisfied_subconstraint_list:
+                    source, _, num = subc.split(':')
+                    if num[-1] == '%':
+                        component_group_num = self._get_target_component_number(source)
+                        num = np.floor(int(num[:-1])*component_group_num/100)
+                    num = int(num)
+                    total_msg_list.append(self._subconstraint_dict[subc][:num])
+                    del self._subconstraint_dict[subc][:num]
+            else:
+                num = int(satisfied_subconstraint_list.split(':')[2])
+                total_msg_list.append(self._subconstraint_dict[satisfied_subconstraint_list][:num])
+                del self._subconstraint_dict[satisfied_subconstraint_list][:num]
+        
+        # flatten
+            total_msg_list = list(itertools.chain.from_iterable(total_msg_list))
+
+        return total_msg_list
+
+    def auto_satisfied(self, message):
         # check message satisfied any sub-constaint in this Constriant
+        self.push_msg(message)
+        msg_list = self.pull_msg()
+
+        return msg_list
+
+    def update_peers(self, peers):
+        self._peer_list = peers
 
 
 class RegisterTable():
-    def __init__(self):
-        self._handler_constraint_dict = {handler_fn: constraint class}        
+    def __init__(self, peer_list):
+        self._handler_constraint_dict = {}
+        self._peer_list = peer_list
+    def register_constraint(self, constraint: str, handler_fn: callable):
+        constraint_class = Constraint(constraint, self._peer_list)
+        self._handler_constraint_dict[handler_fn] = constraint_class
+        # constraint_class._logic_tree.show()
 
-    def add_constraint(self, constraint: class, handler_fn: callable):
-        self._msg_request_handle_dict[handler_fn] = constraint
-
-    def message_trigger(self, message):
+    def push(self, message):
         satisfied_handler_fn = []
 
         for handler_fn, constraint in self._handler_constraint_dict.items():
-            msg_lst = constraint.constraint_satisfied(message)
+            msg_lst = constraint.auto_satisfied(message)
 
             if msg_lst:    
                 satisfied_handler_fn.append((handler_fn, msg_lst))
         
         return satisfied_handler_fn
 
-    def handler_fn(self):
-        return self._handler_constraint_dict.keys()
+    def update_peers(self, peers):
+        for constraint in self._handler_constraint_dict.values():
+            constraint.update_peers(peers)
 
 
 if __name__ == '__main__':
-    n = RegisterTable()
-    n.add_constraint([(env1, 'experience', 1), (env2, 'checkout', 1)], training)
-    n.add_constraint((env1, 'experience', 1), training)
+    # subconstraints
+    subc1 = 'env1:experience:1'
+    subc2 = 'env2:experience:1'
+    subc3 = 'env3:experience:1'
+    subc4 = 'env4:experience:1'
+    subc5 = 'env5:experience:1'
+    subc6 = 'env:experience:40%'
+    subc7 = '*:experience:1'
+    subc8 = 'env5:*:1'
+    subc9 = 'env:experience:3'
+    
+    
+    # constraints
+    cons1 = f"({subc1}, {subc2}, {subc3}, AND)"
+    cons2 = f"({subc1}, {subc2}, {subc3}, OR)"
+    cons3 = f"(({subc1}, {subc2}, OR), {subc4}, AND)"
+    cons4 = f"(({subc3}, {subc4}, AND), ({subc1}, {subc2}, AND), {subc5}, AND)"
+    cons5 = f"((({subc1}, {subc2}, OR), {subc6}, AND), {subc4}, AND)"
+
+    # messages
+    message1 = Message(type='experience', source='env1', destination='l1')
+    message2 = Message(type='experience', source='env2', destination='l1')
+    message3 = Message(type='experience', source='env3', destination='l1')
+    message4 = Message(type='experience', source='env4', destination='l1')
+    message5 = Message(type='experience', source='env5', destination='l1')
+    message6 = Message(type='check_out', source='env5', destination='l1')
+
+    message_pool = [message1,message2,message3,message4,message5,message6]
+    
+    # registerTabel
+    n = RegisterTable(['env1', 'env2', 'env3', 'env4', 'env5'])
+    n.register_constraint(subc1, 'testsubfn1')
+    n.register_constraint(subc6, 'testsubfn6')
+    n.register_constraint(subc7, 'testsubfn7')
+    n.register_constraint(subc8, 'testsubfn8')
+    n.register_constraint(cons1, 'testconsfn1')
+    n.register_constraint(cons2, 'testconsfn2')
+    n.register_constraint(cons3, 'testconsfn3')
+    n.register_constraint(cons4, 'testconsfn4')
+    n.register_constraint(cons5, 'testconsfn5')
+
+    for msg in message_pool:
+        print(n.push(msg))
+
+    # * --> anysource/ anyType
+    # env -> any env source
+    # regular -> env_1:training:3
+    # percentage -> env_1:training:80%
