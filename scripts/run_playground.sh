5efc96d6d60a5bbc1701fa98edf16a5e78ce52c9
kaiqli <59279714+kaiqli@users.noreply.github.com>
2020-09-22 15:06:41 +0800

    fix issue during building playground images (#44)

--- scripts/run_playground.sh
+++ scripts/run_playground.sh
@@ -2,8 +2,8 @@
 
 # script to start playground environment within docker container
 
-./redis-6.0.6/src/redis-server -p 6379 &
-redis-commander -p 40009 &
+./redis-6.0.6/src/redis-server --port 6379 &
+redis-commander --port 40009 &
 
 # Python 3.6
 cd ./docs/_build/html; python -m http.server 40010 -b 0.0.0.0 &
