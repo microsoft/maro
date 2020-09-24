3441ac238a03afbf851f43f1edfc871909d1836f
ysqyang <v-yangqi@microsoft.com>
2020-09-24 15:36:57 +0800

    fixed a bug

--- examples/cim/dqn/multi_process_launcher.py
+++ examples/cim/dqn/multi_process_launcher.py
@@ -13,8 +13,8 @@ from components.config import config
 ACTOR_NUM = config.distributed.learner.peer["actor_worker"]  # must be same as in config
 LEARNER_NUM = config.distributed.actor.peer["actor"]
 
-learner_path = f"{os.path.split(os.path.realpath(__file__))[0]}/components/dist_learner.py &"
-actor_path = f"{os.path.split(os.path.realpath(__file__))[0]}/components/dist_actor.py &"
+learner_path = f"{os.path.split(os.path.realpath(__file__))[0]}/dist_learner.py &"
+actor_path = f"{os.path.split(os.path.realpath(__file__))[0]}/dist_actor.py &"
 
 for l_num in range(LEARNER_NUM):
     os.system(f"python " + learner_path)
