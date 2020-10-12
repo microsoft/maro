b50e686f85d3297026dff8d5261d30b858f01789
Jinyu Wang <Wang.Jinyu@microsoft.com>
2020-10-12 09:40:36 +0000

    code format updated

--- maro/simulator/scenarios/citi_bike/business_engine.py
+++ maro/simulator/scenarios/citi_bike/business_engine.py
@@ -36,9 +36,7 @@ Citi bike metrics used to provide statistics information at current point (may b
 It contains following keys:
 
 trip_requirements (int): Accumulative trips until now.
-
 bike_shortage (int): Accumulative shortage until now.
-
 operation_number (int): Accumulative operation cost until now.
 """
 
@@ -53,16 +51,16 @@ class CitibikeBusinessEngine(AbsBusinessEngine):
             snapshot_resolution, max_snapshots, additional_options
         )
 
-        # trip binary reader
+        # Trip binary reader.
         self._trip_reader: BinaryReader = None
 
-        # update self._config_path with current file path
+        # Update self._config_path with current file path.
         self.update_config_root_path(__file__)
 
-        # holidays for US, as we are using NY data
+        # Holidays for US, as we are using NY data.
         self._us_holidays = holidays.US()
 
-        # our stations list used for quick accessing
+        # Our stations list used for quick accessing.
         self._stations: List[Station] = []
 
         self._total_trips: int = 0
@@ -92,29 +90,29 @@ class CitibikeBusinessEngine(AbsBusinessEngine):
         Args:
             tick (int): Current tick to process.
         """
-        # if we do not set auto event, then we need to push it manually
+        # If we do not set auto event, then we need to push it manually.
         for trip in self._item_picker.items(tick):
-            # generate a trip event, to dispatch to related callback to process this requirement
+            # Generate a trip event, to dispatch to related callback to process this requirement.
             trip_evt = self._event_buffer.gen_atom_event(tick, CitiBikeEvents.RequireBike, payload=trip)
 
             self._event_buffer.insert_event(trip_evt)
 
         if self._decision_strategy.is_decision_tick(tick):
-            # generate an event, so that we can do the checking after all the trip requirement processed
+            # Generate an event, so that we can do the checking after all the trip requirement processed.
             decition_checking_evt = self._event_buffer.gen_atom_event(tick, CitiBikeEvents.RebalanceBike)
 
             self._event_buffer.insert_event(decition_checking_evt)
 
-        # update our additional features that not trip related
+        # Update our additional features that not trip related.
         self._update_station_extra_features(tick)
 
     def post_step(self, tick: int):
-        # we following the snapshot_resolution settings to take snapshot
+        # We following the snapshot_resolution settings to take snapshot.
         if (tick + 1) % self._snapshot_resolution == 0:
-            # NOTE: we should use frame_index method to get correct index in snapshot list
+            # NOTE: We should use frame_index method to get correct index in snapshot list.
             self._frame.take_snapshot(self.frame_index(tick))
 
-            # we reset the station station each resolution
+            # We reset the station station each resolution.
             for station in self._stations:
                 station.shortage = 0
                 station.trip_requirement = 0
@@ -124,7 +122,7 @@ class CitibikeBusinessEngine(AbsBusinessEngine):
                 station.failed_return = 0
                 station.min_bikes = station.bikes
 
-        # stop current episode if we reach max tick
+        # Stop current episode if we reach max tick.
         return tick + 1 == self._max_tick
 
     def get_node_mapping(self) -> dict:
@@ -185,12 +183,12 @@ class CitibikeBusinessEngine(AbsBusinessEngine):
         )
 
     def __del__(self):
-        """Collect resource by order"""
+        """Collect resource by order."""
 
         self._item_picker = None
 
         if self._trip_reader:
-            # close binary reader first, so that we can clean it correctly
+            # Close binary reader first, so that we can clean it correctly.
             self._trip_reader.close()
 
     def _init(self):
@@ -198,10 +196,10 @@ class CitibikeBusinessEngine(AbsBusinessEngine):
         self._register_events()
         self._citi_bike_data_pipeline = None
 
-        # time zone we used to transfer UTC to target time zone
+        # Time zone we used to transfer UTC to target time zone.
         self._time_zone = gettz(self._conf["time_zone"])
 
-        # our weather table used to query weather by date
+        # Our weather table used to query weather by date.
         weather_data_path = self._conf["weather_data"]
         if weather_data_path.startswith("~"):
             weather_data_path = os.path.expanduser(weather_data_path)
@@ -217,19 +215,19 @@ class CitibikeBusinessEngine(AbsBusinessEngine):
 
         self._trip_reader = BinaryReader(self._conf["trip_data"])
 
-        # we keep this used to calculate real datetime to get weather and holiday info
+        # We keep this used to calculate real datetime to get weather and holiday info.
         self._trip_start_date: datetime.datetime = self._trip_reader.start_datetime
 
-        # since binary data hold UTC timestamp, convert it into our target timezone
+        # Since binary data hold UTC timestamp, convert it into our target timezone.
         self._trip_start_date = self._trip_start_date.astimezone(self._time_zone)
 
-        # used to cache last date we updated the station additional features to avoid to much time updating
+        # Used to cache last date we updated the station additional features to avoid to much time updating.
         self._last_date: datetime.datetime = None
 
-        # filter data with tick range by minute (time_unit='m')
+        # Filter data with tick range by minute (time_unit='m').
         self._item_picker = self._trip_reader.items_tick_picker(self._start_tick, self._max_tick, time_unit="m")
 
-        # we use this to initializing frame and stations states
+        # We use this to initializing frame and stations states.
         stations_states = get_station_info(self._conf["stations_init_data"])
 
         self._init_frame(len(stations_states))
@@ -238,7 +236,7 @@ class CitibikeBusinessEngine(AbsBusinessEngine):
 
         self._init_adj_matrix()
 
-        # our decision strategy to determine when we need an action
+        # Our decision strategy to determine when we need an action.
         self._decision_strategy = BikeDecisionStrategy(
             self._stations, self._distance_adj, self._snapshots, self._conf["decision"])
 
@@ -248,36 +246,34 @@ class CitibikeBusinessEngine(AbsBusinessEngine):
             self._conf = safe_load(fp)
 
     def _init_stations(self, stations_states: list):
-        # after frame initializing, it will help us create the station instances, let's create a reference
-        # the attribute is added by frame that as same defined in frame definition
+        # After frame initializing, it will help us create the station instances, let's create a reference.
+        # The attribute is added by frame that as same defined in frame definition.
 
-        # NOTE: this is the build in station list that index start from 0,
-        # we need to create a mapping for it, as our trip data only contains id
+        # NOTE: Tthis is the build in station list that index start from 0,
+        # we need to create a mapping for it, as our trip data only contains id.
         self._stations = self._frame.stations
 
         for state in stations_states:
-            # get related station, and set the init states
+            # Get related station, and set the init states.
             station = self._stations[state.index]
 
             station.set_init_state(state.bikes, state.capacity, state.id)
 
     def _init_adj_matrix(self):
-        # our distance adj
-        # we assuming that our adj is NxN without header
+        # Our distance adj. Assume that the adj is NxN without header.
         distance_adj = np.array(load_adj_from_csv(self._conf["distance_adj_data"], skiprows=1))
 
-        # we only have one node here
+        # We only have one node here.
         self._matrices_node = self._frame.matrices[0]
 
         station_num = len(self._stations)
 
         self._distance_adj = distance_adj.reshape(station_num, station_num)
 
-        # add wrapper to it to make it easy to use,
-        # with this we can get value by:
-        # 1. self._trips_adj[x, y]
-        # 2. self._trips_adj.get_row(0)
-        # 3. self._trips_adj.get_column(0)
+        # Add wrapper to it to make it easy to use, with this we can get value by:
+        # 1. self._trips_adj[x, y].
+        # 2. self._trips_adj.get_row(0).
+        # 3. self._trips_adj.get_column(0).
         self._trips_adj = MatrixAttributeAccessor(self._matrices_node, "trips_adj", station_num, station_num)
 
     def _init_frame(self, station_num: int):
@@ -285,22 +281,22 @@ class CitibikeBusinessEngine(AbsBusinessEngine):
         self._snapshots = self._frame.snapshots
 
     def _register_events(self):
-        # register our own events and their callback handlers
+        # Register our own events and their callback handlers.
         self._event_buffer.register_event_handler(CitiBikeEvents.RequireBike, self._on_required_bike)
         self._event_buffer.register_event_handler(CitiBikeEvents.ReturnBike, self._on_bike_returned)
         self._event_buffer.register_event_handler(CitiBikeEvents.RebalanceBike, self._on_rebalance_bikes)
         self._event_buffer.register_event_handler(CitiBikeEvents.DeliverBike, self._on_bike_deliver)
 
-        # decision event, predefined in event buffer
+        # Decision event, predefined in event buffer.
         self._event_buffer.register_event_handler(DECISION_EVENT, self._on_action_received)
 
     def _tick_2_date(self, tick: int):
-        # get current date to update additional info
-        # NOTE: we do not need hour and minutes for now
+        # Get current date to update additional info.
+        # NOTE: We do not need hour and minutes for now.
         return (self._trip_start_date + relativedelta(minutes=tick)).date()
 
     def _update_station_extra_features(self, tick: int):
-        """update features that not related to trips"""
+        """Update features that not related to trips."""
         cur_datetime = self._tick_2_date(tick)
 
         if self._last_date == cur_datetime:
@@ -313,7 +309,7 @@ class CitibikeBusinessEngine(AbsBusinessEngine):
         weekday = cur_datetime.weekday()
         holiday = cur_datetime in self._us_holidays
 
-        # default weather and temperature
+        # Default weather and temperature.
         weather = 0
         temperature = 0
 
@@ -328,17 +324,17 @@ class CitibikeBusinessEngine(AbsBusinessEngine):
             station.temperature = temperature
 
     def _on_required_bike(self, evt: Event):
-        """callback when there is a trip requirement generated"""
+        """Callback when there is a trip requirement generated."""
 
         trip = evt.payload
         station_idx: int = trip.src_station
         station: Station = self._stations[station_idx]
         station_bikes = station.bikes
 
-        # update trip count, each item only contains 1 requirement
+        # Update trip count, each item only contains 1 requirement.
         station.trip_requirement += 1
 
-        # statistics for metrics
+        # Statistics for metrics.
         self._total_trips += 1
 
         self._trips_adj[station_idx, trip.dest_station] += 1
@@ -350,19 +346,20 @@ class CitibikeBusinessEngine(AbsBusinessEngine):
             station.fulfillment += 1
             station.bikes = station_bikes - 1
 
-            # generate a bike return event by end tick
+            # Generate a bike return event by end tick.
             return_payload = BikeReturnPayload(station_idx, trip.dest_station, 1)
 
-            # durations from csv file is in seconds, convert it into minutes
+            # Durations from csv file is in seconds, convert it into minutes.
             return_tick = evt.tick + trip.durations
 
-            bike_return_evt = self._event_buffer.gen_atom_event(return_tick,
-                                                                CitiBikeEvents.ReturnBike, payload=return_payload)
+            bike_return_evt = self._event_buffer.gen_atom_event(
+                return_tick, CitiBikeEvents.ReturnBike, payload=return_payload
+            )
 
             self._event_buffer.insert_event(bike_return_evt)
 
     def _on_bike_returned(self, evt: Event):
-        """callback when there is a bike returned to a station"""
+        """Callback when there is a bike returned to a station."""
         payload: BikeReturnPayload = evt.payload
 
         station: Station = self._stations[payload.to_station_idx]
@@ -381,19 +378,19 @@ class CitibikeBusinessEngine(AbsBusinessEngine):
 
             station.failed_return += additional_bikes
 
-            # we have to move additional bikes to neighbors
+            # We have to move additional bikes to neighbors.
             self._decision_strategy.move_to_neighbor(src_station, station, additional_bikes)
 
         station.bikes = station_bikes + max_accept_number
 
     def _on_rebalance_bikes(self, evt: Event):
-        """callback when need to check if we should send decision event to agent"""
+        """Callback when need to check if we should send decision event to agent."""
 
-        # get stations that need an action
+        # Get stations that need an action.
         stations_need_decision = self._decision_strategy.get_stations_need_decision(evt.tick)
 
         if len(stations_need_decision) > 0:
-            # generate a decision event
+            # Generate a decision event.
             for station_idx, decision_type in stations_need_decision:
                 decision_payload = DecisionEvent(
                     station_idx, evt.tick,
@@ -406,7 +403,7 @@ class CitibikeBusinessEngine(AbsBusinessEngine):
                 self._event_buffer.insert_event(decision_evt)
 
     def _on_bike_deliver(self, evt: Event):
-        """callback when our transferred bikes reach the destination"""
+        """Callback when our transferred bikes reach the destination."""
         payload: BikeTransferPayload = evt.payload
         station: Station = self._stations[payload.to_station_idx]
 
@@ -429,7 +426,7 @@ class CitibikeBusinessEngine(AbsBusinessEngine):
         station.bikes = station_bikes + max_accept_number
 
     def _on_action_received(self, evt: Event):
-        """callback when we get an action from agent"""
+        """Callback when we get an action from agent."""
         action: Action = None
 
         if evt is None or evt.payload is None:
@@ -439,7 +436,7 @@ class CitibikeBusinessEngine(AbsBusinessEngine):
             from_station_idx: int = action.from_station_idx
             to_station_idx: int = action.to_station_idx
 
-            # ignore invalid cell idx
+            # Ignore invalid cell idx.
             if from_station_idx < 0 or to_station_idx < 0:
                 continue
 
@@ -448,7 +445,7 @@ class CitibikeBusinessEngine(AbsBusinessEngine):
 
             executed_number = min(station_bikes, action.number)
 
-            # insert into event buffer if we have bikes to transfer
+            # Insert into event buffer if we have bikes to transfer.
             if executed_number > 0:
                 station.bikes = station_bikes - executed_number
 
@@ -462,15 +459,16 @@ class CitibikeBusinessEngine(AbsBusinessEngine):
                 self._event_buffer.insert_event(transfer_evt)
 
     def _build_temp_data(self):
-        """build temporary data for predefined environment"""
+        """Build temporary data for predefined environment."""
         logger.warning_yellow(f"Binary data files for scenario: citi_bike topology: {self._topology} not found.")
         citi_bike_process = CitiBikeProcess(is_temp=True)
         if self._topology in citi_bike_process.topologies:
             pid = str(os.getpid())
             logger.warning_yellow(
-                f"Generating temp binary data file for scenario: citi_bike topology: {self._topology} pid: {pid}. \
-                If you want to keep the data, please use MARO CLI command \
-                'maro env data generate -s citi_bike -t {self._topology}' to generate the binary data files first.")
+                f"Generating temp binary data file for scenario: citi_bike topology: {self._topology} pid: {pid}. "
+                "If you want to keep the data, please use MARO CLI command "
+                f"'maro env data generate -s citi_bike -t {self._topology}' to generate the binary data files first."
+            )
             self._citi_bike_data_pipeline = citi_bike_process.topologies[self._topology]
             self._citi_bike_data_pipeline.download()
             self._citi_bike_data_pipeline.clean()
@@ -484,4 +482,5 @@ class CitibikeBusinessEngine(AbsBusinessEngine):
             self._conf["distance_adj_data"] = chagne_file_path(self._conf["distance_adj_data"], trip_folder)
         else:
             raise CommandError(
-                "generate", f"Can not generate data files for scenario: citi_bike topology: {self._topology}")
+                "generate", f"Can not generate data files for scenario: citi_bike topology: {self._topology}"
+            )
