85cd0274aaf9e78919264df73734da47bbc9e9e3
ysqyang <v-yangqi@microsoft.com>
2020-10-14 15:22:50 +0800

    split pg and ac and added ac with shared layers

--- maro/rl/algorithms/torch/ac.py
+++ maro/rl/algorithms/torch/ac.py
@@ -6,10 +6,9 @@ from typing import Callable
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 from maro.rl.algorithms.abs_algorithm import AbsAlgorithm
-from maro.rl.utils.trajectory_utils import get_k_step_returns, get_lambda_returns
+from maro.rl.utils.trajectory_utils import get_lambda_returns
 
 
 class ActorCriticHyperParameters:
@@ -33,40 +32,36 @@ class ActorCriticHyperParameters:
 
 
 class ActorCritic(AbsAlgorithm):
-    """Actor Critic algorithm.
+    """Actor Critic algorithm with separate policy and value models (no shared layers).
 
-    The Actor-Critic algorithm base on the policy gradient theorem. If no value model is provided and hyper-parameter
-    ``k`` is -1, the algorithm reduces to REINFORCE.
+    The Actor-Critic algorithm base on the policy gradient theorem.
 
     Args:
         policy_model (nn.Module): model for generating actions given states.
+        value_model (nn.Module): model for estimating state values.
+        value_loss_func (Callable): loss function for the value model.
         policy_optimizer_cls: torch optimizer class for the policy model.
         policy_optimizer_params: parameters required for the policy optimizer class.
+        value_optimizer_cls: torch optimizer class for the value model.
+        value_optimizer_params: parameters required for the value optimizer class.
         hyper_params: hyper-parameter set for the AC algorithm.
-        value_model (nn.Module): model for estimating state values. If this is None, the sequences passed to ``train()``
-            are assumed to end with a terminal state with value zero. Defaults to None.
-        value_optimizer_cls: torch optimizer class for the value model. Defaults to None.
-        value_optimizer_params: parameters required for the value optimizer class. Defaults to None.
-        value_loss_func (Callable): loss function for the value model.
     """
 
-    def __init__(self, policy_model: nn.Module, policy_optimizer_cls, policy_optimizer_params,
-                 hyper_params: ActorCriticHyperParameters, value_model: nn.Module = None,
-                 value_optimizer_cls=None, value_optimizer_params=None, value_loss_func: Callable = None):
+    def __init__(self,
+                 policy_model: nn.Module,
+                 value_model: nn.Module,
+                 value_loss_func: Callable,
+                 policy_optimizer_cls,
+                 policy_optimizer_params,
+                 value_optimizer_cls,
+                 value_optimizer_params,
+                 hyper_params: ActorCriticHyperParameters):
         super().__init__()
         self._device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self._policy_model = policy_model.to(self._device)
-        self._policy_optimizer = policy_optimizer_cls(self._policy_model.parameters(), **policy_optimizer_params)
         self._value_model = value_model.to(self._device)
-        if self._value_model is not None:
-            assert value_optimizer_cls is not None and value_optimizer_params is not None, \
-                "value_optimizer_cls and value_optimizer_params should not be None if value model is not None"
-            self._value_optimizer = value_optimizer_cls(self._value_model.parameters(), **value_optimizer_params)
-        else:
-            assert hyper_params.k == -1 and hyper_params.lmda == 1.0, \
-                "if not value model is provided, hyper-parameters k and lmda must be set to defaults of -1 and 1.0, " \
-                "respectively. "
-            self._value_optimizer = None
+        self._policy_optimizer = policy_optimizer_cls(self._policy_model.parameters(), **policy_optimizer_params)
+        self._value_optimizer = value_optimizer_cls(self._value_model.parameters(), **value_optimizer_params)
         self._value_loss_func = value_loss_func
         self._hyper_params = hyper_params
 
@@ -76,39 +71,27 @@ class ActorCritic(AbsAlgorithm):
 
     def choose_action(self, state: np.ndarray, epsilon: float = None):
         state = torch.from_numpy(state).unsqueeze(0).to(self._device)   # (1, state_dim)
-        action_dist = F.softmax(self._policy_model(state), dim=1).squeeze()  # (num_actions,)
+        action_dist = self._policy_model(state).squeeze()  # (num_actions,)
         return np.random.choice(self._hyper_params.num_actions, p=action_dist.numpy())
 
     def train(self, state_sequence: np.ndarray, action_sequence: np.ndarray, reward_sequence: np.ndarray):
         states = torch.from_numpy(state_sequence).to(self._device)   # (N, state_dim)
-
-        if self._value_model is None:
-            state_values = state_values_numpy = None
-        else:
-            state_values = self._value_model(states)
-            state_values_numpy = state_values.numpy()
-
-        if self._hyper_params.lmda is None:
-            returns = get_k_step_returns(reward_sequence, self._hyper_params.reward_decay,
-                                         k=self._hyper_params.k, values=state_values_numpy)
-        else:
-            returns = get_lambda_returns(reward_sequence, self._hyper_params.reward_decay, self._hyper_params.lmda,
-                                         k=self._hyper_params.k, values=state_values_numpy)
+        state_values = self._value_model(states)
+        state_values_numpy = state_values.numpy()
+        returns = get_lambda_returns(reward_sequence, self._hyper_params.reward_decay, self._hyper_params.lmda,
+                                     k=self._hyper_params.k, values=state_values_numpy)
         # policy model training
         actions = torch.from_numpy(action_sequence).to(self._device)  # (N,)
-        action_dist = F.softmax(self._policy_model(states), dim=1)   # (N, num_actions)
-        action_prob = action_dist.gather(1, actions.unsqueeze(1))   # (N, 1)
-        log_action_prob = torch.log(action_prob).squeeze()   # (N,)
-        policy_loss = -(log_action_prob * returns).mean()
+        action_prob = self._policy_model(states).gather(1, actions.unsqueeze(1)).squeeze()  # (N,)
+        policy_loss = -(torch.log(action_prob) * returns).mean()
         self._policy_optimizer.zero_grad()
         policy_loss.backward()
         self._policy_optimizer.step()
-        # value model training (if a value model is present)
-        if self._value_model is not None:
-            value_loss = self._value_loss_func(state_values, returns)
-            self._value_optimizer.zero_grad()
-            value_loss.backward()
-            self._value_optimizer.step()
+        # value model training
+        value_loss = self._value_loss_func(state_values, returns)
+        self._value_optimizer.zero_grad()
+        value_loss.backward()
+        self._value_optimizer.step()
 
     def load_trainable_models(self, model_dict):
         self._policy_model = model_dict["policy"]
@@ -126,3 +109,67 @@ class ActorCritic(AbsAlgorithm):
     def dump_trainable_models_to_file(self, path: str):
         """Dump the algorithm's trainable models to disk."""
         torch.save({"policy": self._policy_model.state_dict(), "value": self._value_model.state_dict()}, path)
+
+
+class ActorCriticWithSharedLayers(AbsAlgorithm):
+    """Actor Critic algorithm where policy and value models have shared layers.
+
+    Args:
+        policy_value_model (nn.Module): model for generating action distributions and values for given states using
+            shared bottom layers. The model, when called, must return (value, action distribution).
+        value_loss_func (Callable): loss function for the value model.
+        optimizer_cls: torch optimizer class for the policy model.
+        optimizer_params: parameters required for the policy optimizer class.
+        hyper_params: hyper-parameter set for the AC algorithm.
+    """
+
+    def __init__(self,
+                 policy_value_model: nn.Module,
+                 value_loss_func: Callable,
+                 optimizer_cls,
+                 optimizer_params,
+                 hyper_params: ActorCriticHyperParameters):
+        super().__init__()
+        self._device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self._policy_value_model = policy_value_model.to(self._device)
+        self._optimizer = optimizer_cls(self._policy_value_model.parameters(), **optimizer_params)
+        self._value_loss_func = value_loss_func
+        self._hyper_params = hyper_params
+
+    @property
+    def model(self):
+        return self._policy_value_model
+
+    def choose_action(self, state: np.ndarray, epsilon: float = None):
+        state = torch.from_numpy(state).unsqueeze(0).to(self._device)   # (1, state_dim)
+        action_dist = self._policy_value_model(state)[1].squeeze()  # (num_actions,)
+        return np.random.choice(self._hyper_params.num_actions, p=action_dist.numpy())
+
+    def train(self, state_sequence: np.ndarray, action_sequence: np.ndarray, reward_sequence: np.ndarray):
+        states = torch.from_numpy(state_sequence).to(self._device)   # (N, state_dim)
+        state_values, action_dist = self._policy_value_model(states)
+        state_values_numpy = state_values.numpy()
+        returns = get_lambda_returns(reward_sequence, self._hyper_params.reward_decay, self._hyper_params.lmda,
+                                     k=self._hyper_params.k, values=state_values_numpy)
+        actions = torch.from_numpy(action_sequence).to(self._device)  # (N,)
+        action_prob = action_dist.gather(1, actions.unsqueeze(1)).squeeze()   # (N,)
+        policy_loss = -(torch.log(action_prob) * returns).mean()
+        value_loss = self._value_loss_func(state_values, returns)
+        loss = policy_loss + value_loss
+        self._optimizer.zero_grad()
+        loss.backward()
+        self._optimizer.step()
+
+    def load_trainable_models(self, policy_value_model):
+        self._policy_value_model = policy_value_model
+
+    def dump_trainable_models(self):
+        return self._policy_value_model
+
+    def load_trainable_models_from_file(self, path):
+        """Load trainable models from disk."""
+        self._policy_value_model = torch.load(path)
+
+    def dump_trainable_models_to_file(self, path: str):
+        """Dump the algorithm's trainable models to disk."""
+        torch.save(self._policy_value_model.state_dict(), path)
---
+++ maro/rl/algorithms/torch/pg.py
@@ -0,0 +1,78 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from maro.rl.algorithms.abs_algorithm import AbsAlgorithm
+from maro.rl.utils.trajectory_utils import get_k_step_returns
+
+
+class PolicyGradientHyperParameters:
+    """Hyper-parameter set for the Actor-Critic algorithm.
+
+    Args:
+        num_actions (int): number of possible actions
+        reward_decay (float): reward decay as defined in standard RL terminology
+    """
+    __slots__ = ["num_actions", "reward_decay"]
+
+    def __init__(self, num_actions: int, reward_decay: float):
+        self.num_actions = num_actions
+        self.reward_decay = reward_decay
+
+
+class PolicyGradient(AbsAlgorithm):
+    """Policy gradient algorithm.
+
+    The policy gradient algorithm base on the policy gradient theorem, a.k.a. REINFORCE.
+
+    Args:
+        policy_model (nn.Module): model for generating actions given states.
+        optimizer_cls: torch optimizer class for the policy model.
+        optimizer_params: parameters required for the policy optimizer class.
+        hyper_params: hyper-parameter set for the AC algorithm.
+    """
+
+    def __init__(self, policy_model: nn.Module, optimizer_cls, optimizer_params,
+                 hyper_params: PolicyGradientHyperParameters):
+        super().__init__()
+        self._device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self._policy_model = policy_model.to(self._device)
+        self._policy_optimizer = optimizer_cls(self._policy_model.parameters(), **optimizer_params)
+        self._hyper_params = hyper_params
+
+    @property
+    def model(self):
+        return self._policy_model
+
+    def choose_action(self, state: np.ndarray, epsilon: float = None):
+        state = torch.from_numpy(state).unsqueeze(0).to(self._device)   # (1, state_dim)
+        action_dist = F.softmax(self._policy_model(state), dim=1).squeeze()  # (num_actions,)
+        return np.random.choice(self._hyper_params.num_actions, p=action_dist.numpy())
+
+    def train(self, state_sequence: np.ndarray, action_sequence: np.ndarray, reward_sequence: np.ndarray):
+        states = torch.from_numpy(state_sequence).to(self._device)   # (N, state_dim)
+        returns = get_k_step_returns(reward_sequence, self._hyper_params.reward_decay)
+        actions = torch.from_numpy(action_sequence).to(self._device)  # (N,)
+        action_prob = self._policy_model(states).gather(1, actions.unsqueeze(1)).squeeze()   # (N, 1)
+        policy_loss = -(torch.log(action_prob) * returns).mean()
+        self._policy_optimizer.zero_grad()
+        policy_loss.backward()
+        self._policy_optimizer.step()
+
+    def load_trainable_models(self, policy_model):
+        self._policy_model = policy_model
+
+    def dump_trainable_models(self):
+        return self._policy_model
+
+    def load_trainable_models_from_file(self, path):
+        """Load trainable models from disk."""
+        self._policy_model = torch.load(path)
+
+    def dump_trainable_models_to_file(self, path: str):
+        """Dump the algorithm's trainable models to disk."""
+        torch.save(self._policy_model.state_dict(), path)
