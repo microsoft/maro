5b866e8bee7495a46f8fae2178a31c934629c671
ysqyang <v-yangqi@microsoft.com>
2020-10-22 17:04:54 +0800

    fixed a bug in the dump_experience_pool method

--- maro/rl/agent/abs_agent.py
+++ maro/rl/agent/abs_agent.py
@@ -108,7 +108,8 @@ class AbsAgent(ABC):
     def dump_experience_pool(self, dir_path: str):
         """Dump the experience pool to disk."""
         if self._experience_pool is not None:
-            with open(os.path.join(dir_path, self._name)) as fp:
+            os.makedirs(dir_path, exist_ok=True)
+            with open(os.path.join(dir_path, self._name), "wb") as fp:
                 pickle.dump(self._experience_pool, fp)
 
     def _assert_train_mode(self):
