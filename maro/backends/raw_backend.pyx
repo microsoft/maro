dc19e704388a893ba0b3b909c8b4c8b659467b51
chaosyu <chaos.you@gmail.com>
2020-10-27 14:14:50 +0800

    add np ignore macro

--- maro/backends/raw_backend.pyx
+++ maro/backends/raw_backend.pyx
@@ -3,6 +3,8 @@
 
 #cython: language_level=3
 #distutils: language = c++
+#distutils: define_macros=NPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION
+
 import numpy as np
 cimport numpy as np
 cimport cython
