7d840776f3452a9225484454eedd3c0bbcd9b8b2
chaosyu <chaos.you@gmail.com>
2020-10-28 10:37:04 +0800

    remove un-used options

--- scripts/compile_cython.sh
+++ scripts/compile_cython.sh
@@ -13,4 +13,4 @@ rm -f ./maro/backends/*.cpp
 
 # compile pyx into .c files
 # use numpy backend, and use a big memory block to hold array
-cython ./maro/backends/backend.pyx ./maro/backends/np_backend.pyx ./maro/backends/raw_backend.pyx ./maro/backends/frame.pyx --cplus -3 -E FRAME_BACKEND=NUMPY,NODES_MEMORY_LAYOUT=ONE_BLOCK -X embedsignature=True
+cython ./maro/backends/backend.pyx ./maro/backends/np_backend.pyx ./maro/backends/raw_backend.pyx ./maro/backends/frame.pyx --cplus -3 -E NODES_MEMORY_LAYOUT=ONE_BLOCK -X embedsignature=True
