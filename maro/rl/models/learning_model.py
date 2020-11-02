a2fef981c2b89ccb1010c0b0f8172a7c1b4068ec
ysqyang <v-yangqi@microsoft.com>
2020-11-03 00:28:39 +0800

    added multi-head functionality to LearningModel

--- maro/rl/models/learning_model.py
+++ maro/rl/models/learning_model.py
@@ -5,14 +5,19 @@ import torch.nn as nn
 
 
 class LearningModel(nn.Module):
-    """NN model that consists of multiple building blocks.
+    """NN model that consists of multiple shared blocks and multiple heads.
 
-    The building blocks must be chainable, i.e., the output dimension of one block must match the input dimension of
+    The shared blocks must be chainable, i.e., the output dimension of one block must match the input dimension of
     its successors.
     """
-    def __init__(self, *blocks):
+    def __init__(self, *blocks, **heads):
         super().__init__()
-        self._net = nn.Sequential(*blocks)
+        self._shared = nn.Sequential(*blocks)
+        self._heads = heads
 
     def forward(self, inputs):
-        return self._net(inputs)
+        if not self._heads:
+            return self._shared(inputs)
+        else:
+            features = self._shared(inputs)
+            return {name: layers(features) for name, layers in self._heads.items()}
