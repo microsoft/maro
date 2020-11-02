b5302928a65007cc9c4a2149d87e7a756c0186d1
ysqyang <v-yangqi@microsoft.com>
2020-11-03 00:33:49 +0800

    refined learning model docstring

--- maro/rl/models/learning_model.py
+++ maro/rl/models/learning_model.py
@@ -7,8 +7,10 @@ import torch.nn as nn
 class LearningModel(nn.Module):
     """NN model that consists of multiple shared blocks and multiple heads.
 
-    The shared blocks must be chainable, i.e., the output dimension of one block must match the input dimension of
-    its successors.
+    The shared blocks must be chainable, i.e., the output dimension of a block must match the input dimension of
+    its successor. Heads must be provided in the form of keyword arguments. If at least one head is provided, the
+    output of the model will be a dictionary with the names of the heads as keys and the corresponding head outputs
+    as values. Otherwise, the output will be the output of the last block.
     """
     def __init__(self, *blocks, **heads):
         super().__init__()
