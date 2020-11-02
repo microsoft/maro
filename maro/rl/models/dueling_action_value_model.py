009793ed758135d678b986bd81a9ef01fc536dc8
ysqyang <v-yangqi@microsoft.com>
2020-11-03 00:13:25 +0800

    mv dueling_actiovalue_model and fixed some bugs

--- maro/rl/models/dueling_action_value_model.py
+++ maro/rl/models/dueling_action_value_model.py
@@ -3,25 +3,23 @@
 
 import torch.nn as nn
 
-from maro.rl.models.torch.learning_model import IdentityLayers
-
 
 class DuelingActionValueModel(nn.Module):
-    def __init__(self, value_head: nn.Module, advantage_head: nn.Module, shared_layers:nn.Module = IdentityLayers(),
-                 advantage_mode: str = 'mean'):
+    def __init__(self, value_head: nn.Module, advantage_head: nn.Module, shared_layers:nn.Module = nn.Identity(),
+                 advantage_mode: str = "mean"):
         super().__init__()
         self._value_head = value_head
         self._advantage_head = advantage_head
         self._shared_layers = shared_layers
-        if self._advantage_mode not in {'mean', 'max'}:
-            raise ValueError('Advantage mode must be "mean" or "max"')
+        if self._advantage_mode not in {"mean", "max"}:
+            raise ValueError("Advantage mode must be 'mean' or 'max'")
         self._advantage_mode = advantage_mode
 
     def forward(self, inputs):
         features = self._shared_layers(inputs)
-        state_values = self._value_layers(features)
-        advantages = self._advantage_layers(features)
+        state_values = self._value_head(features)
+        advantages = self._advantage_head(features)
         # use mean or max correction to address the identifiability issue
-        corrections = advantages.mean(1) if self._advantage_mode == 'mean' else advantages.max(1)[0]
+        corrections = advantages.mean(1) if self._advantage_mode == "mean" else advantages.max(1)[0]
         q_values = state_values + advantages - corrections.unsqueeze(1)
         return q_values
