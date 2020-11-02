637fb08bd525047f44eebe970075a839f6b8a79a
ysqyang <v-yangqi@microsoft.com>
2020-11-02 16:02:51 +0800

    1. added logical operator overloading for early stopping checker; 2. added mean value checker

--- maro/rl/early_stopping/abs_early_stopping_checker.py
+++ maro/rl/early_stopping/abs_early_stopping_checker.py
@@ -24,3 +24,66 @@ class AbsEarlyStoppingChecker(ABC):
             A boolean value indicating whether early stopping should be triggered.
         """
         return NotImplemented
+
+    def __or__(self, other_checker):
+        """Return a checker that is the result of logical OR between itself and another checker.
+
+        The resulting checker returns True iff at least one of the checkers returns True.
+        """
+        class OrChecker(AbsEarlyStoppingChecker):
+            def __init__(self, checker, other):
+                super().__init__()
+                self._checker = checker
+                self._other_checker = other
+
+            def __call__(self, metric_series) -> bool:
+                return self._checker(metric_series) or self._other_checker(metric_series)
+
+        return OrChecker(self, other_checker)
+
+    def __and__(self, other_checker):
+        """Return a checker that is the result of logical AND between itself and another checker.
+
+        The resulting checker returns True iff both checkers return True.
+        """
+        class AndChecker(AbsEarlyStoppingChecker):
+            def __init__(self, checker, other):
+                super().__init__()
+                self._checker = checker
+                self._other_checker = other
+
+            def __call__(self, metric_series) -> bool:
+                return self._checker(metric_series) and self._other_checker(metric_series)
+
+        return AndChecker(self, other_checker)
+
+    def __xor__(self, other_checker):
+        """Return a checker that is the result of logical XOR between itself and another checker.
+
+        The resulting checker returns True iff one checker returns True and the other returns False.
+        """
+        class XorChecker(AbsEarlyStoppingChecker):
+            def __init__(self, checker, other):
+                super().__init__()
+                self._checker = checker
+                self._other_checker = other
+
+            def __call__(self, metric_series) -> bool:
+                return self._checker(metric_series) ^ self._other_checker(metric_series)
+
+        return XorChecker(self, other_checker)
+
+    def __invert__(self):
+        """Return a checker that is the result of logical NOT of itself.
+
+        The resulting checker returns True iff itself returns False.
+        """
+        class NotChecker(AbsEarlyStoppingChecker):
+            def __init__(self, checker):
+                super().__init__()
+                self._checker = checker
+
+            def __call__(self, metric_series) -> bool:
+                return not self._checker(metric_series)
+
+        return NotChecker(self)
--- maro/rl/early_stopping/simple_early_stopping_checker.py
+++ maro/rl/early_stopping/simple_early_stopping_checker.py
@@ -6,13 +6,33 @@ from statistics import mean, stdev
 from .abs_early_stopping_checker import AbsEarlyStoppingChecker
 
 
+class MeanValueChecker(AbsEarlyStoppingChecker):
+    """Early stopping checker based on the mean of the last k metric values.
+
+    Args:
+        last_k (int): Number of the latest metric values to check for early stopping.
+        threshold (float): The threshold value against which the mean of the ``last_k`` metric values is compared.
+            The early stopping condition is hit if the mean is greater than or equal to this threshold.
+    """
+    def __init__(self, last_k, threshold):
+        super().__init__()
+        self._last_k = last_k
+        self._threshold = threshold
+
+    def __call__(self, metric_series: list):
+        if self._last_k > len(metric_series):
+            return False
+        else:
+            return mean(metric_series[-self._last_k:]) >= self._threshold
+
+
 class RSDEarlyStoppingChecker(AbsEarlyStoppingChecker):
     """Early stopping checker based on the mean and standard deviation of the last k metric values.
 
     Args:
-        last_k (int): Number of the latest performance records to check for early stopping.
-        threshold (float): The threshold value against which the early stopping metric is compared. The early stopping
-            condition is satisfied if the metric is below this threshold.
+        last_k (int): Number of the latest metric values to check for early stopping.
+        threshold (float): The threshold value against which the relative standard deviation (RSD) of the ``last_k``
+            metric values is compared. The early stopping condition is hit if the RSD is below this threshold.
     """
     def __init__(self, last_k: int, threshold: float):
         super().__init__()
@@ -28,15 +48,15 @@ class RSDEarlyStoppingChecker(AbsEarlyStoppingChecker):
 
 
 class MaxDeltaEarlyStoppingChecker(AbsEarlyStoppingChecker):
-    """Early stopping checker based on the maximum relative change over the last k metric values.
+    """Early stopping checker based on the maximum relative variation over the last k metric values.
 
     The relative change is defined as |m(i+1) - m(i)| / m[i]. The maximum of the last k-1 changes in the metric series
     is compared with the threshold to determine if early stopping should be triggered.
 
     Args:
-        last_k (int): Number of the latest performance records to check for early stopping.
-        threshold (float): The threshold value against which the early stopping metric is compared. The early stopping
-            condition is satisfied if the metric is below this threshold.
+        last_k (int): Number of the latest metric values to check for early stopping.
+        threshold (float): The threshold value against which the maximum relative variation (MRV) of the ``last_k``
+            metric values is compared. The early stopping condition is hit if the MRV is below this threshold.
     """
     def __init__(self, last_k: int, threshold: float):
         super().__init__()
