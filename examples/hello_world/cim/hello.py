56535c856c54c3e55def1af9cbef4dc0627e427d
Michael Li <mic_lee2000@hotmail.com>
2020-11-04 14:34:42 +0800

    removed path from hello.py

--- examples/hello_world/cim/hello.py
+++ examples/hello_world/cim/hello.py
@@ -13,7 +13,7 @@ enable-dump-snapshot parameter means business_engine needs dump snapshot data be
 If you leave value to empty string, it will dump to current folder.
 For getting dump data, please uncomment below line and specify dump destination folder.
 """
-opts['enable-dump-snapshot'] = '/Users/micl/Documents/dumps'
+# opts['enable-dump-snapshot'] = ''
 
 # Initialize an environment with a specific scenario, related topology.
 env = Env(scenario="cim", topology="toy.5p_ssddd_l0.0",
