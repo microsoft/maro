523da989d1b78938cfb181cfd1b08d23f30c0b7e
Michael Li <mic_lee2000@hotmail.com>
2020-11-04 16:28:04 +0800

    Changed import sort.

--- maro/simulator/scenarios/abs_business_engine.py
+++ maro/simulator/scenarios/abs_business_engine.py
@@ -2,12 +2,13 @@
 # Licensed under the MIT license.
 
 import os
-from pathlib import Path
 from abc import ABC, abstractmethod
+from pathlib import Path
 
-from maro.event_buffer import EventBuffer
 from maro.backends.frame import FrameBase, SnapshotList
+from maro.event_buffer import EventBuffer
 from maro.simulator.utils.common import tick_to_frame_index, total_frames
+
 
 class AbsBusinessEngine(ABC):
     """Abstract class for all the business engine, a business engine is the core part of a scenario,
