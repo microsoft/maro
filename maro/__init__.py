10aed3485a77bc0a1e005a15ea8e98885b93e70b
Jinyu Wang <Wang.Jinyu@microsoft.com>
2020-11-05 03:44:08 +0000

    re-format maro init

--- maro/__init__.py
+++ maro/__init__.py
@@ -1,9 +1,9 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-from maro.utils.utils import check_deployment_status, deploy
-
 from .__misc__ import __data_version__, __version__
+
+from maro.utils.utils import check_deployment_status, deploy
 
 if not check_deployment_status():
     deploy()
