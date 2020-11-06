0ea51a435256e9fc972f4a15a3eb9030b922251d
ysqyang <v-yangqi@microsoft.com>
2020-11-06 10:22:56 +0800

    fixed a bug

--- examples/cim/dqn/single_process_launcher.py
+++ examples/cim/dqn/single_process_launcher.py
@@ -5,18 +5,18 @@ import os
 
 import numpy as np
 
+from components.action_shaper import CIMActionShaper
+from components.agent_manager import DQNAgentManager, create_dqn_agents
+from components.config import set_input_dim
+from omponents.experience_shaper import TruncatedExperienceShaper
+from components.state_shaper import CIMStateShaper
+
 from maro.rl import (
     AgentManagerMode, KStepExperienceShaper, MaxDeltaEarlyStoppingChecker, SimpleActor, SimpleLearner,
     TwoPhaseLinearExplorer
 )
 from maro.simulator import Env
 from maro.utils import Logger, convert_dottable
-
-from .components.action_shaper import CIMActionShaper
-from .components.agent_manager import DQNAgentManager, create_dqn_agents
-from .components.config import set_input_dim
-from .components.experience_shaper import TruncatedExperienceShaper
-from .components.state_shaper import CIMStateShaper
 
 
 def launch(config):
