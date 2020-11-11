4fbc37248d108de67512c997dddf34efca21c0ea
ysqyang <v-yangqi@microsoft.com>
2020-11-11 17:56:24 +0800

    fixed a bug

--- maro/rl/algorithms/task_validator.py
+++ maro/rl/algorithms/task_validator.py
@@ -2,6 +2,7 @@
 # Licensed under the MIT license.
 
 from enum import Enum
+from functools import wraps
 
 from maro.rl.models.learning_model import MultiTaskLearningModel
 from maro.utils.exception.rl_toolkit_exception import UnrecognizedTaskError
@@ -9,13 +10,14 @@ from maro.utils.exception.rl_toolkit_exception import UnrecognizedTaskError
 
 def validate_tasks(task_enum: Enum):
     def decorator(init_func):
-        def wrapper(core_model, config):
+        @wraps(init_func)
+        def wrapper(self, core_model, config):
             recognized_tasks = set(member.value for member in task_enum)
             model_tasks = set(core_model.tasks)
             if isinstance(core_model, MultiTaskLearningModel) and model_tasks != recognized_tasks:
                 raise UnrecognizedTaskError(f"Expected task names {recognized_tasks}, got {model_tasks}")
 
-            init_func(core_model, config)
+            init_func(self, core_model, config)
 
         return wrapper
 
