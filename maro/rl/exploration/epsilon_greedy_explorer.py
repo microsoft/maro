bdd3a7d28376ef314ffec932dbc64201f32f8f98
ysqyang <v-yangqi@microsoft.com>
2020-11-17 15:52:52 +0800

    fixed a bug

--- maro/rl/exploration/epsilon_greedy_explorer.py
+++ maro/rl/exploration/epsilon_greedy_explorer.py
@@ -14,13 +14,12 @@ class EpsilonGreedyExplorer(AbsExplorer):
     """
     def __init__(self, num_actions: int):
         super().__init__()
-        self._epsilon_schedule = None
         self._epsilon = None
         self._num_actions = num_actions
 
     def __call__(self, action):
         assert action < self._num_actions, f"Invalid action: {action}"
-        if self._epsilon_schedule is None or random.random() > self._epsilon:
+        if random.random() > self._epsilon:
             return action
         else:
             return random.randrange(self._num_actions)
