6f4ebbeed4a240a7d7ef6bac82a5c2150e66540a
ysqyang <v-yangqi@microsoft.com>
2020-11-18 19:30:09 +0800

    fixed a bug with k-step return in AC

--- maro/rl/algorithms/ac.py
+++ maro/rl/algorithms/ac.py
@@ -75,9 +75,8 @@ class ActorCritic(AbsAlgorithm):
 
     def _get_values_and_bootstrapped_returns(self, state_sequence, reward_sequence):
         state_values = self._model(state_sequence, task_name="critic").detach().squeeze()
-        state_values_numpy = state_values.numpy()
         return_est = get_lambda_returns(
-            reward_sequence, state_values_numpy, self._config.reward_decay, self._config.lam,
+            reward_sequence, state_values, self._config.reward_decay, self._config.lam,
             k=self._config.k
         )
         return_est = torch.from_numpy(return_est)
@@ -87,7 +86,7 @@ class ActorCritic(AbsAlgorithm):
     def train(self, states: np.ndarray, actions: np.ndarray, rewards: np.ndarray):
         state_values, return_est = self._get_values_and_bootstrapped_returns(states, rewards)
         advantages = return_est - state_values
-        if self._model.has_trainable_shared_layers:
+        if self._model.shared_module is not None and self._model.shared_module.is_trainable:
             pass
         else:
             # policy model training
