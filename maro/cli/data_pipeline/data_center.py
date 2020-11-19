9395f0d45b62b2f84280175e19385fb060325bf9
kyu-kuanwei <v-kyu@microsoft.com>
2020-11-19 16:15:48 +0800

    Temp update

--- maro/cli/data_pipeline/data_center.py
+++ maro/cli/data_pipeline/data_center.py
@@ -1,5 +1,8 @@
+import gzip
 import os
+import shutil
 
+import pandas as pd
 from yaml import safe_load
 
 from maro.cli.data_pipeline.base import DataPipeline, DataTopology
@@ -17,8 +20,15 @@ class DataCenterPipeline(DataPipeline):
 
     _download_file_name = "AzurePublicDatasetLinksV2.txt"
 
+    _vm_table_file_name = "vmtable.csv.gz"
+    _clean_vm_table_file_name = "vmtable.csv"
+
+    _cpu_readings_file_name = "vm_cpu_readings-file-1-of-195.csv.gz"
+
     def __init__(self, topology: str, source: str, is_temp: bool = False):
         super().__init__(scenario="data_center", topology=topology, source=source, is_temp=is_temp)
+
+        self._vm_table_file = os.path.join(self._download_folder, self._vm_table_file_name)
 
     def download(self, is_force: bool = False):
         # Download text with all urls.
@@ -29,15 +39,43 @@ class DataCenterPipeline(DataPipeline):
             with open(self._download_file, mode="r", encoding="utf-8") as urls:
                 for remote_url in urls.read().splitlines():
                     file_name = remote_url.split('/')[-1]
-                    if file_name.startswith("vm"):
-                        vm_data_file = os.path.join(self._download_folder, file_name)
-                        if (not is_force) and os.path.exists(vm_data_file):
+                    if file_name.startswith("vmtable"):
+                        if (not is_force) and os.path.exists(self._vm_table_file):
                             logger.info_green("File already exists, skipping download.")
                         else:
-                            logger.info_green(f"Downloading VM data from {remote_url} to {vm_data_file}.")
-                            download_file(source=remote_url, destination=vm_data_file)
+                            logger.info_green(f"Downloading VM data from {remote_url} to {self._vm_table_file}.")
+                            download_file(source=remote_url, destination=self._vm_table_file)
         else:
             logger.warning(f"Not found downloaded source file: {self._download_file}.")
+
+    def clean(self):
+        """Unzip the csv file and process it for building binary file."""
+        super().clean()
+        logger.info_green("Cleaning VM data.")
+        if os.path.exists(self._vm_table_file):
+            # Unzip gz file.
+            unzip_vm_table_file = os.path.join(self._clean_folder, self._clean_vm_table_file_name)
+            logger.info_green("Unzip start.")
+            with gzip.open(self._vm_table_file, mode="r") as f_in:
+                logger.info_green(
+                    f"Unzip {self._clean_vm_table_file_name} from {self._vm_table_file} to {unzip_vm_table_file}."
+                )
+                with open(unzip_vm_table_file, "w") as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+            logger.info_green("Unzip finished.")
+            # Preprocess vmtable.
+        else:
+            logger.warning(f"Not found downloaded source file: {self._vm_table_file}.")
+
+    def _preprocess(self, unzip_vm_table_file: str):
+        headers = [
+            'vmid', 'subscriptionid', 'deploymentid', 'vmcreated', 'vmdeleted', 'maxcpu', 'avgcpu', 'p95maxcpu',
+            'vmcategory', 'vmcorecountbucket', 'vmmemorybucket'
+        ]
+        vm_table_raw = pd.read_csv(unzip_vm_table_file, header=None, index_col=False, names=headers)
+        required_headers = ['vmid', 'vmcreated', 'vmdeleted', 'vmcorecountbucket', 'vmmemorybucket']
+        vm_table = vm_table_raw[required_headers]
+        vm_table['lifetime'] = vm_table['vmdeleted'] - ['vmcreated']
 
 
 class DataCenterTopology(DataTopology):
