ff17e16dde8c6854ba1311ccce13465adbd2eb6a
chaosyu <chaos.you@gmail.com>
2020-11-26 10:11:25 +0800

    support item tick picker as before

--- maro/data_lib/binary/binaryreader.pyx
+++ maro/data_lib/binary/binaryreader.pyx
@@ -48,7 +48,7 @@ cdef class ULONGAcessor(ItemContainerAccessor):
         return self.item.get[ULONGLONG](self.offset)
 
 
-field_access_map = {
+cdef dict field_access_map = {
     1: ShortAccessor,
     2: IntAccessor,
     3: LongAccessor,
@@ -58,16 +58,88 @@ field_access_map = {
 }
 
 
+cdef unit_seconds(str unit):
+    # default for second
+    cdef UINT seconds = 1
+
+    if unit == "m":
+        seconds = 60
+    elif unit == "h":
+        seconds = 60 * 60
+    elif unit == "d":
+        seconds = 60 * 60 * 24
+
+    return seconds
+
+
+cdef calc_time_offset(ULONGLONG start_time, UINT offset, str unit):
+    """Calculate time by offset and time unit."""
+    cdef UINT seconds_per_unit = unit_seconds(unit)
+
+    return offset * seconds_per_unit + start_time
+
+
+cdef class ItemTickPicker:
+    """Wrapper to support get items by tick."""
+    cdef:
+        object _item_generaotr
+        ULONGLONG _starttime
+        str _time_unit
+        object _cached_item
+
+    def __init__(self, item_generaotr, starttime: int, time_unit: str):
+        self._item_generaotr = item_generaotr
+        self._starttime = starttime
+        self._time_unit = time_unit
+        self._cached_item = None
+
+    def items(self, tick: int):
+        """Get items for specified ticks.
+
+        NOTE:
+            This method will compare timestamp of item to pick.
+        """
+        cdef UINT _tick = tick
+        cdef UINT seconds_per_unit = unit_seconds(self._time_unit)
+        cdef ULONGLONG ticks_in_seconds = self._starttime + _tick * seconds_per_unit
+
+        while True:
+            item = self._cached_item
+
+            # clear the cache
+            self._cached_item = None
+
+            if item is None:
+                try:
+                    item = next(self._item_generaotr)
+                except StopIteration:
+                    break
+
+            if item.timestamp >= ticks_in_seconds:
+                delta = item.timestamp - ticks_in_seconds
+
+                # with in one unit
+                if int(delta) < seconds_per_unit:
+                    yield item
+                else:
+                    # or keep it for next time
+                    self._cached_item = item
+                    break
+            else:
+                # here we can log items that not sorted
+                pass
+
+
 cdef class MaroBinaryReader:
     def __init__(self):
         self._item_nt = None
 
     @property
-    def start_time(self):
+    def start_timestamp(self):
         return self._header.start_timestamp
 
     @property
-    def end_time(self):
+    def end_timestamp(self):
         return self._header.end_timestamp
 
     @property
@@ -100,6 +172,7 @@ cdef class MaroBinaryReader:
         self._header = self._reader.get_header()
 
         field_names = []
+
         self._item_fields_accessor = []
 
         for i in range(meta.fields.size()):
@@ -116,11 +189,26 @@ cdef class MaroBinaryReader:
     def reset(self):
         self._reader.reset()
 
+    def items_tick_picker(self, start_time_offset: int = 0, end_time_offset: int = None, time_unit: str = "s"):
+        cdef ULONGLONG start_time = calc_time_offset(self._header.start_timestamp, start_time_offset, time_unit)
+
+        cdef ULONGLONG end_time = INVALID_FILTER
+
+        if end_time_offset:
+            end_Time = calc_time_offset(self._header.start_timestamp, end_time_offset, time_unit)
+
+        self._reader.set_filter(start_time, end_time)
+
+        return ItemTickPicker(self.items(), self._header.start_timestamp, time_unit)
+
     def set_filter(self, start: int, end: int = None):
         if end == None:
             end = INVALID_FILTER
 
         self._reader.set_filter(start, end)
+
+    def disable_filter(self):
+        self._reader.disable_filter()
 
     def items(self):
         """
