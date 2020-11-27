e4fee677fc71590af6d13d7f0fa9c81af377ee81
chaosyu <chaos.you@gmail.com>
2020-11-27 14:15:35 +0800

    data lib ut passed

--- tests/test_binary_convert_read.py
+++ tests/test_binary_convert_read.py
@@ -4,9 +4,11 @@
 import os
 import tempfile
 import unittest
+from dateutil.tz import gettz
+from maro.data_lib import MaroBinaryConverter, MaroBinaryReader
+from maro.simulator.scenarios.helpers import utc_timestamp_to_timezone
 
-from maro.data_lib import BinaryConverter, BinaryReader
-from maro.data_lib.item_meta import BinaryMeta
+ny_tz = gettz("America/New_York")
 
 
 class TestBinaryConverter(unittest.TestCase):
@@ -15,10 +17,13 @@ class TestBinaryConverter(unittest.TestCase):
 
         out_bin = os.path.join(out_dir, "trips.bin")
 
-        meta_file = os.path.join("tests", "data", "data_lib", "case_1", "meta.yml")
+        meta_file = os.path.join(
+            "tests", "data", "data_lib", "case_1", "meta.toml")
         csv_file = os.path.join("tests", "data", "data_lib", "trips.csv")
 
-        bct = BinaryConverter(out_bin, meta_file)
+        bct = MaroBinaryConverter()
+        bct.open(out_bin)
+        bct.load_meta(meta_file)
 
         # add and convert 1st csv file
         bct.add_csv(csv_file)
@@ -27,17 +32,17 @@ class TestBinaryConverter(unittest.TestCase):
         bct.add_csv(csv_file)
 
         # flush will close the file, cannot add again
-        bct.flush()
-
+        bct.close()
 
         # check if output exist
         self.assertTrue(os.path.exists(out_bin))
 
         # check content
-        reader = BinaryReader(out_bin)
+        reader = MaroBinaryReader()
+        reader.open(out_bin)
 
         # start tick should be smallest one
-        start_date = reader.start_datetime
+        start_date = utc_timestamp_to_timezone(reader.start_timestamp, ny_tz)
 
         self.assertEqual(start_date.year, 2019)
         self.assertEqual(start_date.month, 1)
@@ -46,22 +51,21 @@ class TestBinaryConverter(unittest.TestCase):
         self.assertEqual(start_date.minute, 0)
         self.assertEqual(start_date.second, 0)
 
-        end_date = reader.end_datetime
+        end_date = utc_timestamp_to_timezone(reader.end_timestamp, ny_tz)
 
         self.assertEqual(end_date.year, 2019)
         self.assertEqual(end_date.month, 1)
         self.assertEqual(end_date.day, 1)
         self.assertEqual(end_date.hour, 0)
         self.assertEqual(end_date.minute, 5)
-        self.assertEqual(end_date.second, 0)     
-
+        self.assertEqual(end_date.second, 0)
 
         # there should be double items as trips.csv
-        self.assertEqual(4*2, reader.header.item_count)
+        self.assertEqual(4*2, reader.item_count)
 
         # 20 byte
-        self.assertEqual(20, reader.header.item_size)   
-        
+        self.assertEqual(14, reader.item_size)
+
         start_station_index = [0, 0, 1, 0]
 
         idx = 0
@@ -69,97 +73,100 @@ class TestBinaryConverter(unittest.TestCase):
         # check iterating interface
         for item in reader.items():
             # check if fields same as meta
-            self.assertTupleEqual(('timestamp', 'durations', 'src_station', 'dest_station'), item._fields)
+            self.assertTupleEqual(
+                ('timestamp', 'durations', 'src_station', 'dest_station'), item._fields)
 
             # check item start station index
-            self.assertEqual(start_station_index[idx % len(start_station_index)], item.src_station)
+            self.assertEqual(start_station_index[idx % len(
+                start_station_index)], item.src_station)
 
             idx += 1
-        
-        # check if filter works as expected
-        l = len([item for item in reader.items(end_time_offset=0, time_unit="m")])
 
-        # although there are 2 items that match the condition, but they not sorted, reader will not try to read to the end, but 
+        # check if filter works as expected
+        picker = reader.items_tick_picker(
+            start_time_offset=0,  end_time_offset=1, time_unit="m")
+
+        l = len([item for item in picker.items(0)])
+
+        # although there are 2 items that match the condition, but they not sorted, reader will not try to read to the end, but
         # to the first item which not match the condition
         self.assertEqual(1, l)
 
-        l = len([item for item in reader.items(start_time_offset=1, time_unit='m')])
+        print(reader.start_timestamp)
 
-        # reader will try to read 1st one that > end tick, so there should be 6 items 
-        self.assertEqual(6, l)
+        picker = reader.items_tick_picker(start_time_offset=1, time_unit='m')
 
-    def test_convert_without_events(self):
-        out_dir = tempfile.mkdtemp()
+        l = 0
 
-        out_bin = os.path.join(out_dir, "trips.bin")
+        for tick in range(10):
+            for item in picker.items(tick):
+                l += 1
 
-        meta_file = os.path.join("tests", "data", "data_lib", "case_2", "meta.yml")
-        csv_file = os.path.join("tests", "data", "data_lib", "trips.csv")
-
-        bct = BinaryConverter(out_bin, meta_file)
-
-        bct.add_csv(csv_file)
-
-        # flush will close the file, cannot add again
-        bct.flush()
-
-        reader = BinaryReader(out_bin)
-
-        meta: BinaryMeta = reader.meta
-
-        self.assertIsNotNone(meta)
-
-        # check events
-        self.assertListEqual(["require_bike", "return_bike", "rebalance_bike", "deliver_bike"], [event.display_name for event in meta.events])
-
-        self.assertListEqual(["RequireBike", "ReturnBike", "RebalanceBike", "DeliverBike"], [event.type_name for event in meta.events])
-
-        self.assertEqual("RequireBike", meta.default_event_name)
-        self.assertIsNone(meta.event_attr_name)
+        # ItemsItckPicker will filter items that not match current tick
+        # reader.items() will not
+        self.assertEqual(4, l)
 
     def test_convert_with_starttimestamp(self):
         out_dir = tempfile.mkdtemp()
 
         out_bin = os.path.join(out_dir, "trips.bin")
 
-        meta_file = os.path.join("tests", "data", "data_lib", "case_2", "meta.yml")
+        meta_file = os.path.join(
+            "tests", "data", "data_lib", "case_2", "meta.toml")
         csv_file = os.path.join("tests", "data", "data_lib", "trips.csv")
 
-        #12/31/2018 @ 11:59pm (UTC)
-        bct = BinaryConverter(out_bin, meta_file, utc_start_timestamp=1546300740)
+        bct = MaroBinaryConverter()
+        bct.open(out_bin)
+        bct.load_meta(meta_file)
+
+        # 12/31/2018 @ 11:59pm (NY) in UTC
+        bct.set_start_timestamp(1546318800 - 60)
 
         bct.add_csv(csv_file)
 
         # flush will close the file, cannot add again
-        bct.flush()
+        bct.close()
 
-        reader = BinaryReader(out_bin)
+        reader = MaroBinaryReader()
+        reader.open(out_bin)
 
         # check header
-        self.assertEqual(1546300740, reader.header.starttime)
+        self.assertEqual(1546318800 - 60, reader.start_timestamp)
 
         # then tick 0 will not be 2019/01/01 00:00:00
-        l = len([item for item in reader.items(end_time_offset=0, time_unit='m')])
+        picker = reader.items_tick_picker(end_time_offset=1, time_unit='m')
+
+        l = len([item for item in picker.items(0)])
 
         self.assertEqual(0, l)
 
-        # it should be tick 1 for now
-        l = len([item for item in reader.items(end_time_offset=1, time_unit='m')])
+        # # it should be tick 1 for now
+        l = len([item for item in picker.items(1)])
 
         self.assertEqual(1, l)
 
-
-    def test_convert_without_meta_timestamp(self):
+    def test_convert_without_meta(self):
         out_dir = tempfile.mkdtemp()
 
         out_bin = os.path.join(out_dir, "trips.bin")
 
-        meta_file = os.path.join("tests", "data", "data_lib", "case_3", "meta.yml")
         csv_file = os.path.join("tests", "data", "data_lib", "trips.csv")
 
-        #12/31/2018 @ 11:59pm (UTC)
-        with self.assertRaises(Exception) as ctx:
-            bct = BinaryConverter(out_bin, meta_file)
+        # 12/31/2018 @ 11:59pm (UTC)
+        with self.assertRaises(RuntimeError) as ctx:
+            bct = MaroBinaryConverter()
+            bct.open(out_bin)
+
+            bct.add_csv(csv_file)
+
+    def test_convert_without_open(self):
+        csv_file = os.path.join("tests", "data", "data_lib", "trips.csv")
+
+        # 12/31/2018 @ 11:59pm (UTC)
+        with self.assertRaises(RuntimeError) as ctx:
+            bct = MaroBinaryConverter()
+
+            bct.add_csv(csv_file)
 
 
 if __name__ == "__main__":
