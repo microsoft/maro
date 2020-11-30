9915ca9724ba88a80b4bd01271054ea5ca9b0e60
ysqyang <v-yangqi@microsoft.com>
2020-11-30 16:47:33 +0800

    fixed a bug

--- maro/rl/exploration/epsilon_greedy_explorer.py
+++ maro/rl/exploration/epsilon_greedy_explorer.py
@@ -2,6 +2,9 @@
 # Licensed under the MIT license.
 
 import random
+from typing import Union
+
+import numpy as np
 
 from .abs_explorer import AbsExplorer
 
@@ -17,12 +20,14 @@ class EpsilonGreedyExplorer(AbsExplorer):
         self._num_actions = num_actions
         self._epsilon = epsilon
 
-    def __call__(self, action_index: int):
-        assert (action_index < self._num_actions), f"Invalid action: {action_index}"
-        if random.random() > self._epsilon:
-            return action_index
+    def __call__(self, action_index: Union[int, np.ndarray]):
+        def get_exploration_action(action_index, num_actions, epsilon):
+            assert (action_index < num_actions), f"Invalid action: {action_index}"
+            return action_index if np.random.random() > epsilon else np.random.choice(num_actions)
+        if isinstance(action_index, int):
+            return get_exploration_action(action_index, self._num_actions, self._epsilon)
         else:
-            return random.randrange(self._num_actions)
+            return [get_exploration_action(act, self._num_actions, self._epsilon) for act in action_index]
 
     def update(self, *, epsilon: float):
         self._epsilon = epsilon
