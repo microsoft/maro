4fc1cf68e57f49aa4fc73325846df28a60784716
ysqyang <v-yangqi@microsoft.com>
2020-11-30 16:13:31 +0800

    removed epsilon parameter from choose_action

--- maro/rl/algorithms/dqn.py
+++ maro/rl/algorithms/dqn.py
@@ -76,12 +76,8 @@ class DQN(AbsAlgorithm):
         self._target_model = model.copy() if model.is_trainable else None
 
     @expand_dim
-    def choose_action(self, state: np.ndarray, epsilon=None):
-        if epsilon is None or np.random.rand() > epsilon:
-            q_values = self._get_q_values(self._model, state, is_training=False)
-            return q_values.argmax(dim=1).item()
-
-        return np.random.choice(self._config.num_actions)
+    def choose_action(self, state: np.ndarray):
+        return self._get_q_values(self._model, state, is_training=False).argmax(dim=1).data
 
     def _get_q_values(self, model, states, is_training: bool = True):
         if self._config.advantage_mode is not None:
--- maro/rl/algorithms/utils.py
+++ maro/rl/algorithms/utils.py
@@ -56,8 +56,10 @@ def expand_dim(func):
     def wrapper(self, state, **kwargs):
         if isinstance(state, np.ndarray):
             state = torch.from_numpy(state).to(device)
-        if len(state.shape) == 1:
+        is_single = len(state.shape) == 1
+        if is_single:
             state = state.unsqueeze(dim=0)
-        return func(self, state, **kwargs)
+        result = func(self, state, **kwargs)
+        return result.item() if is_single else result.numpy()
 
     return wrapper
