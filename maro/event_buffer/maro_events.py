2094bcde26b3f100b85654ccecf9eacd3ed78d0f
chaosyu <chaos.you@gmail.com>
2020-12-01 16:11:35 +0800

    add missing file

--- maro/event_buffer/maro_events.py
+++ maro/event_buffer/maro_events.py
@@ -7,5 +7,5 @@ from enum import Enum
 
 class MaroEvents (Enum):
     """Predefined decision event types, that used to communicate with outside."""
-    DECISION_EVENT = "maro_event_decision_event"
+    PENDING_DECISION = "maro_event_decision_event"
     TAKE_ACTION = "maro_event_take_action"
