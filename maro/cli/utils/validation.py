15de63f8488dab66f58acaeb76af246b6db6182c
Lyuchun Huang <romic.kid@gmail.com>
2020-12-24 09:53:41 +0000

    feat: refine validation.py and add docstrings

--- maro/cli/utils/validation.py
+++ maro/cli/utils/validation.py
@@ -10,31 +10,73 @@ from deepdiff import DeepDiff
 from maro.utils.exception.cli_exception import InvalidDeploymentTemplateError
 
 
-def validate_and_fill_dict(template_dict: dict, actual_dict: dict, optional_key_to_value: dict):
+def validate_and_fill_dict(template_dict: dict, actual_dict: dict, optional_key_to_value: dict) -> None:
+    """Validate incoming actual_dict with template_dict, and fill optional keys to the template.
+
+    We use deepDiff to find missing keys in the actual_dict, see
+    https://deepdiff.readthedocs.io/en/latest/diff.html#deepdiff-reference for reference.
+
+    Args:
+        template_dict (dict): template dict, we only need the layer structure of keys here, and ignore values.
+        actual_dict (dict): the actual dict with values, may miss some keys.
+        optional_key_to_value (dict): mapping of optional keys to values.
+
+    Returns:
+        None.
+    """
     deep_diff = DeepDiff(template_dict, actual_dict).to_dict()
 
-    missing_keys = deep_diff.get('dictionary_item_removed', [])
-    for key in missing_keys:
-        if key not in optional_key_to_value:
-            raise InvalidDeploymentTemplateError(f"Key '{key}' not found.")
+    missing_key_strs = deep_diff.get('dictionary_item_removed', [])
+    for missing_key_str in missing_key_strs:
+        if missing_key_str not in optional_key_to_value:
+            raise InvalidDeploymentTemplateError(f"Key '{missing_key_str}' not found.")
         else:
-            set_in_dict(
-                actual_dict,
-                get_map_list(deep_diff_str=key),
-                optional_key_to_value[key]
+            _set_value(
+                original_dict=actual_dict,
+                key_list=_get_parent_to_child_key_list(deep_diff_str=missing_key_str),
+                value=optional_key_to_value[missing_key_str]
             )
 
 
-def set_in_dict(data_dict: dict, map_list: list, value):
-    get_from_dict(data_dict, map_list[:-1])[map_list[-1]] = value
+def _set_value(original_dict: dict, key_list: list, value) -> None:
+    """Set the value to the original dict based on the key_list.
+
+    Args:
+        original_dict (dict): original dict that needs to be modified.
+        key_list (list): the parent to child path of keys, which describes that position of the value.
+        value: the value needs to be set.
+
+    Returns:
+        None.
+    """
+    _get_sub_structure_of_dict(original_dict, key_list[:-1])[key_list[-1]] = value
 
 
-def get_map_list(deep_diff_str: str):
-    deep_diff_str = deep_diff_str.replace("root", "")
-    deep_diff_str = deep_diff_str.replace("['", "")
+def _get_parent_to_child_key_list(deep_diff_str: str) -> list:
+    """Get parent to child key list by parsing the deep_diff_str.
+
+    Args:
+        deep_diff_str (str): a specially defined string that indicate the position of the key.
+            e.g. "root['a']['b']" -> {"a": {"b": value}}.
+
+    Returns:
+        list: the parent to child path of keys.
+    """
+
+    deep_diff_str = deep_diff_str.strip("root['")
     deep_diff_str = deep_diff_str.strip("']")
-    return deep_diff_str.split("']")
+    return deep_diff_str.split("']['")
 
 
-def get_from_dict(data_dict: dict, map_list: list):
-    return reduce(operator.getitem, map_list, data_dict)
+def _get_sub_structure_of_dict(original_dict: dict, key_list: list) -> dict:
+    """Get sub structure of dict from original_dict and key_list using reduce.
+
+    Args:
+        original_dict (dict): original dict that needs to be modified.
+        key_list (list): the parent to child path of keys, which describes that position of the value.
+
+    Returns:
+        dict: sub structure of the original_dict.
+    """
+
+    return reduce(operator.getitem, key_list, original_dict)
