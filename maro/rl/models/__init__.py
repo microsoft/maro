f2afa7536d96e64a026e6a6525eb29d6aae77a3a
ysqyang <ysqyang@gmail.com>
2020-12-30 16:10:57 +0800

    V0.2 store refinement (#234)
    * updated docs and images for rl toolkit
    
    * 1. fixed import formats for maro/rl; 2. changed decorators to hypers in store
    
    * fixed lint issues
    
    Co-authored-by: ysqyang <v-yangqi@microsoft.com>

--- maro/rl/models/__init__.py
+++ maro/rl/models/__init__.py
@@ -1,2 +1,8 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
+
+from .abs_block import AbsBlock
+from .fc_block import FullyConnectedBlock
+from .learning_model import LearningModule, LearningModuleManager, OptimizerOptions
+
+__all__ = ["AbsBlock", "FullyConnectedBlock", "LearningModule", "LearningModuleManager", "OptimizerOptions"]
