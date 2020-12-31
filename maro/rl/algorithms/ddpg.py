1d8b89da8a822b05688235d16d5fbf193cb77f01
ysqyang <v-yangqi@microsoft.com>
2020-12-31 23:17:06 +0800

    fixed conflicts

--- maro/rl/algorithms/ddpg.py
+++ maro/rl/algorithms/ddpg.py
@@ -1,11 +1,12 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-from typing import Callable
+from typing import Callable, Union
 
 import numpy as np
 import torch
 
+from maro.rl.exploration.noise_explorer import NoiseExplorer
 from maro.rl.models.learning_model import LearningModel
 
 from .abs_algorithm import AbsAlgorithm
@@ -17,13 +18,13 @@ class DDPGConfig:
         reward_decay (float): Reward decay as defined in standard RL terminology
         q_value_loss_func (Callable): Loss function for the Q-value estimator.
         target_update_frequency (int): Number of training rounds between policy target model updates.
-        explorer_cls: Explorer class.
-        explorer_params: Parameters required for the explorer class.
+        actor_loss_coefficient (float): The coefficient for policy loss in the total loss function, e.g.,
+            loss = q_value_loss + ``policy_loss_coefficient`` * policy_loss. Defaults to 1.0.
         tau (float): Soft update coefficient, e.g., target_model = tau * eval_model + (1-tau) * target_model.
             Defaults to 1.0.
     """
     __slots__ = [
-        "reward_decay", "q_value_loss_func", "target_update_frequency", "tau", "explorer_cls", "explorer_params"
+        "reward_decay", "q_value_loss_func", "target_update_frequency", "policy_loss_coefficient", "tau"
     ]
 
     def __init__(
@@ -31,63 +32,66 @@ class DDPGConfig:
         reward_decay: float,
         q_value_loss_func: Callable,
         target_update_frequency: int,
-        explorer_cls,
-        explorer_params: dict,
+        policy_loss_coefficient: float = 1.0,
         tau: float = 1.0,
     ):
         self.reward_decay = reward_decay
         self.q_value_loss_func = q_value_loss_func
         self.target_update_frequency = target_update_frequency
-        self.explorer_cls = explorer_cls
-        self.explorer_params = explorer_params
+        self.policy_loss_coefficient = policy_loss_coefficient
         self.tau = tau
 
 
 class DDPG(AbsAlgorithm):
-    """The Deep-Q-Networks algorithm.
+    """The Deep Deterministic Policy Gradient (DDPG) algorithm.
 
     See https://arxiv.org/pdf/1509.02971.pdf for details.
 
     Args:
         model (LearningModel): DDPG policy and q-value models.
         config: Configuration for DDPG algorithm.
+        explorer (NoiseExplorer): An NoiseExplorer instance for generating exploratory actions. Defaults to None.
     """
     def __init__(self, model: LearningModel, config: DDPGConfig):
         self.validate_task_names(model.task_names, {"policy", "q_value"})
         super().__init__(model, config)
-        self._explorer = self._config.explorer_cls(self._model.output_dim["policy"], **self._config.explorer_params)
+        self._action_dim = model.output_dim["policy"]
+        self._explorer = explorer
         self._target_model = model.copy() if model.is_trainable else None
         self._train_cnt = 0
 
-    def choose_action(self, state) -> np.ndarray:
+    def choose_action(self, state) -> Union[float, np.ndarray]:
         state = torch.from_numpy(state).to(self._device)
         is_single = len(state.shape) == 1
         if is_single:
             state = state.unsqueeze(dim=0)
 
-        model_action = self.model(state, task_name="policy", is_training=False)
-        if is_single:
-            return self._explorer(model_action)
+        action = self.model(state, task_name="policy", is_training=False).data.numpy()
+        if self._explorer:
+            action = self._explorer(action)
 
-        # batch inference
-        return np.vstack([self._explorer(action) for action in model_action])
+        if self._action_dim == 1:
+            action = action.squeeze(axis=1)
+
+        return action[0] if is_single else action
 
     def train(self, states: np.ndarray, actions: np.ndarray, rewards: np.ndarray, next_states: np.ndarray):
         states = torch.from_numpy(states).to(self._device)
-        actions = torch.from_numpy(actions).to(self._device)
+        actual_actions = torch.from_numpy(actions).to(self._device)
         rewards = torch.from_numpy(rewards).to(self._device)
         next_states = torch.from_numpy(next_states).to(self._device)
-        if len(actions.shape) == 1:
-            actions = actions.unsqueeze(1)  # (N, 1)
-        current_q_values = self._model(torch.cat([states, actions]), task_name="q_value").squeeze(1)  # (N,)
+        if len(actual_actions.shape) == 1:
+            actual_actions = actual_actions.unsqueeze(1)  # (N, 1)
+        current_q_values = self._model(torch.cat([states, actual_actions]), task_name="q_value").squeeze(1)  # (N,)
         next_actions = self._target_model(states, task_name="policy", is_training=False).unsqueeze(dim=1)
         next_q_values = self._target_model(
             torch.cat([next_states, next_actions]), task_name="q_value", is_training=False
         ).squeeze(1)  # (N,)
         target_q_values = (rewards + self._config.reward_decay * next_q_values).detach()  # (N,)
         q_value_loss = self._config.q_value_loss_func(current_q_values, target_q_values)
-        policy_loss = -self._model(torch.cat([states, self._model(states, task_name="policy")])).mean()
-        self._model.learn(q_value_loss + policy_loss)
+        actions_from_model = self._model(states, task_name="policy")
+        policy_loss = -self._model(torch.cat([states, actions_from_model]), task_name="q_value").mean()
+        self._model.learn(q_value_loss + self._config.policy_loss_coefficient * policy_loss)
         self._train_cnt += 1
         if self._train_cnt % self._config.target_update_frequency == 0:
             self._target_model.soft_update(self._model, self._config.tau)
