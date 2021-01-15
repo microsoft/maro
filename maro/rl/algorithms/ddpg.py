c5cb699a63099afcdc155f0519c7cd51f3e988cd
ysqyang <v-yangqi@microsoft.com>
2021-01-15 01:53:24 +0000

    added reference in ddpg.py

--- maro/rl/algorithms/ddpg.py
+++ maro/rl/algorithms/ddpg.py
@@ -45,8 +45,10 @@ class DDPGConfig:
 class DDPG(AbsAlgorithm):
     """The Deep Deterministic Policy Gradient (DDPG) algorithm.
 
-    See https://arxiv.org/pdf/1509.02971.pdf for details.
-
+    References:
+    https://arxiv.org/pdf/1509.02971.pdf
+    https://github.com/openai/spinningup/tree/master/spinup/algos/pytorch/ddpg
+    
     Args:
         model (LearningModel): DDPG policy and q-value models.
         config: Configuration for DDPG algorithm.
