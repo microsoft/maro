067e8b11e7a5c246fbf61071c77aff33ff1e1d48
ysqyang <v-yangqi@microsoft.com>
2021-01-15 15:16:59 +0800

    fixed a typo

--- maro/rl/distributed/__init__.py
+++ maro/rl/distributed/__init__.py
@@ -5,7 +5,7 @@ from .abs_dist_learner import AbsDistLearner
 from .actor import Actor
 from .agent_manager_proxy import AgentManagerProxy
 from .dist_learner import InferenceLearner, SimpleDistLearner
-from .experience_collection import concat_experiences_by_agent, merge_experiences_with_trajectory_boundariesgit
+from .experience_collection import concat_experiences_by_agent, merge_experiences_with_trajectory_boundaries
 
 __all__ = [
     "AbsDistLearner",
