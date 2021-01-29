6e37e9725c0aa93e60560e7f4de8370f1ec084a9
chaosyu <chaos.you@gmail.com>
2021-01-29 10:16:29 +0800

    Update vector env ut to test if support raw backend

--- tests/test_vector_env.py
+++ tests/test_vector_env.py
@@ -6,7 +6,7 @@ import sys
 import unittest
 
 from maro.vector_env import VectorEnv
-
+from .utils import backends_to_test
 
 class TestVectorEnv(unittest.TestCase):
     def test_batch_number_large_than_cpu_core(self):
@@ -20,30 +20,33 @@ class TestVectorEnv(unittest.TestCase):
                            durations=100)
 
     def test_push_single_env(self):
-        with VectorEnv(batch_num=2,
-                       scenario="cim",
-                       topology="toy.5p_ssddd_l0.0",
-                       durations=100) as ve:
+        for backend_name in backends_to_test:
+            os.environ["DEFAULT_BACKEND_NAME"] = backend_name
+            with VectorEnv(batch_num=2,
+                        scenario="cim",
+                        topology="toy.5p_ssddd_l0.0",
+                        durations=100) as ve:
 
-            # We use dict to specified which environment should go to next step.
+                # We use dict to specified which environment should go to next step.
 
-            # Push 1st environment to next step
-            metrics, decision_event, is_done = ve.step({0: None})
+                # Push 1st environment to next step
+                metrics, decision_event, is_done = ve.step({0: None})
 
-            # ticks of each environment
-            ticks = ve.tick
+                # ticks of each environment
+                ticks = ve.tick
 
-            # tick of 1st environment should be greater than 0, 2nd should being 0, as it is not being pushed
-            self.assertEqual(0, ticks[1])
-            self.assertGreater(ticks[0], 0)
+                # tick of 1st environment should be greater than 0, 2nd should being 0, as it is not being pushed
+                self.assertEqual(0, ticks[1])
+                self.assertGreater(ticks[0], 0)
 
-            # push both 2 environment by pass None action, which will be broadcast to all environment
-            while not is_done:
-                metrics, decision_event, is_done = ve.step(None)
+                # push both 2 environment by pass None action, which will be broadcast to all environment
+                while not is_done:
+                    metrics, decision_event, is_done = ve.step(None)
 
-            # The ticks should be same at the end
-            ticks = ve.tick
-            self.assertListEqual([99, 99], ticks)
+                # The ticks should be same at the end
+                ticks = ve.tick
+                self.assertListEqual([99, 99], ticks)
 
 if __name__ == "__main__":
     unittest.main()
+
