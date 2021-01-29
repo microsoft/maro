979f7643803f81315cfa2ed1af398675e0c7cd83
chaosyu <chaos.you@gmail.com>
2021-01-29 10:16:40 +0800

    correct comments on hello

--- examples/vector_env/hello.py
+++ examples/vector_env/hello.py
@@ -6,7 +6,7 @@ from maro.simulator.scenarios.cim.common import Action, DecisionEvent
 
 if __name__ == "__main__":
     with VectorEnv(batch_num=4, scenario="cim", topology="toy.5p_ssddd_l0.0", durations=100) as env:
-        for ep in range(1):
+        for ep in range(2):
             print("current episode:", ep)
 
             metrics, decision_event, is_done = (None, None, False)
@@ -15,17 +15,17 @@ if __name__ == "__main__":
                 action = None
 
                 # Usage:
-
-                # 1. only push 1st environment, leave others behind
+                # 1. Only push speicified (1st for this example) environment, leave others behind
                 # if decision_event:
                 #     env0_dec: DecisionEvent = decision_event[0]
 
-                #     # 1.1 after 1st environment is done, then others will push forward.
+                #     # 1.1 After 1st environment is done, then others will push forward.
                 #     if env0_dec:
                 #         ss0 = env.snapshot_list["vessels"][env0_dec.tick:env0_dec.vessel_idx:"remaining_space"]
                 #         action = {0: Action(env0_dec.vessel_idx, env0_dec.port_idx, -env0_dec.action_scope.load)}
 
-                # 2. only pass action to 1st environment (give None to other environments), but keep pushing all the environment, until the end
+                # 2. Only pass action to 1st environment (give None to other environments),
+                # but keep pushing all the environment, until the end
                 if decision_event:
                     env0_dec: DecisionEvent = decision_event[0]
 
@@ -41,10 +41,5 @@ if __name__ == "__main__":
 
             print("Final tick for each environment:", env.tick)
             print("Final frame index for each environment:", env.frame_index)
-
-            # states querying interface same as normal, but the result is a list of numpy.array
-            # empty_list = env.snapshot_list["ports"][::"empty"]
-
-            # print(empty_list)
 
             env.reset()
