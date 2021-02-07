f835399c6556e87fdad278c122e6aca597acb54c
MicrosoftHam <77261932+MicrosoftHam@users.noreply.github.com>
2021-02-07 19:43:59 +0800

    v0.2 Rule-based algorithms for VM Scheduling (#255)
    * rule_based_algorithm
    
    * revise_the_code_by_aiming_hao
    
    * revise_the_code_by_aiming_hao
    
    * use the np.argmin
    
    * Update best_fit.py
    
    fix the "np not defined"
    
    * refine the code
    
    * fix the error
    
    * refine the code
    
    * fix the error
    
    * fix the error
    
    * refine the code
    
    * remove the history
    
    * refine the code
    
    * update first_fit
    
    * Refine the code
    
    Co-authored-by: Jinyu-W <53509467+Jinyu-W@users.noreply.github.com>
    Co-authored-by: aiming hao <37905948+hamcoder@users.noreply.github.com>

---
+++ examples/vm_scheduling/rule_based_algorithm/launcher.py
@@ -0,0 +1,62 @@
+import io
+import os
+import random
+import timeit
+
+import yaml
+import importlib
+
+from maro.simulator import Env
+from maro.simulator.scenarios.vm_scheduling import AllocateAction, DecisionPayload, PostponeAction
+from maro.utils import convert_dottable
+
+from agent import VMSchedulingAgent
+
+def import_class(name):
+    components = name.rsplit('.', 1)
+    mod = importlib.import_module(components[0])
+    mod = getattr(mod, components[1])
+    return mod
+
+CONFIG_PATH = os.path.join(os.path.split(os.path.realpath(__file__))[0], "config.yml")
+with io.open(CONFIG_PATH, "r") as in_file:
+    raw_config = yaml.safe_load(in_file)
+    config = convert_dottable(raw_config)
+
+
+if __name__ == "__main__":
+    start_time = timeit.default_timer()
+
+    env = Env(
+        scenario=config.env.scenario,
+        topology=config.env.topology,
+        start_tick=config.env.start_tick,
+        durations=config.env.durations,
+        snapshot_resolution=config.env.resolution
+    )
+
+    if config.env.seed is not None:
+        env.set_seed(config.env.seed)
+        random.seed(config.env.seed)
+
+    algorithm_class = import_class(config.algorithm.type)
+    if config.algorithm.args is None:
+        algorithm = algorithm_class()
+    else:
+        algorithm = algorithm_class(**config.algorithm.args)
+
+    agent = VMSchedulingAgent(algorithm)
+
+    metrics, decision_event, is_done = env.step(None)
+
+    while not is_done:
+        action = agent.choose_action(decision_event, env)
+        metrics, decision_event, is_done = env.step(action)
+
+    end_time = timeit.default_timer()
+    print(
+        f"[{config.algorithm.type.split('.')[1]}] Topology: {config.env.topology}. Total ticks: {config.env.durations}."
+        f" Start tick: {config.env.start_tick}."
+    )
+    print(f"[Timer] {end_time - start_time:.2f} seconds to finish the simulation.")
+    print(metrics)
