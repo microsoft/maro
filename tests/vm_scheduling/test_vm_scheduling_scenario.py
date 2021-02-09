e31bea3d937bb20cba9e2a6fea8a1dded71e3015
Kuan Wei Yu <v-kyu@microsoft.com>
2021-02-09 23:00:11 +0800

    V0.2 vm region support (#258)
    * Region init
    
    * Add region, zone, cluster
    
    * Fix bug
    
    * Add update parent id
    
    * Update PM config
    
    * Update number
    
    * Fix import order
    
    * Fix bug
    
    * Modify config
    
    * Add cluster attribute
    
    * Refine naming
    
    * Fix bug
    
    * Modify 336k config
    
    * Update region
    
    * Update config
    
    * Update pm config
    
    * pylint
    
    * Add comment
    
    * Update based on PR comment
    
    * Modify config and zone class
    
    * Add unit test
    
    * Update region part
    
    * Update pylint
    
    * Modify unit test
    
    * Refactor region structure
    
    * Add comment and fix style
    
    * Fix machine num bugs
    
    * Modify config
    
    * Fix style
    
    * Fix bugs and add empty machine attributes
    
    * Add update upper level metrics
    
    * Update config
    
    * Fix lint style
    
    * Modify doc strings
    
    * Fix amount counter
    
    * Update unit test
    
    * fix lint style
    
    * Update the ids init
    
    * Init total and empty machine num
    
    * Update lint style
    
    * Fix snapshot attributes initial state
    
    * Update config
    
    * add topologies for over-subscription and multi-cluster to be compatible with the previous topologies
    
    * Add simulation result
    
    * Move readme
    
    * Add overload results
    
    Co-authored-by: Jinyu-W <53509467+Jinyu-W@users.noreply.github.com>
    Co-authored-by: Jinyu Wang <Wang.Jinyu@microsoft.com>

--- tests/vm_scheduling/test_vm_scheduling_scenario.py
+++ tests/vm_scheduling/test_vm_scheduling_scenario.py
@@ -4,10 +4,12 @@
 import unittest
 
 from maro.data_lib import BinaryConverter
+from maro.event_buffer import EventBuffer
 from maro.simulator.scenarios.vm_scheduling import CpuReader
+from maro.simulator.scenarios.vm_scheduling.business_engine import VmSchedulingBusinessEngine
 
 
-class CpuReaderTest(unittest.TestCase):
+class TestCpuReader(unittest.TestCase):
 
     for i in range(1, 4):
         meta_file = "tests/data/vm_scheduling/cpu_readings.yml"
@@ -85,6 +87,41 @@ class CpuReaderTest(unittest.TestCase):
         expected = 7
         self.assertEqual(expected, len(cpu_utilization_dict))
 
+
+class TestRegion(unittest.TestCase):
+
+    def setUp(self):
+        config_path = "tests/data/vm_scheduling"
+        self.eb = EventBuffer()
+        self.be = VmSchedulingBusinessEngine(
+            event_buffer=self.eb, topology=config_path, start_tick=0, max_tick=3,
+            snapshot_resolution=1, max_snapshots=None, additional_options={}
+        )
+
+    def test_config(self):
+        region_amount = self.be._region_amount
+        expected = 2
+        self.assertEqual(expected, region_amount)
+
+        zone_amount = self.be._zone_amount
+        expected = 2
+        self.assertEqual(expected, zone_amount)
+
+        data_center_amount = self.be._data_center_amount
+        expected = 3
+        self.assertEqual(expected, data_center_amount)
+
+        cluster_amount = self.be._cluster_amount
+        expected = 8
+        self.assertEqual(expected, cluster_amount)
+
+        rack_amount = self.be._rack_amount
+        expected = 75
+        self.assertEqual(expected, rack_amount)
+
+        pm_amount = self.be._pm_amount
+        expected = 1130
+        self.assertEqual(expected, pm_amount)
 
 if __name__ == "__main__":
     unittest.main()
