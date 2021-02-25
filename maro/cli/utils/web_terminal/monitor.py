89a1e235bd0d8347c56554652efa18812ad8202c
zhanyu <zhanyu.wang@pacteraedge.com>
2021-02-25 10:50:47 +0800

    cli vis prototype

---
+++ maro/cli/utils/web_terminal/monitor.py
@@ -0,0 +1,345 @@
+import json
+import os
+import time
+from enum import Enum
+
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+from maro.cli.grass.utils.params import JobStatus
+
+
+class DashboardType(Enum):
+    PROCESS = "process"
+    LOCAL = "local"
+    AZURE = "azure"
+    ONPREMISES = "on-premises"
+
+
+class DataType(Enum):
+    CPU = "cpu"
+    MEMORY = "memory"
+    GPU = "gpu"
+
+
+def write_static_resurce(root, data, node_name, dashboard_type):
+    if dashboard_type in [DashboardType.PROCESS, DashboardType.LOCAL]:
+        root.header("Overall Resource:")
+    else:
+        root.header("Cluster Resource:")
+
+    col1, col2, col3 = root.beta_columns(3)
+    with col1:
+        st.subheader("CPU")
+        st.write(data['cpu'])
+    with col2:
+        st.subheader("Memory")
+        st.write(data['memory'])
+    with col3:
+        st.subheader("GPU")
+        st.write(data['gpu'])
+
+
+def write_attr(root, name, data):
+    data_len = len(data)
+    if data_len > 0:
+        with root:
+            col1, col2 = root.beta_columns([1, 9])
+            with col1:
+                st.subheader(str.upper(name))
+            data_array = []
+            for data_byte in data:
+                data_str = data_byte.decode("utf-8")
+                data_list = pd.Series(json.loads(data_str), dtype="float64")
+                data_point = data_list.mean()
+                data_array.append(data_point)
+
+            data_len = len(data)
+
+            chart_data = pd.DataFrame(data_array)
+            chart_data['Time in minutes'] = chart_data.index
+            if name in ["cpu", "gpu"]:
+                chart_data[0] = chart_data[0] / 100
+            alt_chart = alt.Chart(chart_data).mark_area().encode(
+                x=alt.X('Time in minutes', type='quantitative'),
+                y=alt.Y('0', scale=alt.Scale(domain=(0, 1)), axis=alt.Axis(
+                    format='%', title='Usage'), type='quantitative')
+            )
+            with col2:
+                chart = st.altair_chart(alt_chart, use_container_width=True)
+            return chart, chart_data, data_len
+    else:
+        raise Exception("No data yet...")
+
+
+def write_resource_usage(root, name, data, dashboard_type):
+    name_container = root.empty()
+    node_ref = []
+
+    if dashboard_type in [DashboardType.LOCAL]:
+        col1, col2, col3 = root.beta_columns(3)
+        with col1:
+            st.subheader("CPU")
+            node = st.empty()
+            node_ref.append(node)
+            node.write(data['cpu'])
+        with col2:
+            st.subheader("Memory")
+            node = st.empty()
+            node_ref.append(node)
+            node.write(data['memory'])
+        with col3:
+            st.subheader("GPU")
+            node = st.empty()
+            node_ref.append(node)
+            node.write(data['gpu'])
+        name_container.header("Resource Remaining:")
+    else:
+        for attr_key in data:
+            chart_ref, _, data_len = write_attr(root, attr_key, data[attr_key])
+            node_ref.append(
+                {'chart': chart_ref, 'len': data_len, 'chart_name': attr_key})
+        name_container.header("Resource Usage:")
+    return node_ref
+
+
+def update_node(node_ref, new_data, dashboard_type, org_data_len):
+    if dashboard_type in [DashboardType.LOCAL]:
+        for attr_ref, att_name in zip(node_ref, ["cpu", "memory", "gpu"]):
+            attr_ref.write(new_data[att_name])
+    else:
+
+        for attr_ref in node_ref:
+            attr_data = new_data[attr_ref['chart_name']]
+            data_len = len(attr_data)
+            if data_len > 0:
+                data_array = []
+                for data_byte in attr_data:
+                    data_str = data_byte.decode("utf-8")
+                    data_list = pd.Series(json.loads(data_str), dtype="float64")
+                    data_array.append(data_list.mean())
+                chart_data = pd.DataFrame(data_array)
+                chart_data['x'] = chart_data.index + org_data_len
+                if attr_ref['chart_name'] in ["cpu", "gpu"]:
+                    chart_data[0] = chart_data[0] / 100
+
+                attr_ref['chart'].add_rows(chart_data)
+                attr_ref['len'] += data_len
+
+
+def write_job_list(root, list_name, list_data):
+    root.subheader(f"{list_name}:")
+    if len(list_data) == 0:
+        root.write(f"No job in queue \"{list_name}\" yet.")
+    for data_byte in list_data:
+        data_str = data_byte.decode("utf-8")
+        root.markdown(f" - {data_str}")
+
+
+def write_job_queue(root, data):
+    root.header("Job Queues:")
+    write_job_list(root, "Pending Jobs", data["pending_jobs"])
+    write_job_list(root, "Killed Jobs", data["killed_jobs"])
+
+
+def job_status_to_color(job_status):
+    status_to_color = {
+        JobStatus.RUNNING: "lightgreen",
+        JobStatus.PENDING: "orange",
+        JobStatus.KILLED: "gray",
+        JobStatus.FINISH: "aqua",
+        JobStatus.FAILED: "red",
+    }
+    return status_to_color[job_status]
+
+
+def write_job_detail(root, data):
+
+    component_html = """
+<table style="margin: 1rem; border_width: 0rem;">
+<tr>
+<th>Component</th> <th>Num</td> <th>Command</td>
+</tr>
+"""
+
+    for name in data['components']:
+        component_html += f"""
+<tr>
+<td>{name}</td> <td>{data['components'][name]['num']}</td> <td>{data['components'][name]['command']}</td>
+</tr>
+"""
+
+    component_html += """
+</table>
+"""
+
+    color = job_status_to_color(data['status'])
+
+    job_str = f"""<details>
+<summary> <span class="dot" style="background-color: {color}; border-radius: 50%; height: 1rem; width: 1rem; display: inline-block;"></span>  {data['name']}    Mode: {data['mode']} </summary>
+{component_html}
+</details>"""
+    root.markdown(job_str, True)
+
+
+def write_job_detail_container(root, data):
+
+    component_html = """
+<table style="margin: 1rem; border_width: 0rem;">
+<tr>
+<th>Component</th>
+<th>Num</td>
+<th>Command</td>
+<th>Image</th>
+<th>CPU</td>
+<th>Memory</td>
+<th>GPU</th>
+</tr>
+"""
+
+    for name in data['components']:
+        component_html += f"""
+<tr>
+<td>{name}</td>
+<td>{data['components'][name]['num']}</td>
+<td>{data['components'][name]['command']}</td>
+<td>{data['components'][name]['image']}</td>
+<td>{data['components'][name]['resources']['cpu']}</td>
+<td>{data['components'][name]['resources']['memory']}</td>
+<td>{data['components'][name]['resources']['gpu']}</td>
+</tr>
+"""
+
+    component_html += """
+</table>
+"""
+
+    color = job_status_to_color(data['status'])
+
+    job_str = f"""<details>
+<summary> <span class="dot" style="background-color: {color}; border-radius: 50%; height: 1rem; width: 1rem; display: inline-block;"></span>  {data['name']}    Mode: {data['mode']} </summary>
+{component_html}
+</details>"""
+    root.markdown(job_str, True)
+
+
+def write_job_details(root, data, dashboard_type):
+    root.header("Job Details:")
+    if len(data) == 0:
+        root.write("No job details yet.")
+    if dashboard_type is DashboardType.PROCESS:
+        for job in data:
+            write_job_detail(root, job)
+    else:
+        for job in data:
+            write_job_detail_container(root, job)
+
+
+def load_executor(cluster_name):
+    if cluster_name == "process":
+        from maro.cli.process.executor import ProcessExecutor
+        executor = ProcessExecutor()
+        cluster_type = DashboardType.PROCESS
+    else:
+        from maro.cli.utils.details_reader import DetailsReader
+        cluster_details = DetailsReader.load_cluster_details(
+            cluster_name=cluster_name)
+        if cluster_details["mode"] == "grass/azure":
+            from maro.cli.grass.executors.grass_azure_executor import GrassAzureExecutor
+            executor = GrassAzureExecutor(cluster_name=cluster_name)
+            cluster_type = DashboardType.AZURE
+        elif cluster_details["mode"] == "grass/on-premises":
+            from maro.cli.grass.executors.grass_on_premises_executor import GrassOnPremisesExecutor
+            executor = GrassOnPremisesExecutor(cluster_name=cluster_name)
+            cluster_type = DashboardType.ONPREMISES
+        elif cluster_details["mode"] == "grass/local":
+            from maro.cli.grass.executors.grass_local_executor import GrassLocalExecutor
+            executor = GrassLocalExecutor(cluster_name=cluster_name)
+            cluster_type = DashboardType.LOCAL
+    return executor, cluster_type
+
+
+def load_clusters():
+    clusters = []
+    from maro.cli.utils.params import GlobalPaths
+    for root, _, files in os.walk(GlobalPaths.ABS_MARO_CLUSTERS, topdown=False):
+        for name in files:
+            if os.path.basename(name) == "cluster_details.yml":
+                clusters.append(os.path.basename(root))
+    return clusters
+
+
+def draw_dashboard(target):
+    if not target:
+        return
+    try:
+        static_node_load = False
+        dynamic_node_load = False
+        ct = st.beta_container()
+        resource_ct = ct.beta_container()
+        jce = ct.empty()
+        while True:
+
+            if not (static_node_load or dynamic_node_load):
+                local_executor, dashboard_type = load_executor(target)
+
+                resource_data = local_executor.get_resource()
+                write_static_resurce(
+                    resource_ct, resource_data, target, dashboard_type)
+                static_node_load = True
+
+                resource_ct.markdown("---")
+
+                node_data_dict = local_executor.get_resource_usage(0)
+                node_ref = write_resource_usage(
+                    resource_ct, target, node_data_dict, dashboard_type)
+
+                resource_ct.markdown("---")
+
+                jct = jce.beta_container()
+
+                job_queue_data = local_executor.get_job_queue()
+                write_job_queue(jct, job_queue_data)
+
+                jct.markdown("---")
+
+                job_detail_data = local_executor.get_job_details()
+                write_job_details(jct, job_detail_data, dashboard_type)
+
+                dynamic_node_load = True
+
+            elif static_node_load and dynamic_node_load:
+                data_len = node_ref[0]['len'] if dashboard_type != DashboardType.LOCAL else 0
+                new_data_dict = local_executor.get_resource_usage(data_len)
+                update_node(node_ref, new_data_dict, dashboard_type, data_len)
+
+                jct = jce.beta_container()
+
+                job_queue_data = local_executor.get_job_queue()
+
+                jct.markdown("---")
+
+                job_detail_data = local_executor.get_job_details()
+                write_job_details(jct, job_detail_data, dashboard_type)
+
+            time.sleep(5)
+    except Exception as e:
+        with st.spinner(f'No data yet...{e}'):
+            time.sleep(5)
+
+
+def main():
+    target = None
+
+    with st.spinner("Loading Clusters..."):
+        clusters = load_clusters()
+        for cluster_name in clusters:
+            if st.sidebar.button(f'{cluster_name}'):
+                target = cluster_name
+    with st.empty():
+        draw_dashboard(target)
+
+
+if __name__ == '__main__':
+    main()
---
+++ maro/cli/utils/web_terminal/terminal-srv.py
@@ -0,0 +1,156 @@
+import argparse
+import fcntl
+import os
+import platform
+import pty
+import select
+import shlex
+import struct
+import subprocess
+import termios
+import time
+
+from flask import Flask, redirect, request, send_file, send_from_directory
+from flask_socketio import SocketIO
+
+port = 8080
+
+app = Flask("Terminal-Service")
+app.config["fd"] = None
+app.config["pid"] = None
+app.config["child_pid"] = None
+socketio = SocketIO(app)
+
+
+def set_terminal_size(fd, row, col, xpix=0, ypix=0):
+    winsize = struct.pack("HHHH", row, col, xpix, ypix)
+    fcntl.ioctl(fd, termios.TIOCSWINSZ, winsize)
+
+
+def read_and_forward_pty_output():
+    max_read_bytes = 1024 * 20
+    while True:
+        socketio.sleep(0.01)
+        if app.config["fd"]:
+            timeout_sec = 0
+            (data_ready, _, _) = select.select(
+                [app.config["fd"]], [], [], timeout_sec)
+            if data_ready:
+                output = os.read(app.config["fd"], max_read_bytes).decode()
+                socketio.emit(
+                    "pty-output", {"output": output}, namespace="/pty")
+
+
+@app.route("/")
+def index():
+    return send_file("index.html")
+
+
+@app.route("/<path:path>")
+def root_folder(path: str):
+    return send_from_directory(".", path)
+
+
+@app.route("/assets/<path:path>")
+def assets_files(path: str):
+    return send_from_directory("assets", path)
+
+
+@app.route("/dashboard")
+def dashboard():
+    return redirect('http://localhost:8501', code=301)
+
+
+@socketio.on("pty-input", namespace="/pty")
+def pty_input(data):
+    """write to the child pty. The pty sees this as if you are typing in a real
+    terminal.
+    """
+    if app.config["fd"]:
+        os.write(app.config["fd"], data["input"].encode())
+
+
+@socketio.on("resize", namespace="/pty")
+def resize(data):
+    if app.config["fd"]:
+        set_terminal_size(app.config["fd"], data["rows"], data["cols"])
+
+
+@socketio.on("connect", namespace="/pty")
+def connect():
+    """new client connected"""
+
+    if app.config["child_pid"]:
+        # already started child process, don't start another
+        return
+
+    # create child process attached to a pty we can read from and write to
+    (child_pid, fd) = pty.fork()
+    if child_pid == 0:
+        subprocess.run(app.config["cmd"])
+    else:
+
+        app.config["fd"] = fd
+        app.config["child_pid"] = child_pid
+        set_terminal_size(fd, 16, 50)
+        cmd = " ".join(shlex.quote(c) for c in app.config["cmd"])
+        print("child pid is", child_pid)
+        print(
+            f"starting background task with command `{cmd}` to continously read "
+            "and forward pty output to client"
+        )
+        socketio.start_background_task(target=read_and_forward_pty_output)
+        print("task started")
+
+
+def os_is_windows() -> bool:
+    info = platform.platform()
+    if "Windows" in info:
+        return True
+    else:
+        return False
+
+
+def shell_cmd() -> str:
+    if os_is_windows():
+        return "cmd.exe"
+    else:
+        return "bash"
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description=(
+            "Welcome to MARO. "
+            "Repository: https://github.com/microsoft/maro"
+            "Documentation: https://maro.readthedocs.io/en/latest/"
+        ),
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("-p", "--port", default=port,
+                        help="port to run server on")
+    parser.add_argument(
+        "--host",
+        default="127.0.0.1",
+        help="host to run server on (use 0.0.0.0 to allow access from other hosts)",
+    )
+    parser.add_argument("--debug", action="store_true",
+                        help="debug the server")
+    parser.add_argument("--version", action="store_true",
+                        help="print version and exit")
+    parser.add_argument(
+        "--command", default=shell_cmd(), help="Command to run in the terminal"
+    )
+    parser.add_argument(
+        "--cmd-args",
+        default="",
+        help="arguments to pass to command (i.e. --cmd-args='arg1 arg2 --flag')",
+    )
+    args = parser.parse_args()
+    print(f"serving on http://127.0.0.1:{args.port}")
+    app.config["cmd"] = [args.command] + shlex.split(args.cmd_args)
+    socketio.run(app, debug=args.debug, port=args.port, host=args.host)
+
+
+if __name__ == "__main__":
+    main()
