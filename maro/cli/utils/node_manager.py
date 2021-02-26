09369808a7d6e828e88536ba4952bd083d31cd06
zhanyu <zhanyu.wang@pacteraedge.com>
2021-02-26 14:49:12 +0800

    update for lint

---
+++ maro/cli/utils/node_manager.py
@@ -0,0 +1,29 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+import os
+
+
+def start_node_manager(*args, **kwargs):
+    monitor_path = os.path.expanduser("~/.maro/web_terminal/monitor.py")
+    os.system(
+        f"streamlit run {monitor_path} & "
+    )
+
+    terminal_path = os.path.expanduser("~/.maro/web_terminal/terminal-srv.py")
+    os.system(
+        f"cd {os.path.dirname(terminal_path)} ;"
+        f"python {terminal_path} &"
+    )
+
+
+def stop_node_manager(*args, **kwargs):
+    monitor_path = os.path.expanduser("~/.maro/web_terminal/monitor.py")
+    os.system(
+        f"pkill -f '{monitor_path}' "
+    )
+
+    terminal_path = os.path.expanduser("~/.maro/web_terminal/terminal-srv.py")
+    os.system(
+        f"pkill -f '{terminal_path}' "
+    )
