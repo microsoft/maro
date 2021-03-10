20c2fe06cd034a3da5fa4312eef75314c09f81c4
chaosyu <chaos.you@gmail.com>
2021-03-10 14:47:38 +0800

    is positive balance

--- examples/hello_world/supply_chain/state_shaping.py
+++ examples/hello_world/supply_chain/state_shaping.py
@@ -67,61 +67,67 @@ class SupplyChainStateShaping:
         self._env = env
 
     def shape(self):
-        self.is_positive_balance()
+        is_positive_balance = self._origin_group_by_sku_is_positive_balance()
 
-    def is_positive_balance(self):
+        print(is_positive_balance)
+
+    def _origin_group_by_sku_is_positive_balance(self):
+        # original code collect states of facilities and sku related units (manufacture, seller, consumer),
+        # other's states is added to facility
+        result = []
+
+        filter = lambda x: 1 if x > 1 else 0
+
+        cur_frame_index = self._env.frame_index
+
         features = ("id", "facility_id", "balance_sheet_profit", "balance_sheet_loss")
+        sku_related_features = features + ("product_id",)
 
-        storage_nodes =  self._env.snapshot_list["storage"]
-        storage_number = len(storage_nodes)
-
-        storage_balance_sheet = storage_nodes[self._env.frame_index::features]
-        storage_balance_sheet = storage_balance_sheet.flatten().reshape(storage_number, -1).astype(np.int)
-
-        distribution_nodes = self._env.snapshot_list["distribution"]
-        distribution_number = len(distribution_nodes)
-
-        distribution_balance_sheet = distribution_nodes[self._env.frame_index::features]
-        distribution_balance_sheet = distribution_balance_sheet.flatten().reshape(distribution_number, -1).astype(np.int)
-
-        transport_nodes = self._env.snapshot_list["transport"]
-        transport_number = len(transport_nodes)
-
-        transport_balance_sheet = transport_nodes[self._env.frame_index::features]
-        transport_balance_sheet = transport_balance_sheet.flatten().reshape(transport_number, -1).astype(np.int)
-
-        manufacture_nodes = self._env.snapshot_list["manufacture"]
-        manufacture_number = len(manufacture_nodes)
-
-        manufacture_balance_sheet = manufacture_nodes[self._env.frame_index::features]
-        manufacture_balance_sheet = manufacture_balance_sheet.flatten().reshape(manufacture_number, -1).astype(np.int)
-
-        seller_nodes = self._env.snapshot_list["seller"]
-        seller_number = len(seller_nodes)
-
-        seller_balance_sheet = seller_nodes[self._env.frame_index::features]
-        seller_balance_sheet = seller_balance_sheet.flatten().reshape(seller_number, -1).astype(np.int)
-
+        # facility balance sheet
         facility_nodes = self._env.snapshot_list["facility"]
-        facility_number = len(facility_nodes)
+        facility_balance_sheet = facility_nodes[cur_frame_index::features]
+        facility_balance_sheet = facility_balance_sheet.flatten().reshape(len(facility_nodes), -1).astype(np.int)
 
-        facility_balance_sheet = facility_nodes[self._env.frame_index::features]
-        facility_balance_sheet = facility_balance_sheet.flatten().reshape(facility_number, -1).astype(np.int)
+        facility_balance_sheet_total = facility_balance_sheet[:, 2] + facility_balance_sheet[:, 3]
 
-        print("storage balance sheet")
-        print(storage_balance_sheet)
+        facility_is_positive_balance = list(map(filter, facility_balance_sheet_total))
 
-        print("distribution balance sheet")
-        print(distribution_balance_sheet)
+        result.extend(facility_is_positive_balance)
 
-        print("transport balance sheet")
-        print(transport_balance_sheet)
+        # then each sku group for each facility
+        manufacture_nodes = self._env.snapshot_list["manufacture"]
+        seller_nodes = self._env.snapshot_list["seller"]
+        consumer_nodes = self._env.snapshot_list["consumer"]
 
-        print("manufacture balance sheet")
-        print(manufacture_balance_sheet)
+        manufacture_balance_sheet = manufacture_nodes[cur_frame_index::sku_related_features].flatten().reshape(len(manufacture_nodes), -1).astype(np.int)
+        seller_balance_sheet = seller_nodes[cur_frame_index::sku_related_features].flatten().reshape(len(seller_nodes), -1).astype(np.int)
+        consumer_balance_sheet = consumer_nodes[cur_frame_index::sku_related_features].flatten().reshape(len(consumer_nodes), -1).astype(np.int)
 
-        print("seller balance sheet")
-        print(seller_balance_sheet)
+        #
+        for facility_id in facility_balance_sheet[:, 0]:
+            manufacture_states = manufacture_balance_sheet[manufacture_balance_sheet[:, 1] == facility_id]
+            seller_states = seller_balance_sheet[seller_balance_sheet[:, 1] == facility_id]
+            consumer_states = consumer_balance_sheet[consumer_balance_sheet[:, 1] == facility_id]
 
-        print("facility balance sheet")
-        print(facility_balance_sheet)
+            length = max(len(manufacture_states), len(seller_states), len(consumer_states))
+
+            # this facility does not have any sku related units
+            if length == 0:
+                continue
+
+            sku_balance_sheet_total = np.zeros((length, ), dtype=np.int)
+
+            if len(manufacture_states) > 0:
+                sku_balance_sheet_total += manufacture_states[:, 2] + manufacture_states[:, 3]
+
+            if len(seller_states) > 0:
+                sku_balance_sheet_total += seller_states[:, 2] + seller_states[:, 3]
+
+            if len(consumer_states) > 0:
+                sku_balance_sheet_total += consumer_states[:, 2] + consumer_states[:, 3]
+
+            sku_is_positive_balance = list(map(filter, sku_balance_sheet_total))
+
+            result.extend(sku_is_positive_balance)
+
+        return result
