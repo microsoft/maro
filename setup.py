d7bbca49f0ae3c3f40c75abccedfb6afed7ebf28
J-shang <shangning128@163.com>
2021-03-11 06:14:43 +0000

    update python verison

--- setup.py
+++ setup.py
@@ -110,8 +110,9 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Scientific/Engineering :: Artificial Intelligence"],
-    python_requires=">=3.6,<3.8",
+    python_requires=">=3.6,<=3.8",
     setup_requires=[
         "numpy==1.19.1",
     ],
