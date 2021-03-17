d37f38517d1e7b7190761b993da42028863c4e7d
Meroy Chen <39452768+Meroy9819@users.noreply.github.com>
2021-03-17 14:56:03 +0800

    Add Env-Geographic visualization tool, CIM hello as example (#291)
    * streamit with questdb
    
    * script to import current dump data, except attention file, use influxdb line protocol for batch sending.
    
    * refine the interface to flatten dictionary
    
    * add messagetype.file to upload file later
    
    * correct tag name
    
    * correct the way to initial streamit, make it possible to use it any where after start
    
    * add data collecting in cim business engine
    
    * streamit client refactoring
    
    * fix import issue
    
    * update cim hello world, with a commented code to enable vis data streaming
    
    * fix metric replace bug
    
    * refactor the type checking code
    
    * V0.2 remove props from be (#269)
    
    * Fix bug
    
    * fix bu
    
    * Master vm doc - data preparation (#285)
    
    * Update vm docs
    
    * Update docs
    
    * Update data preparation docs
    
    * Update
    
    * Update docs
    
    Co-authored-by: Jinyu-W <53509467+Jinyu-W@users.noreply.github.com>
    
    * maro geo vis
    
    * add new line
    
    * doc update
    
    * lint refine
    
    * lint update
    
    * lint updata
    
    * lint update
    
    * lint update
    
    * lint update
    
    * code revert
    
    * add declare
    
    * code revert
    
    * add new line
    
    * add comment
    
    * delete surplus
    
    * delete core
    
    * lint update
    
    * lint update
    
    * lint update
    
    * lint update
    
    * specify version
    
    * lint update
    
    * specify docker version
    
    * import sort
    
    * backend revert
    
    * Delete test.py
    
    * format refact
    
    * doc update
    
    * import orders
    
    * change import orders
    
    * change import orders
    
    * add version of http-server
    
    * add specified port
    
    * delete print
    
    * lint update
    
    * lint update
    
    * lint update
    
    * update doc
    
    * dependecy update
    
    * update business engine
    
    * business engine
    
    * business engine update
    
    Co-authored-by: chaosyu <chaos.you@gmail.com>
    Co-authored-by: Michael Li <mic_lee2000@hotmail.com>
    Co-authored-by: Kuan Wei Yu <v-kyu@microsoft.com>
    Co-authored-by: Jinyu-W <53509467+Jinyu-W@users.noreply.github.com>

---
+++ maro/cli/maro_real_time_vis/back_end/vis_app/data_process/request/request_params.py
@@ -0,0 +1,21 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+from enum import Enum
+
+
+class request_column(Enum):
+    vessel_header = "tick, index, capacity, empty, full, remaining_space, route_idx"
+    decision_header = "tick, vessel_index, port_index, quantity"
+    order_header = "tick, from_port_index, dest_port_index, quantity"
+    port_header = "tick, index, capacity, empty, full, shortage, booking, fulfillment"
+
+
+class request_settings(Enum):
+    request_url = "http://127.0.0.1:9000/exec"
+    request_header = {
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'PUT,GET,POST,DELETE,OPTIONS',
+        'Cache-Control': 'no-cache, no-transform'
+    }
