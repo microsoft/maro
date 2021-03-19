8ed989d4a32701de8289b6e105440f14d8ba8cfe
kaiqi li <kaiqi.li@pacteraedge.com>
2021-03-19 10:27:28 +0000

    fixed internal logger dumplicated output

--- maro/communication/message.py
+++ maro/communication/message.py
@@ -8,12 +8,9 @@ from enum import Enum
 from typing import Union
 
 # private lib
-from maro.utils import InternalLogger
 from maro.utils.exit_code import NON_RESTART_EXIT_CODE
 
 from .utils import session_id_generator
-
-logger = InternalLogger(component_name="message")
 
 
 class SessionType(Enum):
@@ -113,5 +110,4 @@ class SessionMessage(Message):
         elif self.session_type == SessionType.NOTIFICATION:
             self.session_stage = session_stage if session_stage else NotificationSessionStage.REQUEST
         else:
-            logger.error(f"Receive unrecognized session type {self.session_type}, please use the SessionType class.")
-            sys.exit(NON_RESTART_EXIT_CODE)
+            raise KeyError(f'Unsupported session type {self.session_type}.')
