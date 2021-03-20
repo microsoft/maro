9d1bb087fc6aef220fb104aee824b2e1e34f85eb
chaosyu <chaos.you@gmail.com>
2021-03-20 18:03:13 +0800

    add missing sku info

--- maro/simulator/scenarios/supply_chain/facilities/retailer.py
+++ maro/simulator/scenarios/supply_chain/facilities/retailer.py
@@ -12,7 +12,7 @@ from .facility import FacilityBase
 class RetailerFacility(FacilityBase):
     """Retail facility used to generate order from upstream, and sell products by demand."""
 
-    SkuInfo = namedtuple("SkuInfo", ("name", "id", "price", "cost", "init_in_stock", "sale_gamma", "order_cost"))
+    SkuInfo = namedtuple("SkuInfo", ("name", "id", "price", "cost", "init_in_stock", "sale_gamma", "order_cost", "backlog_ratio"))
 
     # Product unit list of this facility.
     products: List[ProductUnit]
@@ -32,8 +32,9 @@ class RetailerFacility(FacilityBase):
                 sku_config.get("price", 0),
                 sku_config.get("cost", 0),
                 sku_config["init_in_stock"],
-                sku_config["sale_gamma"],
-                sku_config.get("order_cost", 0)
+                sku_config.get("sale_gamma", 0),
+                sku_config.get("order_cost", 0),
+                sku_config.get("backlog_ratio", 0)
             )
 
             self.skus[sku.id] = sku_info
