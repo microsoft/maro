1cdabf178f3c50452206b2cbee377f9475ac2557
chaosyu <chaos.you@gmail.com>
2021-03-22 17:25:13 +0800

    lint fix

--- maro/simulator/scenarios/supply_chain/__init__.py
+++ maro/simulator/scenarios/supply_chain/__init__.py
@@ -6,5 +6,5 @@ from .actions import ConsumerAction, ManufactureAction
 from .datamodels import (
     ConsumerDataModel, DistributionDataModel, ManufactureDataModel, SellerDataModel, StorageDataModel, VehicleDataModel
 )
-from .facilities import RetailerFacility, SupplierFacility, WarehouseFacility, FacilityBase
+from .facilities import FacilityBase, RetailerFacility, SupplierFacility, WarehouseFacility
 from .units import ConsumerUnit, DistributionUnit, ProductUnit, SellerUnit, SkuUnit, StorageUnit, UnitBase, VehicleUnit
