0361dcfc63fddad6dc8cec66ec19080a7a04e536
chaosyu <chaos.you@gmail.com>
2021-03-22 14:02:17 +0800

    add missing exported class

--- maro/simulator/scenarios/supply_chain/__init__.py
+++ maro/simulator/scenarios/supply_chain/__init__.py
@@ -6,5 +6,5 @@ from .actions import ConsumerAction, ManufactureAction
 from .datamodels import (
     ConsumerDataModel, DistributionDataModel, ManufactureDataModel, SellerDataModel, StorageDataModel, VehicleDataModel
 )
-from .facilities import RetailerFacility, SupplierFacility, WarehouseFacility
-from .units import ProductUnit, SellerUnit, SkuUnit, StorageUnit, UnitBase, VehicleUnit
+from .facilities import RetailerFacility, SupplierFacility, WarehouseFacility, FacilityBase
+from .units import ConsumerUnit, DistributionUnit, ProductUnit, SellerUnit, SkuUnit, StorageUnit, UnitBase, VehicleUnit
