c2e646245f82febbea78879f3198924ac4142e04
zhanyu <zhanyu.wang@pacteraedge.com>
2021-04-06 18:02:10 +0800

    update dashboard for new design part8

--- maro/cli/utils/web_terminal/terminal-srv.py
+++ maro/cli/utils/web_terminal/terminal-srv.py
@@ -107,7 +107,7 @@ def connect():
 
         app.config["fd"] = fd
         app.config["child_pid"] = child_pid
-        set_terminal_size(fd, 20, 50)
+        set_terminal_size(fd, 17, 75)
         cmd = " ".join(shlex.quote(c) for c in app.config["cmd"])
         print("child pid is", child_pid)
         print(
