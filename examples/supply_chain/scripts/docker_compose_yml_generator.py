e39a7baafec5b6d778a3d355b1bda2386cfc7f87
ysqyang <v-yangqi@microsoft.com>
2021-04-15 04:09:26 +0000

    some renaming

--- examples/supply_chain/scripts/docker_compose_yml_generator.py
+++ examples/supply_chain/scripts/docker_compose_yml_generator.py
@@ -17,7 +17,7 @@ with open(config_path, "r") as fp:
     num_actors = config["distributed"]["num_actors"]
     redis_host = config["distributed"]["redis_host"]
 
-docker_compose_yaml = {
+docker_compose_manifest = {
     "version": "3.9", 
     "services": {
         "redis": {"image": "redis:6", "container_name": redis_host},
@@ -33,12 +33,12 @@ docker_compose_yaml = {
 
 for i in range(num_actors):
     actor_id = f"actor.{i}"
-    actor_template = deepcopy(docker_compose_yaml["services"]["learner"])
-    del actor_template["build"]
-    actor_template["command"][-1] = "2"
-    actor_template["container_name"] = actor_id
-    actor_template["environment"] = [f"COMPONENT_NAME={actor_id}"]
-    docker_compose_yaml["services"][actor_id] = actor_template
+    actor_manifest = deepcopy(docker_compose_manifest["services"]["learner"])
+    del actor_manifest["build"]
+    actor_manifest["command"][-1] = "2"
+    actor_manifest["container_name"] = actor_id
+    actor_manifest["environment"] = [f"COMPONENT_NAME={actor_id}"]
+    docker_compose_manifest["services"][actor_id] = actor_manifest
 
 with open(join(sc_code_dir, "docker-compose.yml"), "w") as fp:
-    yaml.safe_dump(docker_compose_yaml, fp)
+    yaml.safe_dump(docker_compose_manifest, fp)
