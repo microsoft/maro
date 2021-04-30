ec9dcf80bde2e47165fbc26285f9475ede3f76fa
ysqyang <v-yangqi@microsoft.com>
2021-04-30 13:24:39 +0000

    made supply_chain scenario folder mountable

--- examples/supply_chain/learner.py
+++ examples/supply_chain/learner.py
@@ -1,8 +1,10 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+from os.path import dirname, join, realpath
 from maro.rl import Learner
 
+sc_code_dir = dirname(realpath(__file__))
 
 class SCLearner(Learner):
     def end_of_training(self, ep, segment, **kwargs):
