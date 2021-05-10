9eb7c555188b6ddaa7343784558a26cebee3778d
ysqyang <v-yangqi@microsoft.com>
2021-05-10 05:30:19 +0000

    rm obsolete files

--- maro/rl/storage/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT license.
-
-from .abs_store import AbsStore
-from .sampler import AbsSampler, UniformSampler
-from .sampler_cls_index import SAMPLER_CLS
-from .simple_store import SimpleStore
-
-__all__ = ["SAMPLER_CLS", "AbsSampler", "AbsStore", "SimpleStore", "UniformSampler"]
