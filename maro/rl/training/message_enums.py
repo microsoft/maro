3b5faeb367c8d77782e0b68cafec73ab9b4a84c1
ysqyang <v-yangqi@microsoft.com>
2021-06-11 12:07:49 +0000

    updated figures

--- maro/rl/training/message_enums.py
+++ maro/rl/training/message_enums.py
@@ -21,9 +21,8 @@ class MsgTag(Enum):
 class MsgKey(Enum):
     ACTION = "action"
     AGENT_ID = "agent_id"
-    EPISODE = "episode_index"
-    SEGMENT = "segment_index"
-    TIME_STEP = "time_step"
+    EPISODE = "episode"
+    SEGMENT = "segment"
     ENV_SUMMARY = "env_summary"
     EXPERIENCES = "experiences"
     NUM_EXPERIENCES = "num_experiences"
