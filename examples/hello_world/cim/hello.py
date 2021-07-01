c02f0a86fbd7896e3332d28a654395a500b916ec
Wang.Jinyu <jinywan@microsoft.com>
2021-07-01 10:07:47 +0000

    update hello files for CIM

--- examples/hello_world/cim/hello.py
+++ examples/hello_world/cim/hello.py
@@ -1,25 +1,19 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+from random import seed, randint
+
 from maro.simulator import Env
 from maro.simulator.scenarios.cim.common import Action, ActionType
 
 
 if __name__ == "__main__":
-    start_tick = 0
-    durations = 100  # 100 days
-
-    opts = dict()
-    """
-    enable-dump-snapshot parameter means business_engine needs dump snapshot data before reset.
-    If you leave value to empty string, it will dump to current folder.
-    For getting dump data, please uncomment below line and specify dump destination folder.
-    """
     # Initialize an environment with a specific scenario, related topology.
-    env = Env(scenario="cim", topology="global_trade.22p_l0.1",
-                start_tick=start_tick, durations=durations)
+    env = Env(scenario="cim", topology="global_trade.22p_l0.1", start_tick=0, durations=100)
+
     # To reset environmental data before starting a new experiment.
     env.reset()
+
     # Query environment summary, which includes business instances, intra-instance attributes, etc.
     print(env.summary)
 
@@ -28,25 +22,18 @@ if __name__ == "__main__":
         metrics, decision_event, is_done = env.step(None)
 
         while not is_done:
-            past_week_ticks = [x for x in range(
-                max(decision_event.tick - 7, 0), decision_event.tick)]
-            decision_port_idx = decision_event.port_idx
-            intr_port_infos = ["booking", "empty", "shortage"]
+            action_scope = decision_event.action_scope
+            to_discharge = action_scope.discharge > 0 and randint(0, 1) > 0
 
-            # Query the decision port booking, empty container inventory, shortage information in the past week
-            past_week_info = env.snapshot_list["ports"][past_week_ticks:
-                                                        decision_port_idx:
-                                                        intr_port_infos]
-
-            dummy_action = Action(
+            random_action = Action(
                 decision_event.vessel_idx,
                 decision_event.port_idx,
-                0,
-                ActionType.LOAD
+                randint(0, action_scope.discharge if to_discharge else action_scope.load),
+                ActionType.DISCHARGE if to_discharge else ActionType.LOAD
             )
 
             # Drive environment with dummy action (no repositioning)
-            metrics, decision_event, is_done = env.step(dummy_action)
+            metrics, decision_event, is_done = env.step(random_action)
 
         # Query environment business metrics at the end of an episode,
         # it is your optimized object (usually includes multi-target).
--- examples/hello_world/cim/hello_geo_vis.py
+++ examples/hello_world/cim/hello_geo_vis.py
@@ -9,32 +9,25 @@ os.environ["MARO_STREAMIT_ENABLED"] = "true"
 os.environ["MARO_STREAMIT_EXPERIMENT_NAME"] = "experiment_example"
 
 
+from random import seed, randint
+
 from maro.simulator import Env
-from maro.simulator.scenarios.cim.common import Action, ActionType
+from maro.simulator.scenarios.cim.common import Action, ActionScope, ActionType
 from maro.streamit import streamit
 
 
 if __name__ == "__main__":
-    start_tick = 0
-    durations = 100  # 100 days
+    seed(0)
+    NUM_EPISODE = 2
 
-    opts = dict()
     with streamit:
-        """
-        enable-dump-snapshot parameter means business_engine needs dump snapshot data before reset.
-        If you leave value to empty string, it will dump to current folder.
-        For getting dump data, please uncomment below line and specify dump destination folder.
-        """
-
         # Initialize an environment with a specific scenario, related topology.
-        env = Env(scenario="cim", topology="global_trade.22p_l0.1",
-                  start_tick=start_tick, durations=durations, options=opts)
+        env = Env(scenario="cim", topology="global_trade.22p_l0.1", start_tick=0, durations=100)
+
         # To reset environmental data before starting a new experiment.
         env.reset()
-        # Query environment summary, which includes business instances, intra-instance attributes, etc.
-        print(env.summary)
 
-        for ep in range(2):
+        for ep in range(NUM_EPISODE):
             # Tell streamit we are in a new episode.
             streamit.episode(ep)
 
@@ -42,25 +35,18 @@ if __name__ == "__main__":
             metrics, decision_event, is_done = env.step(None)
 
             while not is_done:
-                past_week_ticks = [x for x in range(
-                    max(decision_event.tick - 7, 0), decision_event.tick)]
-                decision_port_idx = decision_event.port_idx
-                intr_port_infos = ["booking", "empty", "shortage"]
+                action_scope = decision_event.action_scope
+                to_discharge = action_scope.discharge > 0 and randint(0, 1) > 0
 
-                # Query the decision port booking, empty container inventory, shortage information in the past week
-                past_week_info = env.snapshot_list["ports"][past_week_ticks:
-                                                            decision_port_idx:
-                                                            intr_port_infos]
-
-                dummy_action = Action(
+                random_action = Action(
                     decision_event.vessel_idx,
                     decision_event.port_idx,
-                    0,
-                    ActionType.LOAD
+                    randint(0, action_scope.discharge if to_discharge else action_scope.load),
+                    ActionType.DISCHARGE if to_discharge else ActionType.LOAD
                 )
 
                 # Drive environment with dummy action (no repositioning)
-                metrics, decision_event, is_done = env.step(dummy_action)
+                metrics, decision_event, is_done = env.step(random_action)
 
             # Query environment business metrics at the end of an episode,
             # it is your optimized object (usually includes multi-target).
