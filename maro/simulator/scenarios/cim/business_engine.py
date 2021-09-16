60edcecdab3f938ec7a8ad3d40d637ff29016bc9
Huoran Li <huoranli@microsoft.com>
2021-09-16 12:46:03 +0800

    Lint error

--- maro/simulator/scenarios/cim/business_engine.py
+++ maro/simulator/scenarios/cim/business_engine.py
@@ -7,15 +7,16 @@ from math import ceil, floor
 from typing import Optional
 
 import numpy as np
-from maro.backends.frame import FrameBase, SnapshotList
 from yaml import safe_load
 
+from maro.backends.frame import FrameBase, SnapshotList
 from maro.data_lib.cim import CimDataContainerWrapper, Order, Stop
 from maro.event_buffer import AtomEvent, CascadeEvent, EventBuffer, MaroEvents
 from maro.simulator.scenarios import AbsBusinessEngine
 from maro.simulator.scenarios.helpers import DocableDict
 from maro.simulator.scenarios.matrix_accessor import MatrixAttributeAccessor
 from maro.streamit import streamit
+
 from .common import Action, ActionScope, ActionType, DecisionEvent
 from .event_payload import EmptyReturnPayload, LadenReturnPayload, VesselDischargePayload, VesselStatePayload
 from .events import Events
