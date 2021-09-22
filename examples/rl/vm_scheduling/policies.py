db17d70bf6cc7228766e30cd901c7ec91dce8f8d
yaqiu <v-yaqiu@microsoft.com>
2021-09-22 15:47:12 +0000

    added get_state and set_state to vm_scheduling policy models

--- examples/rl/vm_scheduling/policies.py
+++ examples/rl/vm_scheduling/policies.py
@@ -53,6 +53,13 @@ class MyQNet(DiscreteQNet):
             param.grad = grad[name]
 
         self.optim.step()
+    
+    def get_state(self):
+        return {"network": self.state_dict(), "optim": self.optim.state_dict()}
+
+    def set_state(self, state):
+        self.load_state_dict(state["network"])
+        self.optim.load_state_dict(state["optim"])
 
 
 class MyACNet(DiscreteACNet):
@@ -91,6 +98,19 @@ class MyACNet(DiscreteACNet):
 
         self.actor_optim.step()
         self.critic_optim.step()
+
+    def get_state(self):
+        return {
+            "network": self.state_dict(),
+            "actor_optim": self.actor_optim.state_dict(),
+            "critic_optim": self.critic_optim.state_dict()
+        }
+
+    def set_state(self, state):
+        self.load_state_dict(state["network"])
+        self.actor_optim.load_state_dict(state["actor_optim"])
+        self.critic_optim.load_state_dict(state["critic_optim"])
+
 
 if algorithm == "dqn":
     policy_func_dict = {"dqn": lambda name: DQN(name, MyQNet(), **dqn_conf)}
