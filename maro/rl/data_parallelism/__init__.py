5ea27c831b882475a9464951d2f3eeafa8360450
buptchan <v-guanchen@microsoft.com>
2021-10-14 10:49:09 +0000

    Move `grad_worker` into marl/rl/workflows

--- maro/rl/data_parallelism/__init__.py
+++ maro/rl/data_parallelism/__init__.py
@@ -1,10 +1,8 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-from .grad_worker import grad_worker
 from .task_queue import TaskQueueClient, task_queue
 
 __all__ = [
-    'grad_worker',
     'TaskQueueClient', 'task_queue'
 ]
