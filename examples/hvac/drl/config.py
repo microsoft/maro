dd939e594ea6432a6d1eea2621f5adf71063ee0c
Jinyu Wang <Wang.Jinyu@microsoft.com>
2021-11-10 05:30:10 +0000

    remove unused parameters

--- examples/hvac/drl/config.py
+++ examples/hvac/drl/config.py
@@ -3,75 +3,56 @@ from shutil import copy2
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-actor_critic_agent_hyperparameters = {
-    "Actor": {
-        "learning_rate": 0.0003,
-        "linear_hidden_units": [128, 128],
-        "hidden_activations": 'tanh',
-        "dropout": 0.3,
-        "final_layer_activation": 'tanh',
-        "batch_norm": False,
-        "tau": 0.005,
-        "gradient_clipping_norm": 5,
-        "initialiser": "Xavier"
-    },
-
-    "Critic": {
-        "learning_rate": 0.0003,
-        "linear_hidden_units": [128, 128],
-        "hidden_activations": 'tanh',
-        "dropout": 0.3,
-        "final_layer_activation": None,
-        "batch_norm": False,
-        "buffer_size": 1000000,
-        "tau": 0.005,
-        "gradient_clipping_norm": 5,
-        "initialiser": "Xavier"
-    },
-
-    "min_steps_before_learning": 5000,
-    "batch_size": 256,
-    "discount_rate": 0.99,
-    "mu": 0.0, #for O-H noise
-    "theta": 0.05, #for O-H noise
-    "sigma": 0.05, #for O-H noise
-    "action_noise_std": 0.1,  # for TD3
-    "action_noise_clipping_range": 0.1,  # for TD3
-    "update_every_n_steps": 100,
-    "learning_updates_per_learning_session": 2,
-    "automatically_tune_entropy_hyperparameter": True,
-    "entropy_term_weight": 0.1,
-    "add_extra_noise": False,
-    "do_evaluation_iterations": True,
-    "clip_rewards": False
-}
 
 class Config(object):
     def __init__(self):
         self.num_episodes_to_run = 30
-        self.algorithm = "sac"  # ddpg, sac
+        self.algorithm = "ddpg"  # ddpg, sac
         self.experiment_name = f"{self.algorithm}_test"
 
         self.hyperparameters = {
-            "Policy_Gradient_Agents": {
-                "learning_rate": 0.05,
-                "linear_hidden_units": [30, 15],
-                "final_layer_activation": "TANH",
-                "learning_iterations_per_round": 10,
-                "discount_rate": 0.9,
-                "batch_norm": False,
-                "clip_epsilon": 0.2,
-                "episodes_per_learning_round": 10,
-                "normalise_rewards": True,
-                "gradient_clipping_norm": 5,
-                "mu": 0.0,
-                "theta": 0.15,
-                "sigma": 0.2,
-                "epsilon_decay_rate_denominator": 1,
-                "clip_rewards": False
-            },
+            "Actor_Critic_Agents": {
+                "Actor": {
+                    "learning_rate": 0.0003,
+                    "linear_hidden_units": [128, 128],
+                    "hidden_activations": 'tanh',
+                    "dropout": 0.3,
+                    "final_layer_activation": 'tanh',
+                    "batch_norm": False,
+                    "tau": 0.005,
+                    "gradient_clipping_norm": 5,
+                    "initialiser": "Xavier"
+                },
 
-            "Actor_Critic_Agents": actor_critic_agent_hyperparameters,
+                "Critic": {
+                    "learning_rate": 0.0003,
+                    "linear_hidden_units": [128, 128],
+                    "hidden_activations": 'tanh',
+                    "dropout": 0.3,
+                    "final_layer_activation": None,
+                    "batch_norm": False,
+                    "buffer_size": 1000000,
+                    "tau": 0.005,
+                    "gradient_clipping_norm": 5,
+                    "initialiser": "Xavier"
+                },
+
+                "min_steps_before_learning": 5000,
+                "batch_size": 256,
+                "discount_rate": 0.99,
+                "mu": 0.0, #for O-H noise
+                "theta": 0.05, #for O-H noise
+                "sigma": 0.05, #for O-H noise
+                "action_noise_std": 0.1,  # for TD3
+                "action_noise_clipping_range": 0.1,  # for TD3
+                "update_every_n_steps": 100,
+                "learning_updates_per_learning_session": 2,
+                "automatically_tune_entropy_hyperparameter": True,
+                "entropy_term_weight": 0.1,
+                "add_extra_noise": False,
+                "do_evaluation_iterations": True,
+                "clip_rewards": False
+            }
         }
 
         # Parameters that could kept the stable
