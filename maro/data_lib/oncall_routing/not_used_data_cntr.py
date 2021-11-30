1fd284dfb296f89c7acfde95fadccec3f46e50ae
Huoran Li <huoranli@microsoft.com>
2021-11-30 16:24:51 +0800

    Huoran/oncall routing data (#416)
    * Route sample logic
    
    * Oncall sample logic
    
    * Minor
    
    * Minor
    
    * Minor

--- maro/data_lib/oncall_routing/not_used_data_cntr.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT license.
-
-class OncallRoutingDataContainer(object):
-    def __init__(self) -> None:
-        super(OncallRoutingDataContainer, self).__init__()
--- maro/simulator/scenarios/oncall_routing/__init__.py
+++ maro/simulator/scenarios/oncall_routing/__init__.py
@@ -1,2 +1,12 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
+
+from .common import Coordinate, PlanElement, RouteNumber
+from .order import GLOBAL_ORDER_COUNTER, Order
+from .utils import EST_RAND_KEY, GLOBAL_RAND_KEY, ONCALL_RAND_KEY, PLAN_RAND_KEY
+
+__all__ = [
+    "Coordinate", "PlanElement", "RouteNumber",
+    "GLOBAL_ORDER_COUNTER", "Order",
+    "EST_RAND_KEY", "GLOBAL_RAND_KEY", "ONCALL_RAND_KEY", "PLAN_RAND_KEY"
+]
