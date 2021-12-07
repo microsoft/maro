201494e2e92ca3bc49bfdd7ec813216f61b2faab
Jinyu-W <53509467+Jinyu-W@users.noreply.github.com>
2021-12-07 17:52:32 +0800

    replace GLOBAL ORDER COUNTER with GLOBAL ORDER ID GENERATOR; add reset function for predictors (#425)
    remove outdated comments
    
    fix lint issue
    
    Co-authored-by: Jinyu Wang <Wang.Jinyu@microsoft.com>

--- maro/simulator/scenarios/oncall_routing/__init__.py
+++ maro/simulator/scenarios/oncall_routing/__init__.py
@@ -3,11 +3,11 @@
 
 from .common import PlanElement
 from .coordinate import Coordinate
-from .order import GLOBAL_ORDER_COUNTER, Order
+from .order import GLOBAL_ORDER_ID_GENERATOR, Order
 from .utils import EST_RAND_KEY, GLOBAL_RAND_KEY, ONCALL_RAND_KEY, PLAN_RAND_KEY
 
 __all__ = [
     "Coordinate", "PlanElement",
-    "GLOBAL_ORDER_COUNTER", "Order",
+    "GLOBAL_ORDER_ID_GENERATOR", "Order",
     "EST_RAND_KEY", "GLOBAL_RAND_KEY", "ONCALL_RAND_KEY", "PLAN_RAND_KEY"
 ]
