ccb3b841ab516835ef0675a171b1987e016b1aa4
Huoran Li <huoranli@microsoft.com>
2021-12-28 13:50:41 +0800

    Polish rl_v3/policy_trainer/abs_trainer.py

--- maro/rl_v3/utils/__init__.py
+++ maro/rl_v3/utils/__init__.py
@@ -1,9 +1,15 @@
+from typing import Union
+
 from .objects import SHAPE_CHECK_FLAG
 from .torch_util import match_shape, ndarray_to_tensor
 from .transition_batch import MultiTransitionBatch, TransitionBatch
 
+
+AbsTransitionBatch = Union[TransitionBatch, MultiTransitionBatch]
+
+
 __all__ = [
     "SHAPE_CHECK_FLAG",
     "match_shape", "ndarray_to_tensor",
-    "MultiTransitionBatch", "TransitionBatch"
+    "AbsTransitionBatch", "MultiTransitionBatch", "TransitionBatch"
 ]
