bd33c772faa11c67b33d25690a3cb11ef8ef7f4b
ysqyang <v-yangqi@microsoft.com>
2022-03-15 17:39:01 +0800

    addeed ppo to supply chain example

--- examples/rl/supply_chain/config.py
+++ examples/rl/supply_chain/config.py
@@ -1,17 +1,19 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+from os.path import dirname, join, realpath
+
 env_conf = {
     "scenario": "supply_chain",
     # Currently available topologies are "sample" or "random". New topologies must consist of a single folder
     # that contains a single config.yml and should be placed under /maro/simulator/scenarios/supply_chain/topologies
-    "topology": "sample",
+    "topology":  join(dirname(realpath(__file__)), "walmart", "large_capacity"),
     "durations": 100,  # number of ticks per episode
 }
 
 distribution_features = ("remaining_order_quantity", "remaining_order_number")
 seller_features = ("total_demand", "sold", "demand")
 
-rl_algorithm = "dqn"
+rl_algorithm = "ppo"
 
 NUM_CONSUMER_ACTIONS = 10
--- examples/rl/supply_chain/policy_trainer.py
+++ examples/rl/supply_chain/policy_trainer.py
@@ -37,5 +37,14 @@ if rl_algorithm == "dqn":
     trainer_creator = {
         agent_name.split(".")[0]: get_dqn for agent_name in agent2policy.values()
     }
+elif rl_algorithm == "ppo":
+    from .algorithms.ppo import get_policy, get_ppo
+    policy_creator = {
+        agent_name: partial(get_policy, STATE_DIM, NUM_CONSUMER_ACTIONS)
+        for agent_name in agent2policy.values()
+    }
+    trainer_creator = {
+        agent_name.split(".")[0]: partial(get_ppo, STATE_DIM) for agent_name in agent2policy.values()
+    }
 else:
     raise ValueError(f"Unsupported algorithm: {rl_algorithm}")
