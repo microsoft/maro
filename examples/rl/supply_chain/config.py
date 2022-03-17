c8ce2569b7cfbef51ae2374d66d46490ef952f3f
yaqiu <v-yaqiu@microsoft.com>
2022-03-17 07:43:27 +0000

    fixed or policy bugs

--- examples/rl/supply_chain/config.py
+++ examples/rl/supply_chain/config.py
@@ -8,12 +8,12 @@ env_conf = {
     # Currently available topologies are "sample" or "random". New topologies must consist of a single folder
     # that contains a single config.yml and should be placed under /maro/simulator/scenarios/supply_chain/topologies
     "topology":  join(dirname(realpath(__file__)), "walmart", "large_capacity"),
-    "durations": 100,  # number of ticks per episode
+    "durations": 10,  # number of ticks per episode
 }
 
 distribution_features = ("remaining_order_quantity", "remaining_order_number")
 seller_features = ("total_demand", "sold", "demand")
 
-rl_algorithm = "ppo"
+rl_algorithm = "null"
 
 NUM_CONSUMER_ACTIONS = 10
--- examples/rl/supply_chain/policy_trainer.py
+++ examples/rl/supply_chain/policy_trainer.py
@@ -5,34 +5,38 @@ from functools import partial
 
 from maro.simulator.scenarios.supply_chain import ConsumerUnit, ManufactureUnit, ProductUnit
 from maro.simulator.scenarios.supply_chain.world import SupplyChainEntity
+from .algorithms.rule_based import or_policy_creator
 from .config import rl_algorithm, NUM_CONSUMER_ACTIONS
 from .env_helper import entity_dict
 from .state_template import STATE_DIM
 
 
 def get_policy_name(entity: SupplyChainEntity) -> str:
-    if entity.is_facility:
+    if entity.skus is None:
         return "facility_policy"
-    elif entity.class_type == ManufactureUnit:
+    elif issubclass(entity.class_type, ManufactureUnit):
         return "manufacturer_policy"
-    elif entity.class_type == ProductUnit:
+    elif issubclass(entity.class_type, ProductUnit):
         return "product_policy"
-    elif entity.class_type == ConsumerUnit:
-        return f"dqn_{entity.id}.policy"
-
-
-agent2policy = {}
-for id_, entity in entity_dict.items():
-    if issubclass(entity.class_type, ManufactureUnit):
-        agent2policy[id_] = f"dqn_manufacture_{entity.id}.policy"
     elif issubclass(entity.class_type, ConsumerUnit):
-        agent2policy[id_] = f"dqn_consumer_{entity.id}.policy"
+        return "consumer_policy"
+    raise TypeError(f"Unrecognized entity class type: {entity.class_type}")
+
+
+agent2policy = {id_: get_policy_name(entity) for id_, entity in entity_dict.items()}
+
+# agent2policy = {}
+# for id_, entity in entity_dict.items():
+#     if issubclass(entity.class_type, ManufactureUnit):
+#         agent2policy[id_] = f"dqn_manufacture_{entity.id}.policy"
+#     elif issubclass(entity.class_type, ConsumerUnit):
+#         agent2policy[id_] = f"dqn_consumer_{entity.id}.policy"
 
 if rl_algorithm == "dqn":
     from .algorithms.dqn import get_dqn, get_policy
     policy_creator = {
-        agent_name: partial(get_policy, STATE_DIM, NUM_CONSUMER_ACTIONS)
-        for agent_name in agent2policy.values()
+        policy_name: partial(get_policy, STATE_DIM, NUM_CONSUMER_ACTIONS)
+        for policy_name in agent2policy.values()
     }
     trainer_creator = {
         agent_name.split(".")[0]: get_dqn for agent_name in agent2policy.values()
@@ -40,11 +44,14 @@ if rl_algorithm == "dqn":
 elif rl_algorithm == "ppo":
     from .algorithms.ppo import get_policy, get_ppo
     policy_creator = {
-        agent_name: partial(get_policy, STATE_DIM, NUM_CONSUMER_ACTIONS)
-        for agent_name in agent2policy.values()
+        policy_name: partial(get_policy, STATE_DIM, NUM_CONSUMER_ACTIONS)
+        for policy_name in agent2policy.values()
     }
     trainer_creator = {
-        agent_name.split(".")[0]: partial(get_ppo, STATE_DIM) for agent_name in agent2policy.values()
+        policy_name.split(".")[0]: partial(get_ppo, STATE_DIM) for policy_name in agent2policy.values()
     }
+elif rl_algorithm == 'null':
+    policy_creator = or_policy_creator
+    trainer_creator = None
 else:
     raise ValueError(f"Unsupported algorithm: {rl_algorithm}")
