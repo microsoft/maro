ecd9dcecb07e4c4ad5a7c000f69e2ffed62b5f54
yaqiu <v-yaqiu@microsoft.com>
2022-03-17 08:07:34 +0000

    removed extra space

--- examples/rl/supply_chain/config.py
+++ examples/rl/supply_chain/config.py
@@ -7,7 +7,7 @@ env_conf = {
     "scenario": "supply_chain",
     # Currently available topologies are "sample" or "random". New topologies must consist of a single folder
     # that contains a single config.yml and should be placed under /maro/simulator/scenarios/supply_chain/topologies
-    "topology":  "sample",
+    "topology": "sample",
     "durations": 100,  # number of ticks per episode
 }
 
