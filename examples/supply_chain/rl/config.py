ecf8c646d4d87b85838836bb8598c8e8a04b49ae
yaqiu <v-yaqiu@microsoft.com>
2022-03-23 16:23:22 +0000

    reverted unwanted changes

--- examples/supply_chain/rl/config.py
+++ examples/supply_chain/rl/config.py
@@ -5,8 +5,8 @@ env_conf = {
     "scenario": "supply_chain",
     # Currently available topologies are "sample" or "random". New topologies must consist of a single folder
     # that contains a single config.yml and should be placed under /maro/simulator/scenarios/supply_chain/topologies
-    "topology": "sample", # "/home/yaqiu/maro/examples/rl/supply_chain/walmart/large_capacity",
-    "durations": 10,  # number of ticks per episode
+    "topology": "sample",
+    "durations": 100,  # number of ticks per episode
 }
 
 distribution_features = ("remaining_order_quantity", "remaining_order_number")
