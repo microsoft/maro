fc007fb4303832e3436562bb5629ac5b94ba3c40
v-heli1 <v-heli1@microsoft.com>
2022-05-12 17:25:06 +0800

    add state only test

--- tests/supply_chain/test_supply_chain_state_only.py
+++ tests/supply_chain/test_supply_chain_state_only.py
@@ -35,16 +35,16 @@ HOBBY_1_ID = 30
 
 class MyTestCase(unittest.TestCase):
     """
-        Consumer test:
-
-        . initial state
-        . state after reset
-        . set_action directly from code
-        . set_action by env.step
-        . call on_order_reception directly to simulation order arrived
-        . call update_open_orders directly
-        . with dynamics sampler
-        """
+        state only  test:
+        . consumer_state_only
+            . 'pending_order_daily'
+        . seller_state_only
+            . 'sale_mean'
+            . 'sale_hist'
+        . distribution_state_only
+            . 'pending_order'
+            . 'in_transit_orders'
+    """
 
     def test_consumer_state_only(self) -> None:
         """Test the 'pending_order_daily' of the consumer unit."""
@@ -53,9 +53,7 @@ class MyTestCase(unittest.TestCase):
         assert isinstance(be, SupplyChainBusinessEngine)
 
         supplier_3 = be.world._get_facility_by_name("Supplier_SKU3")
-        distribution_unit = supplier_3.distribution
         warehouse_1 = be.world._get_facility_by_name("Warehouse_001")
-
         distribution_unit = supplier_3.distribution
 
         order = Order(warehouse_1, SKU3_ID, 10, "train")
@@ -83,8 +81,6 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(1, len(distribution_unit._order_queues["train"]))
         self.assertEqual(10, sum([order.quantity for order in distribution_unit._order_queues["train"]]))
 
-        # while env.tick < expected_tick:
-        #     env.step(None)
         env.step(None)
         self.assertEqual(20, env.metrics['products'][warehouse_1_sku3]['pending_order_daily'][2])
         env.step(None)
@@ -123,13 +119,14 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(10, env.metrics['products'][warehouse_1_sku3]['pending_order_daily'][0])
 
     def test_seller_state_only(self) -> None:
-        """Test "sale_mean" and "sale_std"""
+        """Test "sale_mean" and "_sale_hist"""
 
         env = build_env("case_05", 600)
         be = env.business_engine
         assert isinstance(be, SupplyChainBusinessEngine)
         Store_001: FacilityBase = be.world._get_facility_by_name("Store_001")
         storeproductunit_sku1, storeproductunit_sku2, storeproductunit_sku3 = 1, 3, 2
+
         self.assertEqual([1, 1, 1, 1, 1, 1], Store_001.children[storeproductunit_sku1].seller._sale_hist)
         self.assertEqual([2, 2, 2, 2, 2, 2], Store_001.children[storeproductunit_sku2].seller._sale_hist)
         self.assertEqual([3, 3, 3, 3, 3, 3], Store_001.children[storeproductunit_sku3].seller._sale_hist)
@@ -145,198 +142,97 @@ class MyTestCase(unittest.TestCase):
         # The result should be (3+3+3+3+3)/6=2.5
         self.assertEqual(2.5, env.metrics['products'][29]['sale_mean'])
         # The result should be (2+2+2+2+2)/6=1.6666666666666667
-        self.assertEqual(1.6666666666666667, env.metrics['products'][29]['sale_mean'])
+        self.assertEqual(1.6666666666666667, env.metrics['products'][32]['sale_mean'])
 
-    def test_consumer_action(self) -> None:
-        """Consumer of sku3 in Supplier_SKU1, which would purchase from Supplier_SKU3."""
-        env = build_env("case_01", 100)
+    def test_distribution_state_only(self) -> None:
+        """Test the 'pending_order' and 'in_transit_orders'of the distribution unit."""
+        env = build_env("case_05", 100)
         be = env.business_engine
         assert isinstance(be, SupplyChainBusinessEngine)
 
-        env.step(None)
+        supplier_3 = be.world._get_facility_by_name("Supplier_SKU3")
+        distribution_unit = supplier_3.distribution
+        warehouse_1 = be.world._get_facility_by_name("Warehouse_001")
+        Retailer_1: FacilityBase = be.world._get_facility_by_name("Retailer_001")
 
-        supplier_1: FacilityBase = be.world._get_facility_by_name("Supplier_SKU1")
-        supplier_3: FacilityBase = be.world._get_facility_by_name("Supplier_SKU3")
-        sku3_consumer_unit = supplier_1.products[SKU3_ID].consumer
+        order = Order(warehouse_1, SKU3_ID, 20, "train")
 
-        consumer_node_index = sku3_consumer_unit.data_model_index
+        # There are 2 "train" in total, and 1 left after scheduling this order.
+        distribution_unit.place_order(env.tick, order)
+        self.assertEqual(0, len(distribution_unit._order_queues["train"]))
+        self.assertEqual(0, sum([order.quantity for order in distribution_unit._order_queues["train"]]))
+        warehouse_1_sku3 = 11
 
-        features = ("id", "facility_id", "product_id", "order_base_cost", "purchased", "received", "order_product_cost")
-        IDX_ID, IDX_FACILITY_ID, IDX_PRODUCT_ID, IDX_ORDER_COST = 0, 1, 2, 3
-        IDX_PURCHASED, IDX_RECEIVED, IDX_ORDER_PRODUCT_COST = 4, 5, 6
+        # Here the vlt of "train" is less than "pending_order_daily" length
+        self.assertEqual([0, 0, 0, 20], env.metrics['products'][warehouse_1_sku3]['pending_order_daily'])
 
-        consumer_nodes = env.snapshot_list["consumer"]
+        # add another order, it would be successfully scheduled, but none available vehicle left now.
+        distribution_unit.place_order(env.tick, order)
+        self.assertEqual(0, len(distribution_unit._order_queues["train"]))
+        self.assertEqual(0, sum([order.quantity for order in distribution_unit._order_queues["train"]]))
+        self.assertEqual([0, 0, 0, 20 + 20], env.metrics['products'][warehouse_1_sku3]['pending_order_daily'])
 
-        # ############################### Test Action with 0 quantity ######################################
-        # zero quantity will be ignored
-        action_with_zero = ConsumerAction(sku3_consumer_unit.id, SKU3_ID, supplier_3.id, 0, "train")
-        env.step([action_with_zero])
+        # 3rd order, will cause the pending order increase
+        order_1 = Order(warehouse_1, SKU3_ID, 25, "train")
+        distribution_unit.place_order(env.tick, order_1)
+        supplier_3_id, warehouse_1_id, retailer_1_id = 1, 6, 13
 
-        states = consumer_nodes[env.frame_index:consumer_node_index:features].flatten().astype(np.int)
+        self.assertEqual(1, len(distribution_unit._order_queues["train"]))
+        self.assertEqual(25, sum([order.quantity for order in distribution_unit._order_queues["train"]]))
+        self.assertEqual(25, env.metrics['facilities'][supplier_3_id]['pending_order'][SKU3_ID])
 
-        # Nothing happened at tick 0, at the action will be recorded
-        self.assertEqual(action_with_zero.product_id, states[IDX_PRODUCT_ID])
-        self.assertEqual(action_with_zero.quantity, states[IDX_PURCHASED])
+        Warehouse_1_distribution_unit = warehouse_1.distribution
+        order_2 = Order(Retailer_1, SKU3_ID, 5, "train")
 
-        self.assertEqual(sku3_consumer_unit.id, states[IDX_ID])
-        self.assertEqual(SKU3_ID, states[IDX_PRODUCT_ID])
+        Warehouse_1_distribution_unit.place_order(env.tick, order_2)
+        Warehouse_1_distribution_unit.place_order(env.tick, order_2)
+        self.assertEqual(0, env.metrics['facilities'][warehouse_1_id]['pending_order'][SKU3_ID])
 
-        # ############################### Test Action with positive quantity ######################################
-        action = ConsumerAction(sku3_consumer_unit.id, SKU3_ID, supplier_3.id, 1, "train")
-        purchased_tick = env.tick
-        env.step([action])
+        # 3rd order, will cause the pending_order increase
+        Warehouse_1_distribution_unit.place_order(env.tick, order_2)
+        self.assertEqual(5, env.metrics['facilities'][warehouse_1_id]['pending_order'][SKU3_ID])
 
-        purchased_frame = env.business_engine.frame_index(purchased_tick)
-        states = consumer_nodes[purchased_frame:consumer_node_index:features].flatten().astype(np.int)
-        self.assertEqual(action.quantity, states[IDX_PURCHASED])
-        self.assertEqual(0, states[IDX_RECEIVED])
-        self.assertEqual(action.product_id, states[IDX_PRODUCT_ID])
+        # There is no place_order for the distribution of supplier_3, there should be no change
+        self.assertEqual(25, env.metrics['facilities'][supplier_3_id]['pending_order'][SKU3_ID])
 
-        arrival_tick = purchased_tick + 7
-        while env.tick <= arrival_tick:
+        start_tick = env.tick
+        expected_supplier_tick = start_tick + 3
+        expected_warehouse_tick = start_tick + 7
+
+        while env.tick < expected_supplier_tick - 1:
             env.step(None)
 
-        arrival_frame = env.business_engine.frame_index(arrival_tick)
-        states = consumer_nodes[arrival_frame:consumer_node_index:features].flatten().astype(np.int)
-        self.assertEqual(action.quantity, states[IDX_RECEIVED])
+        self.assertEqual(40, env.metrics['products'][warehouse_1_sku3]['pending_order_daily'][0])
+        env.step(None)
+        self.assertEqual(0, env.metrics['products'][warehouse_1_sku3]['pending_order_daily'][0])
 
-    def test_consumer_on_order_reception(self) -> None:
-        env = build_env("case_01", 100)
-        be = env.business_engine
-        assert isinstance(be, SupplyChainBusinessEngine)
+        # will arrive at the end of this tick, still on the way.
+        assert env.tick == expected_supplier_tick
+        self.assertEqual(1, len(distribution_unit._order_queues["train"]))
+        self.assertEqual(25, sum([order.quantity for order in distribution_unit._order_queues["train"]]))
+        self.assertEqual(5, env.metrics['facilities'][warehouse_1_id]['pending_order'][SKU3_ID])
 
         env.step(None)
 
-        supplier_1: FacilityBase = be.world._get_facility_by_name("Supplier_SKU1")
-        supplier_3: FacilityBase = be.world._get_facility_by_name("Supplier_SKU3")
-        sku3_consumer_unit = supplier_1.products[SKU3_ID].consumer
-        consumer_nodes = env.snapshot_list["consumer"]
-        consumer_node_index = sku3_consumer_unit.data_model_index
+        self.assertEqual(0, len(distribution_unit._order_queues["train"]))
+        self.assertEqual(0, sum([order.quantity for order in distribution_unit._order_queues["train"]]))
+        self.assertEqual(-40, env.metrics['facilities'][warehouse_1_id]['in_transit_orders'][SKU3_ID])
+        self.assertEqual(5, env.metrics['facilities'][warehouse_1_id]['pending_order'][SKU3_ID])
 
-        features = (
-            "id", "facility_id", "product_id", "order_base_cost", "purchased", "received", "order_product_cost", "id")
-        IDX_ID, IDX_FACILITY_ID, IDX_PRODUCT_ID, IDX_ORDER_COST = 0, 1, 2, 3
-        IDX_PURCHASED, IDX_RECEIVED, IDX_ORDER_PRODUCT_COST = 4, 5, 6
-        states = consumer_nodes[env.frame_index:consumer_node_index:features].flatten().astype(np.int)
+        while env.tick < expected_warehouse_tick:
+            env.step(None)
+        assert env.tick == expected_warehouse_tick
 
-        required_quantity = 550
-        action = ConsumerAction(sku3_consumer_unit.id, SKU3_ID, supplier_3.id, required_quantity, "train")
-
-        env.step([action])
-        states = consumer_nodes[env.frame_index:consumer_node_index:features].flatten().astype(np.int)
-
-        # simulate purchased product is arrived by vehicle unit
-        sku3_consumer_unit.on_order_reception(supplier_3.id, SKU3_ID, required_quantity, required_quantity)
-
-        # now all order is done
-        self.assertEqual(0, sku3_consumer_unit._open_orders[supplier_3.id][SKU3_ID])
-        self.assertEqual(required_quantity, sku3_consumer_unit._received)
-
-        # NOTE: we cannot test the received state by calling on_order_reception directly,
-        # as it will be cleared by env.step, do it on vehicle unit test.
-
+        # warehouse_1_distribution has a vlt of 7 and arrives 7+1 ticks later
+        self.assertEqual(5, env.metrics['facilities'][warehouse_1_id]['pending_order'][SKU3_ID])
         env.step(None)
+        self.assertEqual(0, env.metrics['facilities'][warehouse_1_id]['pending_order'][SKU3_ID])
 
-    def test_consumer_unit_dynamics_sampler(self):
-        """Tested the store_001  Interaction between consumer unit and dynamics csv data.
-           The data file of this test is test_case_ 04.csv"""
-        env = build_env("case_04", 600)
-        be = env.business_engine
-        assert isinstance(be, SupplyChainBusinessEngine)
-        env.step(None)
-        Store_001: FacilityBase = be.world._get_facility_by_name("Store_001")
-        FOOD_1_consumer_unit = Store_001.products[FOOD_1_ID].consumer
+        # when the order arrives, Retailer_1's in_transit_orders shall be the negative of retailer_1's arrived orders -10
+        self.assertEqual(-10, env.metrics['facilities'][retailer_1_id]['in_transit_orders'][SKU3_ID])
 
-        consumer_node_index = FOOD_1_consumer_unit.data_model_index
-
-        features = ("id", "facility_id", "product_id", "order_base_cost", "purchased", "received", "order_product_cost")
-        IDX_ID, IDX_FACILITY_ID, IDX_PRODUCT_ID, IDX_ORDER_COST = 0, 1, 2, 3
-        IDX_PURCHASED, IDX_RECEIVED, IDX_ORDER_PRODUCT_COST = 4, 5, 6
-
-        consumer_nodes = env.snapshot_list["consumer"]
-
-        # check initial state
-        self.assertEqual(0, FOOD_1_consumer_unit._received)
-        self.assertEqual(0, FOOD_1_consumer_unit._purchased)
-        self.assertEqual(0, FOOD_1_consumer_unit._order_product_cost)
-
-        # check data model state
-        # order cost from configuration
-        self.assertEqual(200, FOOD_1_consumer_unit._unit_order_cost)
-
-        # NOTE: 0 is an invalid(initial) id
-        self.assertEqual(0, FOOD_1_consumer_unit.data_model.purchased)
-        self.assertEqual(0, FOOD_1_consumer_unit.data_model.received)
-        self.assertEqual(0, FOOD_1_consumer_unit.data_model.order_product_cost)
-
-        # check sources
-        for source_facility_id in FOOD_1_consumer_unit.source_facility_id_list:
-            source_facility: FacilityBase = be.world.get_facility_by_id(source_facility_id)
-
-            # check if source facility contains the FOOD_1 config
-            self.assertTrue(FOOD_1_ID in source_facility.skus)
-
-        env.step(None)
-
-        # check state
-        states = consumer_nodes[env.frame_index:consumer_node_index:features].flatten().astype(np.int)
-
-        self.assertEqual(FOOD_1_consumer_unit.id, states[IDX_ID])
-        self.assertEqual(FOOD_1_consumer_unit.facility.id, states[IDX_FACILITY_ID])
-        self.assertEqual(FOOD_1_ID, states[IDX_PRODUCT_ID])
-        self.assertEqual(0, states[IDX_ORDER_COST])
-
-        env.reset()
-        env.step(None)
-
-        states = consumer_nodes[env.frame_index:consumer_node_index:features].flatten().astype(np.int)
-
-        # Nothing happened at tick 0, so most states will be 0
-        self.assertEqual(0, states[IDX_PURCHASED])
-        self.assertEqual(0, states[IDX_RECEIVED])
-        self.assertEqual(0, states[IDX_ORDER_PRODUCT_COST])
-
-        self.assertEqual(FOOD_1_consumer_unit.id, states[IDX_ID])
-        self.assertEqual(FOOD_1_ID, states[IDX_PRODUCT_ID])
-
-        """test_consumer_action"""
-
-        features = ("id", "facility_id", "product_id", "order_base_cost", "purchased", "received", "order_product_cost")
-        IDX_ID, IDX_FACILITY_ID, IDX_PRODUCT_ID, IDX_ORDER_COST = 0, 1, 2, 3
-        IDX_PURCHASED, IDX_RECEIVED, IDX_ORDER_PRODUCT_COST = 4, 5, 6
-
-        consumer_nodes = env.snapshot_list["consumer"]
-
-        # ############################### Test Action with 0 quantity ######################################
-        # zero quantity will be ignore
-        action_with_zero = ConsumerAction(FOOD_1_consumer_unit.id, FOOD_1_ID, Store_001.id, 0, "train")
-        env.step([action_with_zero])
-
-        states = consumer_nodes[env.frame_index:consumer_node_index:features].flatten().astype(np.int)
-
-        # Nothing happened at tick 0, at the action will be recorded
-        self.assertEqual(action_with_zero.product_id, states[IDX_PRODUCT_ID])
-        self.assertEqual(action_with_zero.quantity, states[IDX_PURCHASED])
-
-        self.assertEqual(FOOD_1_consumer_unit.id, states[IDX_ID])
-        self.assertEqual(FOOD_1_ID, states[IDX_PRODUCT_ID])
-
-        # ############################### Test Action with positive quantity ######################################
-        action = ConsumerAction(FOOD_1_consumer_unit.id, FOOD_1_ID, Store_001.id, 0, "train")
-        env.step([action])
-
-        self.assertEqual(action.quantity, FOOD_1_consumer_unit._purchased)
-        self.assertEqual(0, FOOD_1_consumer_unit._received)
-
-        states = consumer_nodes[env.frame_index:consumer_node_index:features].flatten().astype(np.int)
-
-        # action field should be recorded
-        self.assertEqual(action.product_id, states[IDX_PRODUCT_ID])
-
-        self.assertEqual(action.quantity, states[IDX_PURCHASED])
-
-        # no receives
-        self.assertEqual(0, states[IDX_RECEIVED])
+        # after the order arrives, the previous pending_order of supplier_3's distribution has also arrived, so warehouse_1's in_transit_orders should be -40 + (-25) equals -65
+        self.assertEqual(-65, env.metrics['facilities'][warehouse_1_id]['in_transit_orders'][SKU3_ID])
 
 
 if __name__ == '__main__':
