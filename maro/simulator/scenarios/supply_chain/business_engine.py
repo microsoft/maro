8ef024f265e6df144ff0f1d10929ba294c5bf20f
Huoran Li <huo53926@126.com>
2022-06-09 17:18:13 +0800

    Pass tests

--- maro/simulator/scenarios/supply_chain/business_engine.py
+++ maro/simulator/scenarios/supply_chain/business_engine.py
@@ -208,3 +208,9 @@ class SupplyChainBusinessEngine(AbsBusinessEngine):
             }
 
         return self._metrics_cache
+
+    def get_agent_idx_list(self) -> List[int]:
+        return []
+
+    def set_seed(self, seed: int) -> None:
+        pass
