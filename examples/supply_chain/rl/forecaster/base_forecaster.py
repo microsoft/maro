1d77f45f1142b9e8d18af292cbcb74ad4dd897ee
Xianliang Yang <xianya@microsoft.com>
2022-08-23 16:59:42 +0800

    format

--- examples/supply_chain/rl/forecaster/base_forecaster.py
+++ examples/supply_chain/rl/forecaster/base_forecaster.py
@@ -8,7 +8,7 @@ import pandas as pd
 class BaseForecaster(object):
     def __init__(self, data_loader_conf: dict) -> None:
         self.data_loader_conf = data_loader_conf
-    
+
     @abstractmethod
     def forecast_future_demand(self, state: dict, df_history: pd.DataFrame) -> pd.DataFrame:
         raise NotImplementedError
