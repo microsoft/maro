99ff7b99ae2a69a1343c0b38ff2308c13dabd074
Huoran Li <huoranli@microsoft.com>
2023-01-18 10:39:05 +0800

    Move to tests

---
+++ tests/rl/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+from tests.rl.gym_wrapper.rl_component_bundle import rl_component_bundle
+
+__all__ = [
+    "rl_component_bundle",
+]
