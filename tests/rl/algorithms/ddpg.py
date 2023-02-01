23f39d1f3951de786f51d6ab188d829867223654
Huoran Li <huoranli@microsoft.com>
2023-02-01 03:15:02 +0000

    Add DDPG. Performance not good...

---
+++ tests/rl/algorithms/ddpg.py
@@ -0,0 +1,85 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+import torch
+from torch.optim import Adam
+
+from maro.rl.model import QNet
+from maro.rl.model.algorithm_nets.ddpg import ContinuousDDPGNet
+from maro.rl.model.fc_block import FullyConnected
+from maro.rl.policy import ContinuousRLPolicy
+from maro.rl.training.algorithms import DDPGParams, DDPGTrainer
+
+actor_net_conf = {
+    "hidden_dims": [256, 256],
+    "activation": torch.nn.ReLU,
+    "output_activation": torch.nn.Tanh,
+}
+critic_net_conf = {
+    "hidden_dims": [256, 256],
+    "activation": torch.nn.Tanh,
+}
+actor_learning_rate = 1e-3
+critic_learning_rate = 1e-3
+
+
+class MyContinuousDDPGNet(ContinuousDDPGNet):
+    def __init__(self, state_dim: int, action_dim: int, action_limit: float) -> None:
+        super(MyContinuousDDPGNet, self).__init__(state_dim=state_dim, action_dim=action_dim)
+
+        self._net = FullyConnected(
+            input_dim=state_dim,
+            output_dim=action_dim,
+            hidden_dims=actor_net_conf["hidden_dims"],
+            activation=actor_net_conf["activation"],
+            output_activation=actor_net_conf["output_activation"],
+        )
+        self._action_limit = action_limit
+
+    def _get_actions_impl(self, states: torch.Tensor, exploring: bool) -> torch.Tensor:
+        return self._net(states) * self._action_limit
+
+
+class MyQCriticNet(QNet):
+    def __init__(self, state_dim: int, action_dim: int) -> None:
+        super(MyQCriticNet, self).__init__(state_dim=state_dim, action_dim=action_dim)
+        self._critic = FullyConnected(
+            input_dim=state_dim + action_dim,
+            output_dim=1,
+            hidden_dims=critic_net_conf["hidden_dims"],
+            activation=critic_net_conf["activation"],
+        )
+        self._optim = Adam(self._critic.parameters(), lr=critic_learning_rate)
+
+    def _get_q_values(self, states: torch.Tensor, actions: torch.Tensor) -> torch.Tensor:
+        return self._critic(torch.cat([states, actions], dim=1).float()).squeeze(-1)
+
+
+def get_ddpg_policy(
+    name: str,
+    action_lower_bound: list,
+    action_upper_bound: list,
+    gym_state_dim: int,
+    gym_action_dim: int,
+    action_limit: float,
+) -> ContinuousRLPolicy:
+    return ContinuousRLPolicy(
+        name=name,
+        action_range=(action_lower_bound, action_upper_bound),
+        policy_net=MyContinuousDDPGNet(gym_state_dim, gym_action_dim, action_limit),
+    )
+
+
+def get_ddpg_trainer(name: str, state_dim: int, action_dim: int) -> DDPGTrainer:
+    return DDPGTrainer(
+        name=name,
+        reward_discount=0.99,
+        replay_memory_capacity=1000000,
+        batch_size=100,
+        params=DDPGParams(
+            get_q_critic_net_func=lambda: MyQCriticNet(state_dim, action_dim),
+            num_epochs=1,
+            n_start_train=1000,
+            soft_update_coef=0.005,
+        ),
+    )
