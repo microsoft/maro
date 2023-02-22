7b8109567674b777847e24af21685e9fc107613f
Jinyu Wang <jinywan@microsoft.com>
2023-02-22 06:45:29 +0000

    update plot func for rl tests

--- tests/rl/plot.py
+++ tests/rl/plot.py
@@ -54,26 +54,25 @@ def get_on_policy_data(log_dir: str) -> Tuple[np.ndarray, np.ndarray]:
 
 
 def plot_performance_curves(title: str, dir_names: List[str], smooth_window_size: int) -> None:
-    for name in dir_names:
-        log_dir = os.path.join(LOG_DIR, name)
-        if not os.path.exists(log_dir):
+    for algorithm in color_map.keys():
+        if algorithm in ["ddpg", "sac", "td3"]:
+            func = get_off_policy_data
+        elif algorithm in ["ppo", "vpg"]:
+            func = get_on_policy_data
+
+        log_dirs = [os.path.join(LOG_DIR, name) for name in dir_names if algorithm in name]
+        series = [func(log_dir) for log_dir in log_dirs if os.path.exists(log_dir)]
+        if len(series) == 0:
             continue
 
-        if "ppo" in name:
-            algorithm = "ppo"
-            func = get_on_policy_data
-        elif "sac" in name:
-            algorithm = "sac"
-            func = get_off_policy_data
-        elif "ddpg" in name:
-            algorithm = "ddpg"
-            func = get_off_policy_data
-        else:
-            raise "unknown algorithm name"
+        x = series[0][0]
+        assert all(len(_x) == len(x) for _x, _ in series), f"Input data should share the same length!"
+        ys = np.array([smooth(y, smooth_window_size) for _, y in series])
+        y_mean = np.mean(ys, axis=0)
+        y_std = np.std(ys, axis=0)
 
-        x, y = func(log_dir)
-        y = smooth(y, smooth_window_size)
-        plt.plot(x, y, label=algorithm, color=color_map[algorithm])
+        plt.plot(x, y_mean, label=algorithm, color=color_map[algorithm])
+        plt.fill_between(x, y_mean - y_std, y_mean + y_std, color=color_map[algorithm], alpha=0.2)
 
     plt.legend()
     plt.title(title)
@@ -91,6 +90,10 @@ if __name__ == "__main__":
     for env_name in ["HalfCheetah", "Hopper", "Walker2d", "Swimmer", "Ant"]:
         plot_performance_curves(
             title=env_name,
-            dir_names=[f"{algorithm}_{env_name.lower()}" for algorithm in ["ppo", "sac", "ddpg"]],
+            dir_names=[
+                f"{algorithm}_{env_name.lower()}_{seed}"
+                for algorithm in ["ppo", "sac", "ddpg"]
+                for seed in [42, 729, 1024, 2023, 3500]
+            ],
             smooth_window_size=args.smooth,
         )
